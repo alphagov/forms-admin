4a4ebcf5bbcf17439f6ea930bf55970570d7d54d
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2022-03-31 11:53:42 +0100

    Add initial frontend build
    Includes:
    - rollup
    - babel
    - jest
    - dart-sass
    - govuk-frontend
    - assets build configuration to make govuk assets available

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,1 +1,4 @@
 // Entry point for the build script in your package.json
+import { initAll } from 'govuk-frontend'
+
+initAll()
--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,14 @@
-import resolve from "@rollup/plugin-node-resolve"
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import { babel } from '@rollup/plugin-babel'
 
 export default {
-  input: "app/javascript/application.js",
+  input: 'app/javascript/application.js',
   output: {
-    file: "app/assets/builds/application.js",
-    format: "es",
+    file: 'app/assets/builds/application.js',
+    format: 'es',
     inlineDynamicImports: true,
     sourcemap: true
   },
-  plugins: [
-    resolve()
-  ]
+  plugins: [resolve(), commonjs(), babel({ babelHelpers: 'bundled' })]
 }
