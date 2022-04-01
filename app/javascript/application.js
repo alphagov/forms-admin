8d5b26fff6b92691f35f53b343d864b00c02aa2b
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2022-04-01 11:43:06 +0100

    Add postcss and autoprefixer

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,5 @@
 // Entry point for the build script in your package.json
 import { initAll } from 'govuk-frontend'
+import '../assets/stylesheets/application.sass.scss'
 
 initAll()
--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,28 @@
 import resolve from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
+import scss from 'rollup-plugin-scss'
+import postcss from 'postcss'
+import autoprefixer from 'autoprefixer'
 import { babel } from '@rollup/plugin-babel'
 
-export default {
-  input: 'app/javascript/application.js',
-  output: {
-    file: 'app/assets/builds/application.js',
-    format: 'es',
-    inlineDynamicImports: true,
-    sourcemap: true
-  },
-  plugins: [resolve(), commonjs(), babel({ babelHelpers: 'bundled' })]
-}
+export default [
+  {
+    input: 'app/javascript/application.js',
+    output: {
+      file: 'app/assets/builds/application.js',
+      format: 'es',
+      inlineDynamicImports: true,
+      sourcemap: true
+    },
+    plugins: [
+      resolve(),
+      commonjs(),
+      scss({
+        processor: () => postcss([autoprefixer({ grid: 'no-autoplace' })]),
+        output: true,
+        sourceMap: true
+      }),
+      babel({ babelHelpers: 'bundled' })
+    ]
+  }
+]
