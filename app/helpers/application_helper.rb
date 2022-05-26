3ceccacc4a1ec27f779b2fffa8b4c0dcdaaf6424
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2022-05-26 14:44:10 +0100

    Add page title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,11 @@
 
 # Rubocop ate my lunch
 module ApplicationHelper
+  def page_title(separator = " – ")
+    [content_for(:title), 'GOV.UK Forms'].compact.join(separator)
+  end
+
+  def set_page_title(title)
+    content_for(:title){ title }
+  end
 end
