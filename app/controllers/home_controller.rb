471f305d40e6768b2d648571a328974e9b289ef7
Dan Burnley <dan@danops.uk>
2022-06-09 16:09:11 +0100

    Add additional examples

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,13 @@ class HomeController < ApplicationController
   def index
     @forms = Form.all
     @forms.first.pages
+    # Create a page
     # page = Page.new(form_id: @forms.first.id)
     # page.question_text = "a"
     # page.answer_type = "single_line"
     # page
     # page.save(form_id: @forms.first.id)
+    # Find a page
+    # page = Page.find(1, params: {form_id: 1})
   end
 end
