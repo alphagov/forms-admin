1123699e6da969e88c5e463b8146087fb0d6b87f
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2022-06-16 15:01:11 +0100

    Add validation

--- app/models/page.rb
+++ app/models/page.rb
@@ -4,4 +4,6 @@ class Page < ActiveResource::Base
   self.include_format_in_path = false
 
   belongs_to :form
+  validates :question_text, presence: true
+  validates :answer_type, presence: true, inclusion: { in: %w[single_line address date email national_insurance_number phone_number] }
 end
