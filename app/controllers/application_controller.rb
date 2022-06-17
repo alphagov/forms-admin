650b5b49d35d772998759d140d9cf307b1b2ffb2
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2022-06-17 15:55:04 +0100

    Use GOV.UK Form Builder gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
   include GDS::SSO::ControllerMethods
   before_action :authenticate_user!
+  default_form_builder GOVUKDesignSystemFormBuilder::FormBuilder
 end
