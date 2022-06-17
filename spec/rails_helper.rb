1524d031b0bda8d3045fd8b7fc3462afb9e4cc06
Dan Burnley <dan@danops.uk>
2022-06-17 17:00:57 +0100

    Remove webmock from tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,7 +61,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-  config.before(:example, type: :request) do |example|
+  config.before(:example, type: :request) do |_example|
     User.create!(email: "email@example.com")
   end
 end
