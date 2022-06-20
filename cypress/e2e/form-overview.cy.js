283440fc7e8569c178c3904b868a91d5518f444d
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2022-06-20 16:13:42 +0100

    52 create page for adding a question to a form (#57)
    * Add create and edit page forms
    
    Fix linting
    
    * Add validation
    
    * Add 'add question' button to form page
    
    Add test for button
    
    * Update Cypress to 10.1.0
    
    * Add request tests
    
    * Make page specs DRY with shared context
    
    * Use GOV.UK Form Builder gem
    
    * Add basic cypress tests
    
    * Add Cypress output to .gitignore
    
    * Use f. shorthand to avoid confusion with @form
    
    * Refactor routes
    
    * Fix linting
    
    * Update tests to use ActiveResource::HttpMock
    
    * Fix failing Cypress test
    
    * Localise forms
    
    * Add back links
    
    * Use form partial to avoid duplication

--- cypress/e2e/form-overview.cy.js
+++ cypress/e2e/form-overview.cy.js
@@ -5,11 +5,16 @@ describe('Form overview page', () => {
 
   it('allows the user to edit the form', () => {
     cy.contains('Edit').click()
-    cy.url().should('eq', `${Cypress.config().baseUrl}/forms/2/edit`)
+    cy.url().should('eq', `${Cypress.config().baseUrl}/forms/2/change-name`)
   })
 
   it('allows the user to navigate back to the home page', () => {
     cy.contains('Back').click()
     cy.url().should('eq', `${Cypress.config().baseUrl}/`)
+  })
+
+  it('contains an add question button', () => {
+    cy.contains('Add a question').click()
+    cy.url().should('eq', `${Cypress.config().baseUrl}/forms/2/pages/new`)
   })
 })
