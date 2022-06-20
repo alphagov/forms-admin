503b85aa706abdb0ab97945cb934a3ebf54a40db
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2022-06-20 11:31:45 +0100

    Update tests to use ActiveResource::HttpMock

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -1,36 +1,5 @@
 require "rails_helper"
 
-RSpec.shared_context "with mocked page requests" do
-  let(:form_response) do
-    stub_request(:get, "#{ENV['API_BASE']}/api/v1/forms/2")
-      .to_return(status: 200, body: { name: "Form name", submission_email: "submission@email.com", id: 2 }.to_json)
-  end
-
-  let(:form_pages_response) do
-    stub_request(:get, "#{ENV['API_BASE']}/api/v1/forms/2/pages")
-      .to_return(status: 200, body: [{
-        id: 1,
-        form_id: 2,
-        question_text: "What is your work address?",
-        question_short_name: "Work address",
-        hint_text: "This should be the location stated in your contract.",
-        answer_type: "address",
-      }].to_json)
-  end
-
-  let(:page_response) do
-    stub_request(:get, "#{ENV['API_BASE']}/api/v1/forms/2/pages/1")
-    .to_return(status: 200, body: {
-      id: 1,
-      form_id: 2,
-      question_text: "What is your work address?",
-      question_short_name: "Work address",
-      hint_text: "This should be the location stated in your contract.",
-      answer_type: "address",
-    }.to_json)
-  end
-end
-
 RSpec.describe "Pages", type: :request do
   before do
     User.create!(email: "user@example.com")
@@ -38,44 +7,108 @@ RSpec.describe "Pages", type: :request do
 
   describe "Editing an existing page" do
     describe "Given a page" do
-      include_context "with mocked page requests"
+      let(:form_response) do
+        {
+          name: "Form name",
+          submission_email: "submission@email.com",
+          id: 2,
+        }.to_json
+      end
+
+      let(:form_pages_response) do
+        [{
+          id: 1,
+          form_id: 2,
+          question_text: "What is your work address?",
+          question_short_name: "Work address",
+          hint_text: "This should be the location stated in your contract.",
+          answer_type: "address",
+        }].to_json
+      end
+
+      let(:page_response) do
+        {
+          id: 1,
+          form_id: 2,
+          question_text: "What is your work address?",
+          question_short_name: "Work address",
+          hint_text: "This should be the location stated in your contract.",
+          answer_type: "address",
+        }.to_json
+      end
 
       before do
-        form_response
-        form_pages_response
-        page_response
+        ActiveResource::HttpMock.respond_to do |mock|
+          mock.get "/api/v1/forms/2", {}, form_response, 200
+          mock.get "/api/v1/forms/2/pages", {}, form_pages_response, 200
+          mock.get "/api/v1/forms/2/pages/1", {}, page_response, 200
+        end
+
         get edit_page_path(form_id: 2, page_id: 1)
       end
 
       it "Reads the page from the API" do
-        expect(form_response).to have_been_made
-        expect(form_pages_response).to have_been_made
-        expect(page_response).to have_been_made
+        form_request = ActiveResource::Request.new(:get, "/api/v1/forms/2")
+        expect(ActiveResource::HttpMock.requests).to include form_request
+
+        form_pages_request = ActiveResource::Request.new(:get, "/api/v1/forms/2")
+        expect(ActiveResource::HttpMock.requests).to include form_pages_request
+
+        page_request = ActiveResource::Request.new(:get, "/api/v1/forms/2")
+        expect(ActiveResource::HttpMock.requests).to include page_request
       end
     end
   end
 
   describe "Updating an existing page" do
     describe "Given a page" do
-      include_context "with mocked page requests"
+      let(:form_response) do
+        {
+          name: "Form name",
+          submission_email: "submission@email.com",
+          id: 2,
+        }.to_json
+      end
 
-      let(:page_update_response) do
-        stub_request(:put, "#{ENV['API_BASE']}/api/v1/forms/2/pages/1")
-          .with(body: {
-            id: 1,
-            question_text: "What is your home address?",
-            question_short_name: "Home address",
-            hint_text: "This should be the location stated in your contract.",
-            answer_type: "address",
-          }.to_json)
-          .to_return(status: 200)
+      let(:form_pages_response) do
+        [{
+          id: 1,
+          form_id: 2,
+          question_text: "What is your work address?",
+          question_short_name: "Work address",
+          hint_text: "This should be the location stated in your contract.",
+          answer_type: "address",
+        }].to_json
+      end
+
+      let(:page_response) do
+        {
+          id: 1,
+          form_id: 2,
+          question_text: "What is your work address?",
+          question_short_name: "Work address",
+          hint_text: "This should be the location stated in your contract.",
+          answer_type: "address",
+        }.to_json
+      end
+
+      let(:updated_page_data) do
+        {
+          question_text: "What is your home address?",
+          question_short_name: "Home address",
+          hint_text: "This should be the location stated in your contract.",
+          answer_type: "address",
+        }
       end
 
       before do
-        form_response
-        form_pages_response
-        page_response
-        page_update_response
+        ActiveResource::HttpMock.respond_to do |mock|
+          mock.get "/api/v1/forms/2", {}, form_response, 200
+          mock.get "/api/v1/forms/2/pages", {}, form_pages_response, 200
+          mock.get "/api/v1/forms/2/pages/1", {}, page_response, 200
+          mock.put "/api/v1/forms/2/pages/1"
+        end
+
         patch update_page_path(form_id: 2, page_id: 1), params: { page: {
           form_id: 2,
           question_text: "What is your home address?",
@@ -86,13 +119,19 @@ RSpec.describe "Pages", type: :request do
       end
 
       it "Reads the page from the API" do
-        expect(form_response).to have_been_made
-        expect(form_pages_response).to have_been_made
-        expect(page_response).to have_been_made
+        form_request = ActiveResource::Request.new(:get, "/api/v1/forms/2")
+        expect(ActiveResource::HttpMock.requests).to include form_request
+
+        form_pages_request = ActiveResource::Request.new(:get, "/api/v1/forms/2/pages")
+        expect(ActiveResource::HttpMock.requests).to include form_pages_request
+
+        page_request = ActiveResource::Request.new(:put, "/api/v1/forms/2/pages/1")
+        expect(ActiveResource::HttpMock.requests).to include page_request
       end
 
       it "Updates the page on the API" do
-        expect(page_update_response).to have_been_made
+        expected_request = ActiveResource::Request.new(:put, "/api/v1/forms/2/pages/1", updated_page_data.to_json)
+        expect(ActiveResource::HttpMock.requests).to include(expected_request)
       end
 
       it "Redirects you to the page list" do
@@ -103,29 +142,41 @@ RSpec.describe "Pages", type: :request do
 
   describe "Creating a new page" do
     describe "Given a valid page" do
-      include_context "with mocked page requests"
+      let(:form_response) do
+        {
+          name: "Form name",
+          submission_email: "submission@email.com",
+          id: 2,
+        }.to_json
+      end
 
-      let(:page_creation_request) do
-        stub_request(:post, "#{ENV['API_BASE']}/api/v1/forms/2/pages")
-          .with(body: {
-            question_text: "What is your home address?",
-            question_short_name: "Home address",
-            hint_text: "This should be the location stated in your contract.",
-            answer_type: "address",
-          })
-          .to_return(status: 200, body: {
-            question_text: "What is your home address?",
-            question_short_name: "Home address",
-            hint_text: "This should be the location stated in your contract.",
-            answer_type: "address",
-            id: 1,
-          }.to_json)
+      let(:form_pages_response) do
+        [{
+          id: 1,
+          form_id: 2,
+          question_text: "What is your work address?",
+          question_short_name: "Work address",
+          hint_text: "This should be the location stated in your contract.",
+          answer_type: "address",
+        }].to_json
+      end
+
+      let(:new_page_data) do
+        {
+          question_text: "What is your home address?",
+          question_short_name: "Home address",
+          hint_text: "This should be the location stated in your contract.",
+          answer_type: "address",
+        }
       end
 
       before do
-        form_response
-        form_pages_response
-        page_creation_request
+        ActiveResource::HttpMock.respond_to do |mock|
+          mock.get "/api/v1/forms/2", {}, form_response, 200
+          mock.get "/api/v1/forms/2/pages", {}, form_pages_response, 200
+          mock.post "/api/v1/forms/2/pages"
+        end
+
         post create_page_path(2), params: { page: {
           question_text: "What is your home address?",
           question_short_name: "Home address",
@@ -139,7 +190,8 @@ RSpec.describe "Pages", type: :request do
       end
 
       it "Creates the page on the API" do
-        expect(page_creation_request).to have_been_made
+        expected_request = ActiveResource::Request.new(:post, "/api/v1/forms/2/pages", new_page_data.to_json)
+        expect(ActiveResource::HttpMock.requests).to include(expected_request)
       end
     end
   end
