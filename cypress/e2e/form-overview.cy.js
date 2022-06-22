4b19720c30bcb480943237b74c68b49d170ec871
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2022-06-22 10:55:06 +0100

    Add cypress test

--- cypress/e2e/form-overview.cy.js
+++ cypress/e2e/form-overview.cy.js
@@ -17,4 +17,19 @@ describe('Form overview page', () => {
     cy.contains('Add a question').click()
     cy.url().should('eq', `${Cypress.config().baseUrl}/forms/2/pages/new`)
   })
+
+  it('contains a list of questions', () => {
+    cy.findByRole('heading', {
+      name: 'Your questions'
+    }).should('be.visible')
+
+    cy.findAllByText('How many forms do you expect to publish annually?')
+      .first()
+      .should('be.visible')
+
+    cy.findByRole('link', {
+      name: 'Edit How many forms do you expect to publish annually?'
+    }).click()
+    cy.url().should('eq', `${Cypress.config().baseUrl}/forms/2/pages/27/edit`)
+  })
 })
