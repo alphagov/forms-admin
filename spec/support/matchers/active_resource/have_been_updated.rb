9488372ad41636a5ec8c2b20a2e41df4bb40060e
Dan Burnley <dan@danops.uk>
2022-06-23 14:54:45 +0100

    Set next param on page creation

--- spec/support/matchers/active_resource/have_been_updated.rb
+++ spec/support/matchers/active_resource/have_been_updated.rb
@@ -2,7 +2,7 @@ require_relative "helper_methods"
 
 RSpec::Matchers.define :have_been_updated do
   match do |resource|
-    expected_request_path = resource.class.element_path(resource.id)
+    expected_request_path = resource.class.element_path(resource.id, resource.prefix_options)
     expected_request = ActiveResource::Request.new(:put, expected_request_path, resource.to_json)
     matched_request = ActiveResource::HttpMock.requests.find do |request|
       request.method == expected_request.method &&
@@ -14,7 +14,7 @@ RSpec::Matchers.define :have_been_updated do
   end
 
   failure_message do |resource|
-    expected_request_path = resource.class.element_path(resource.id)
+    expected_request_path = resource.class.element_path(resource.id, resource.prefix_options)
     expected_request = ActiveResource::Request.new(:put, expected_request_path, resource.to_json)
 
     HelperMethods.format_failure_message(resource, expected_request, ActiveResource::HttpMock.requests)
