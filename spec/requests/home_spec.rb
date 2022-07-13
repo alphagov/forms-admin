dd21941f3f53e7c2540764ca83f9635740c2f836
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2022-07-13 15:17:28 +0100

    Add org parameter to form list request (#65)
    * Add org parameter to form list request
    
    * Fix linting

---
+++ spec/requests/home_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+RSpec.describe "Home", type: :request do
+  describe "Viewing all forms for an organisation" do
+    describe "Given a form" do
+      let(:forms_response) do
+        [{
+          id: 2,
+          name: "Form name",
+          submission_email: "submission@email.com",
+          org: "test-org",
+        }]
+      end
+
+      before do
+        ActiveResource::HttpMock.respond_to do |mock|
+          mock.get "/api/v1/forms?org=test-org", {}, forms_response.to_json, 200
+        end
+        get root_path
+      end
+
+      it "Reads the forms from the API" do
+        forms_request = ActiveResource::Request.new(:get, "/api/v1/forms?org=test-org")
+        expect(ActiveResource::HttpMock.requests).to include forms_request
+      end
+    end
+  end
+end
