3b884a7c445252c364e31c202699037b5eb09ca8
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2022-07-13 12:33:16 +0100

    Bump to 7.0.3.1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_01_153028) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_13_111847) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
