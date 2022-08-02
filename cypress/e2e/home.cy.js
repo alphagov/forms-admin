413021c53667f6caf70689b950e341ddb5fce15c
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2022-08-02 15:00:15 +0100

    Add phase banner
    Add beta phase banner

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -12,4 +12,15 @@ describe('Home screen', function () {
     cy.contains('Edit').click()
     cy.url().should('match', /\/forms\/\d+/)
   })
+
+  it('contains the beta phase banner', function () {
+    cy.findAllByText('Beta')
+      .first()
+      .should('be.visible')
+    cy.findAllByText('feedback')
+      .first()
+      .should('be.visible')
+      .should('have.attr', 'href')
+      .and('equal', 'mailto:govuk-forms@digital.cabinet-office.gov.uk')
+  })
 })
