7c4b4386edf68cd1c827a20ba9aa46d94569110d
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2022-08-18 10:01:52 +0100

    Add environment to Sentry (#102)
    * Fix sentry logging and add environment
    
    * Remove broken 404 logic
    
    * Use PAAS_ENVIRONMENT var
    
    * Fix linting
    
    * Add missing environment variable

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :check_service_unavailable
   default_form_builder GOVUKDesignSystemFormBuilder::FormBuilder
-  rescue_from ActiveResource::ResourceNotFound, with: :render_not_found_error
 
   def check_service_unavailable
     if ENV["SERVICE_UNAVAILABLE"].present?
--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,8 +1,9 @@
 Sentry.init do |config|
   config.dsn = ENV["SENTRY_DSN"]
-  config.breadcrumbs_logger = %i[sentry_logger http_logger]
+  config.breadcrumbs_logger = %i[active_support_logger http_logger]
   config.debug = true
   config.traces_sample_rate = 1.0
+  config.environment = ENV["PAAS_ENVIRONMENT"] || "local"
 end
 
 # Uncomment out the below to test Sentry - this
