0d7beeb5e921ffe7f7c4ea48d2a7f3318042284f
Alistair Laing <aliuk2012@users.noreply.github.com>
2022-08-25 09:06:11 +0100

    Replace form show page content with task list

--- cypress/e2e/form-overview.cy.js
+++ cypress/e2e/form-overview.cy.js
@@ -3,23 +3,17 @@ describe('Form overview page', function () {
     cy.visit(`/forms/${this.formId}`)
   })
 
-  it('allows the user to edit the form', function () {
-    cy.contains('Edit').click()
-    cy.url().should(
-      'eq',
-      `${Cypress.config().baseUrl}/forms/${this.formId}/change-name`
-    )
-  })
-
   it('allows the user to navigate back to the home page', function () {
     cy.contains('Back').click()
     cy.url().should('eq', `${Cypress.config().baseUrl}/`)
   })
 
-  it('contains a question section', function () {
-    cy.findByRole('heading', {
-      name: 'Your questions'
-    }).should('be.visible')
+  it('allows the user to edit the form name', function () {
+    cy.contains('Edit the name of your form').click()
+    cy.url().should(
+      'eq',
+      `${Cypress.config().baseUrl}/forms/${this.formId}/change-name`
+    )
   })
 
   it('contains a link to add/edit the forms questions', function () {
@@ -28,5 +22,32 @@ describe('Form overview page', function () {
       'eq',
       `${Cypress.config().baseUrl}/forms/${this.formId}/pages`
     )
+  })
+
+  describe('Task list for new forms without pages', function () {
+    beforeEach(function () {
+      // Create test form in database
+      cy.fixture('seed-data').then(function (seedDataJSON) {
+        const { form } = seedDataJSON
+
+        cy.createForm(form)
+          .its('id')
+          .as('formId')
+          .then(function () {
+            cy.visit(`/forms/${this.formId}`)
+          })
+      })
+    })
+    after(function () {
+      cy.deleteForm(this.formId)
+    })
+
+    it('add/edit the forms questions directs user to create new question page', function () {
+      cy.contains('Add and edit your questions').click()
+      cy.url().should(
+        'eq',
+        `${Cypress.config().baseUrl}/forms/${this.formId}/pages/new`
+      )
+    })
   })
 })
