7a4961fefad0ce183ea631c14347ae4712003a18
Thomas Iles <tomiles@gmail.com>
2022-09-15 11:42:12 +0100

    Stop page titles being double html escaped (#125)
    When editing pages, the title of the page uses question_text. When the
    question_text contains some characters, such as ', rails escapes them
    twice, which produces titles with hex codes in.
    
    This commit stops that happening and adds a view test to the edit page
    view. There are other tests which could be added - testing the title
    helper etc. but the view test seemed like the most reliable way to test
    the escaping behaviour. This means rendering using the application
    layout, which breaks out of the unit testing model a bit.
    
    Co-authored-by: Tom Iles <tom.iles@engineering.digital.dwp.gov.uk>

---
+++ spec/views/pages/edit.html.erb_spec.rb
@@ -0,0 +1,37 @@
+require "rails_helper"
+
+describe "pages/edit.html.erb" do
+  let(:question_text) { nil }
+
+  before do
+    # Initialize models
+    page = Page.new(id: 1, question_text:, form_id: 1)
+    form = Form.new(id: 1, name: "Form 1", form_id: 1, pages: [page])
+
+    # If models aren't persisted, they won't work with form builders correctly
+    allow(page).to receive(:persisted?).and_return(true)
+    allow(form).to receive(:persisted?).and_return(true)
+
+    # Assign instance variables so they can be accessed from views
+    assign(:form, form)
+    assign(:page, page)
+    assign(:answer_types, [])
+
+    # This is normally done in the ApplicationController, but we aren't using
+    # that in this test
+    ActionView::Base.default_form_builder = GOVUKDesignSystemFormBuilder::FormBuilder
+
+    # Render the template with layout to include page title, we could check
+    # expect(view.content_for(:title)).to have_content("Question'<> 1 – GOV.UK Forms")
+    # instead, but this checks behaviour better
+    render(template: "pages/edit", layout: "layouts/application")
+  end
+
+  context "when given a title with characters which need escaping" do
+    let(:question_text) { "Question'<> 1" }
+
+    it "has the correct title" do
+      expect(rendered).to have_title("Question'<> 1 – GOV.UK Forms")
+    end
+  end
+end
