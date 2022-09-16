a43f14fb157dbe9be7abb5025295ef963b7e1da6
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2022-09-16 13:25:32 +0100

    Update runner links with new preview URL

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe ApplicationHelper, type: :helper do
   describe "#link_to_runner" do
     it "returns url to the form-runners form" do
-      expect(helper.link_to_runner("example.com", 2)).to eq "example.com/form/2"
+      expect(helper.link_to_runner("example.com", 2)).to eq "example.com/preview-form/2"
     end
   end
 end
