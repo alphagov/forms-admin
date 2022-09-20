d329ec14a1ca2747d52374b7d25940b309915282
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2022-09-20 13:53:11 +0100

    Add copy to clipboard button

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,15 @@
 // Entry point for the build script in your package.json
 import { initAll } from 'govuk-frontend'
+import copyToClipboard from './modules/copy-to-clipboard'
+
+document
+  .querySelectorAll('[data-module="copy-to-clipboard"]')
+  .forEach(element => {
+    copyToClipboard(
+      element,
+      element.querySelector('[data-copy-target]'),
+      element.dataset.copyButtonText
+    )
+  })
 
 initAll()
