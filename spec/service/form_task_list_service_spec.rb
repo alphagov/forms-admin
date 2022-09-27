ffcc678e2858d6ddbc02003e0a24c6293afad03c
Alistair Laing <aliuk2012@users.noreply.github.com>
2022-09-27 22:05:07 +0100

    Refactor form task list out to a service
    It seems like the time is right to refactor the
    private method in the forms controller and better
    to make it a service. The idea is that if you pass
    in a form, the service will work out which sections
    and tasks to return. This array of objects represents
    a group of actions that a user can take on a give form.
    
    This refactor will help us when we start looking at
    including status for each section and it allows us
    to add more methods in future like return a list of
    complete/incomplete tasks

---
+++ spec/service/form_task_list_service_spec.rb
@@ -0,0 +1,112 @@
+require "rails_helper"
+
+describe FormTaskListService do
+  describe "#all_tasks" do
+    let(:form) { build(:form, id: 1) }
+
+    it "returns array of tasks objects for a given form" do
+      expect(described_class.call(form:).all_tasks).to be_an_instance_of(Array)
+    end
+
+    it "returns 4 sections" do
+      expected_sections = [{ title: "Task 1" }, { title: "Task 2" }, { title: "Task 3" }, { title: "Task 4" }]
+      expect(described_class.call(form:).all_tasks.count).to eq expected_sections.count
+    end
+
+    describe "section 1 tasks" do
+      let(:section) do
+        described_class.call(form:).all_tasks.first
+      end
+
+      let(:section_rows) { section[:rows] }
+
+      it "has links to edit form name" do
+        expect(section_rows.first[:task_name]).to eq "Edit the name of your form"
+        expect(section_rows.first[:path]).to eq "/forms/1/change-name"
+      end
+
+      it "has a link to add new pages/questions (if no pages/questions exist)" do
+        expect(section_rows[1][:task_name]).to eq "Add and edit your questions"
+        expect(section_rows[1][:path]).to eq "/forms/1/pages/new"
+      end
+
+      it "has a link to add/edit existing pages (if pages/questions exist)" do
+        page = build :page
+        form.pages = [page]
+        expect(section_rows[1][:task_name]).to eq "Add and edit your questions"
+        expect(section_rows[1][:path]).to eq "/forms/1/pages"
+      end
+
+      it "has a link to add/edit 'What happens next'" do
+        expect(section_rows[2][:task_name]).to eq "Add information about what happens next"
+        expect(section_rows[2][:path]).to eq "/forms/1/what-happens-next"
+      end
+    end
+
+    describe "section 2 tasks" do
+      let(:section) do
+        described_class.call(form:).all_tasks[1]
+      end
+
+      let(:section_rows) { section[:rows] }
+
+      it "has link to set submission email" do
+        expect(section_rows.first[:task_name]).to eq "Set the email address completed forms will be sent to"
+        expect(section_rows.first[:path]).to eq "/forms/1/change-email"
+      end
+    end
+
+    describe "section 3 tasks" do
+      let(:section) do
+        described_class.call(form:).all_tasks[2]
+      end
+
+      let(:section_rows) { section[:rows] }
+
+      it "has link to set privacy policy url" do
+        expect(section_rows.first[:task_name]).to eq "Provide a link to privacy information for this form"
+        expect(section_rows.first[:path]).to eq "/forms/1/privacy_policy"
+      end
+    end
+
+    describe "section 4 tasks" do
+      let(:section) do
+        described_class.call(form:).all_tasks[3]
+      end
+
+      let(:section_rows) { section[:rows] }
+
+      it "has text to make the form live (no link)" do
+        expect(section_rows.first[:task_name]).to eq "Make your form live"
+        expect(section_rows.first[:path]).to be_empty
+      end
+
+      describe "when form is ready to make live" do
+        let(:section) do
+          allow(form).to receive(:ready_for_live?).and_return(true)
+          described_class.call(form:).all_tasks[3]
+        end
+
+        let(:section_rows) { section[:rows] }
+
+        it "has link to make the form live" do
+          expect(section_rows.first[:task_name]).to eq "Make your form live"
+          expect(section_rows.first[:path]).to eq "/forms/1/make_live"
+        end
+      end
+
+      describe "when form is live" do
+        let(:section) do
+          allow(form).to receive(:live?).and_return(true)
+          described_class.call(form:).all_tasks[3]
+        end
+
+        let(:section_rows) { section[:rows] }
+
+        it "has no tasks" do
+          expect(section_rows).to be_empty
+        end
+      end
+    end
+  end
+end
