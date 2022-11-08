8826229fe258fbc6a0d3ac3ee408e5b22f67ee1c
Tom Iles <tom.iles@engineering.digital.dwp.gov.uk>
2022-11-08 16:39:35 +0000

    Fix: set email controller flow

--- app/controllers/forms/set_email_controller.rb
+++ app/controllers/forms/set_email_controller.rb
@@ -5,17 +5,17 @@ module Forms
     end
 
     def create
-      # @change_email_form = ChangeEmailForm.new(change_email_form_params)
+      @set_email_form = SetEmailForm.new(set_email_form_params)
 
-      # if @change_email_form.submit
-      #   redirect_to form_path(@change_email_form.form)
-      # else
-      #   render :new
-      # end
+      if @set_email_form.submit
+        redirect_to form_path(@set_email_form.form)
+      else
+        render :new
+      end
     end
 
-    # def change_email_form_params
-    #   params.require(:forms_change_email_form).permit(:submission_email).merge(form: current_form)
-    # end
+    def set_email_form_params
+      params.require(:forms_set_email_form).permit(:submission_email).merge(form: current_form)
+    end
   end
 end
