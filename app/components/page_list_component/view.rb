ad7e33c265e39b63daf1025a2993f2258ddd38e8
Tom Iles <tom.iles@engineering.digital.dwp.gov.uk>
2022-11-25 15:24:18 +0000

    Add PageListComponent
    Extract the page list shown on the edit pages screen into a component.
    
    This is to make it easier to change structure and easier to add feature
    flag for moving questions.

---
+++ app/components/page_list_component/view.rb
@@ -0,0 +1,11 @@
+module PageListComponent
+  class View < ViewComponent::Base
+    delegate :question_text_with_optional_suffix, to: :helpers
+
+    def initialize(form_id:, pages: [])
+      super
+      @pages = pages
+      @form_id = form_id
+    end
+  end
+end
