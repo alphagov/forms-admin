eeab5a1f337e520ccd1ab55624afc78288ebe6bf
Tom Iles <tom.iles@engineering.digital.dwp.gov.uk>
2022-11-29 16:29:46 +0000

    Improve view tests for pages/_forms partial
    Add tests cases for all parts of the partial, including conditional
    features.

--- spec/views/pages/_forms.html.erb_spec.rb
+++ spec/views/pages/_forms.html.erb_spec.rb
@@ -1,16 +1,11 @@
 require "rails_helper"
 
 describe "pages/_form.html.erb", type: :view do
-  let(:question) { build :page, id: 1, form_id: 1 }
+  let(:question) { build :page, :with_hints, id: 1, form_id: 1 }
   let(:form) { build :form, id: 1, pages: [question] }
   let(:is_new_page) { true }
 
   before do
-
-    # This is normally done in the ApplicationController, but we aren't using
-    # that in this test
-    ActionView::Base.default_form_builder = GOVUKDesignSystemFormBuilder::FormBuilder
-
     render partial: 'pages/form', locals: { is_new_page: is_new_page,
                                             form_object: form,
                                             page_object: question,
@@ -19,19 +14,47 @@ describe "pages/_form.html.erb", type: :view do
     }
   end
 
-  describe "new question detail page" do
-    it "has a hidden field for the answer type" do
-      page = Capybara.string(rendered)
-      expect(page.find('#page_answer_type', visible: false)[:value]).to eq question.answer_type
-    end
+  it "has a form with correct action" do
+    expect(rendered).to have_selector('form[action="http://example.com"]')
   end
 
-  describe "edit question details page" do
+  it "has a hidden field for the answer type" do
+    expect(rendered).to have_field('page[answer_type]', with: question.answer_type, type: :hidden)
+  end
+
+  it "has a field with the question text" do
+    expect(rendered).to have_field(type: "text", with: question.question_text)
+  end
+
+  it "has a field with the hint text" do
+    expect(rendered).to have_field(type: "text", with: question.hint_text)
+  end
+
+  it "has an unchecked optional checkbox" do
+    expect(rendered).to have_unchecked_field('page[is_optional]')
+  end
+
+  it "has a link to change the answer type" do
+    expect(rendered).to have_link(text: "Change", href:"http://change-me-please.com")
+  end
+
+  it "has a submit button with the correct text" do
+    expect(rendered).to have_button('Save and add next question')
+  end
+
+  it "does not have a delete button" do
+    expect(rendered).to_not have_button('delete')
+  end
+
+  context "when it is not a new page" do
     let(:is_new_page) { false }
 
     it "has no hidden field for the answer type" do
-      page = Capybara.string(rendered)
-      expect(page).not_to have_selector('#page_answer_type', visible: false)
+      expect(rendered).not_to have_field('page[answer_type]', type: :hidden)
+    end
+
+    it "has a delete button" do
+      expect(rendered).to have_button('delete')
     end
   end
 end
