78499b7d0f149635d955f2382f8da131519569a2
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2022-12-14 17:16:15 +0000

    Fix intermittently breaking test

--- spec/factories/forms/type_of_answer_form.rb
+++ spec/factories/forms/type_of_answer_form.rb
@@ -4,7 +4,11 @@ FactoryBot.define do
     form { build :form }
 
     trait :without_selection_answer_type do
-      answer_type { %w[single_line number address date email national_insurance_number phone_number long_text organisation_name].sample }
+      if FeatureService.enabled?(:autocomplete_answer_types)
+        answer_type { %w[single_line number address date email national_insurance_number phone_number long_text organisation_name].sample }
+      else
+        answer_type { %w[single_line number address date email national_insurance_number phone_number long_text].sample }
+      end
     end
   end
 end
