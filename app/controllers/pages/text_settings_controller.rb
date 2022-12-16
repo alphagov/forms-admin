2bf84ec0040933fd3ac20ce90da38fcc635514b5
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2022-12-16 11:42:19 +0000

    Remove unnecessary instance variable

--- app/controllers/pages/text_settings_controller.rb
+++ app/controllers/pages/text_settings_controller.rb
@@ -2,14 +2,12 @@ class Pages::TextSettingsController < PagesController
   def new
     input_type = session[:page]["answer_settings"]["input_type"] if session[:page]["answer_settings"].present?
     @text_settings_form = Forms::TextSettingsForm.new(input_type:)
-    @input_types = Forms::TextSettingsForm::INPUT_TYPES
     @text_settings_path = text_settings_create_path(@form)
     render "pages/text_settings"
   end
 
   def create
     @text_settings_form = Forms::TextSettingsForm.new(text_settings_form_params)
-    @input_types = Forms::TextSettingsForm::INPUT_TYPES
     @text_settings_path = text_settings_create_path(@form)
 
     if @text_settings_form.submit(session)
@@ -23,7 +21,6 @@ class Pages::TextSettingsController < PagesController
     @page = Page.find(params[:page_id], params: { form_id: @form.id })
     input_type = @page&.answer_settings&.input_type ? @page.answer_settings&.input_type : ""
     @text_settings_form = Forms::TextSettingsForm.new(input_type:, page: @page)
-    @input_types = Forms::TextSettingsForm::INPUT_TYPES
     @text_settings_path = text_settings_update_path(@form)
     render "pages/text_settings"
   end
@@ -31,7 +28,6 @@ class Pages::TextSettingsController < PagesController
   def update
     @page = Page.find(params[:page_id], params: { form_id: @form.id })
     @text_settings_form = Forms::TextSettingsForm.new(text_settings_form_params)
-    @input_types = Forms::TextSettingsForm::INPUT_TYPES
     @text_settings_path = text_settings_update_path(@form)
 
     if @text_settings_form.assign_values_to_page(@page) && @page.save!
