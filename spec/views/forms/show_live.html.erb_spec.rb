1e9008f5cc811c5794e2ac7f1c2db0d02c9ab265
Tom Iles <tom.iles@engineering.digital.dwp.gov.uk>
2023-01-17 11:21:56 +0000

    Add content for show_live_form
    Add the content for showing a live form.
    
    This display is meant to be read only, although it has the option to
    edit the form through links. This is to aid the transition to the
    draft/live workflow where live forms will be read only and all editing
    is done on a draft version of the form, which is then published.
    
    Co-authored-by: Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
    Co-authored-by: Alistair Laing <aliuk2012@users.noreply.github.com>

--- spec/views/forms/show_live.html.erb_spec.rb
+++ spec/views/forms/show_live.html.erb_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 describe "forms/show_live.html.erb" do
-  let(:form) { build(:form, :live, id: 2) }
+  let(:form) { build(:form, :live, :with_pages, id: 2) }
 
   around do |example|
     ClimateControl.modify RUNNER_BASE: "runner-host" do
@@ -10,11 +10,104 @@ describe "forms/show_live.html.erb" do
   end
 
   before do
+    allow(view).to receive(:form_pages_path).and_return("/form-pages-path")
+    allow(view).to receive(:declaration_path).and_return("/declaration_path")
+    allow(view).to receive(:what_happens_next_path).and_return("/what_happens_next_path")
+
     assign(:form, form)
-    render template: "forms/show_live"
+    render(template: "forms/show_live", layout: "layouts/application")
   end
 
-  it "contains placeholder text" do
-    expect(rendered).to have_content("placeholder")
+  it "has the correct title" do
+    expect(rendered).to have_title "#{form.name} – GOV.UK Forms"
+  end
+
+  it "back link is set to root" do
+    expect(rendered).to have_link("Back to your forms", href: "/")
+  end
+
+  it "contains page heading" do
+    expect(rendered).to have_css("h1.govuk-heading-l", text: form.name)
+  end
+
+  it "rendered live tag" do
+    expect(rendered).to have_css(".govuk-tag.govuk-tag--blue", text: "LIVE")
+  end
+
+  it "contains a link to preview the form" do
+    expect(rendered).to have_link("Preview this form in a new tab", href: "runner-host/preview-form/2/#{form.form_slug}", visible: :all)
+  end
+
+  it "contains a link to the form in the runner" do
+    expect(rendered).to have_content("runner-host/form/2/#{form.form_slug}")
+  end
+
+  it "contains a link to edit questions" do
+    expect(rendered).to have_link("#{form.pages.count} questions", href: "/form-pages-path")
+  end
+
+  context "with only a single question" do
+    let(:form) { build(:form, :live, :with_pages, id: 2, pages_count: 1) }
+
+    it "contains a link to edit questions with correct pluralization" do
+      expect(rendered).to have_link("1 question", href: "/form-pages-path")
+    end
+  end
+
+  it "contains a link to declaration" do
+    expect(rendered).to have_link("Declaration", href: "/declaration_path")
+  end
+
+  it "contains a link to what happens next" do
+    expect(rendered).to have_link("What happens next", href: "/what_happens_next_path")
+  end
+
+  it "contains the submission email" do
+    expect(rendered).to have_content(form.submission_email)
+  end
+
+  it "contains link to privacy policy " do
+    expect(rendered).to have_link(form.privacy_policy_url, href: form.privacy_policy_url)
+  end
+
+  context "with a support email address" do
+    let(:form) { build(:form, :live, :with_pages, id: 2, support_email: "support@example.gov.uk") }
+
+    it "shows the support email address" do
+      expect(rendered).to have_css("h3", text: "Email")
+      expect(rendered).to have_content("support@example.gov.uk")
+    end
+  end
+
+  context "with a support phone" do
+    let(:form) { build(:form, :live, :with_pages, id: 2, support_phone: "phone details") }
+
+    it "shows the support email address" do
+      expect(rendered).to have_css("h3", text: "Phone")
+      expect(rendered).to have_content("phone details")
+    end
+  end
+
+  context "with a support online" do
+    let(:form) { build(:form, :live, :with_pages, id: 2, support_url_text: "website", support_url: "www.example.gov.uk") }
+
+    it "shows the support contact online" do
+      expect(rendered).to have_css("h3", text: "Support contact online")
+      expect(rendered).to have_link(form.support_url_text, href: form.support_url)
+    end
+  end
+
+  context "with no support information set" do
+    let(:form) { build(:form, :live, :with_pages, id: 2, support_email: nil, support_phone: nil, support_url_text: nil, support_url: nil) }
+
+    it "does not include support details if they are not set" do
+      expect(rendered).not_to have_css("h3", text: "Email")
+      expect(rendered).not_to have_css("h3", text: "Phone")
+      expect(rendered).not_to have_css("h3", text: "Support contact online")
+    end
+  end
+
+  it "contains a link to edit the form" do
+    expect(rendered).to have_link("Edit this form", href: form_path(form.id, edit: true))
   end
 end
