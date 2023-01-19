8695b49a348bddf3c46145e105c631cb289040dd
Tom Iles <tom.iles@engineering.digital.dwp.gov.uk>
2023-01-19 09:21:36 +0000

    Extract status display tag into component
    The Form status is shown as a tag in a description list with some hidden
    text for screen reader context. This pattern appears in the live and
    'draft' form show template.
    
    This commit extracts the pattern into a simple component without logic
    or config to keep the two uses consistent.

---
+++ spec/components/form_status_tag_description_component/view_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+RSpec.describe FormStatusTagDescriptionComponent::View, type: :component do
+  describe "draft/default status" do
+    before do
+      render_inline(described_class.new)
+    end
+
+    it "renders the draft status by default" do
+      expect(page).to have_text("DRAFT")
+    end
+  end
+
+  describe "live status" do
+    before do
+      render_inline(described_class.new(status: "live"))
+    end
+
+    it "renders the status text" do
+      expect(page).to have_text("LIVE")
+    end
+  end
+end
