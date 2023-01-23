2634d6f155acd7ad53fc1c0acd2d952b584c27c8
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2023-01-23 17:30:04 +0000

    Update spec/requests/pages/name_settings_spec.rb
    Co-authored-by: Alistair Laing <aliuk2012@users.noreply.github.com>

--- spec/requests/pages/name_settings_spec.rb
+++ spec/requests/pages/name_settings_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe "NameSettings controller", type: :request, feature_autocomplete_answer_types: true do
+RSpec.describe Pages::NameSettingsController, type: :request, feature_autocomplete_answer_types: true do
   let(:form) { build :form, id: 1 }
   let(:pages) { build_list :page, 5, form_id: form.id }
 
