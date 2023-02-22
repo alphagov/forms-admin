cae2089fcaaa55e60e72316fc73417531006961a
Alistair Laing <aliuk2012@users.noreply.github.com>
2023-02-22 20:36:32 +0000

    Update 'Edit your form' task list page
    Updates the content and design of the 'edit your form' page to match the
    designs in Mural [1].
    
    When a form has never been published before we what to explain to the
    user that they need to make their form live. However, if the form has
    been made live for the first task we need to inform the user to make
    their changes live.
    
    Although there are some similarities between the copy for the 'create a
    form' task list and the 'edit your form' task lists, I found that it was
    easier to keep the two separated in the locales file. We make
    FormTaskListService responsible for selecting the appropriate content
    depending on whether the form has been made live already or not.
    
    In the task list service I wanted to avoid too much repetition, so I
    centralised the logic that chooses whether we want content for creating
    or editing. The way I (@lfdebrux) did this does result in some fairly
    ugly string interpolation; there may be a better way. We also need to
    help i18n-tasks match these dynamic keys with what's in the config file,
    so we add a new pattern mapper scanner to the config, following the
    examples therein [[2]].
    
    Note that these changes don't need to be behind a feature flag, as the
    content is valid whether the changes are to a draft or going live
    immediately.
    
    This commit also fixes the back link and the location of the preview
    link.
    
    [1]: https://app.mural.co/t/gaap0347/m/gaap0347/1667389115580/c92466b787f2ce28bc5ca3786ee60fde382a8aa6?wid=0-1674836853966
    [2]: https://github.com/glebm/i18n-tasks/blob/v1.0.11/templates/config/i18n-tasks.yml#L142-L153
    
    Co-authored-by: hannahkc <hannah.cooper@digital.cabinet-office.gov.uk>
    Co-authored-by: Alistair Laing <aliuk2012@users.noreply.github.com>
    Co-authored-by: Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>

--- app/service/form_task_list_service.rb
+++ app/service/form_task_list_service.rb
@@ -17,14 +17,18 @@ class FormTaskListService
 
   def all_tasks
     [
-      { title: I18n.t("forms.task_lists.section_1.title"), rows: section_1_tasks },
-      { title: I18n.t("forms.task_lists.section_2.title"), rows: section_2_tasks },
-      { title: I18n.t("forms.task_lists.section_3.title"), rows: section_3_tasks },
-      { title: I18n.t("forms.task_lists.section_4.title"), rows: section_4_tasks },
+      { title: I18n.t("forms.task_list_#{create_or_edit}.section_1.title"), rows: section_1_tasks },
+      { title: I18n.t("forms.task_list_#{create_or_edit}.section_2.title"), rows: section_2_tasks },
+      { title: I18n.t("forms.task_list_#{create_or_edit}.section_3.title"), rows: section_3_tasks },
+      { title: I18n.t("forms.task_list_#{create_or_edit}.section_4.title"), rows: section_4_tasks },
     ]
   end
 
 private
+
+  def create_or_edit
+    @form.live? ? "edit" : "create"
+  end
 
   def section_1_tasks
     question_path = if @form.pages.any?
@@ -33,23 +37,23 @@ private
                       type_of_answer_new_path(@form.id)
                     end
     [
-      { task_name: I18n.t("forms.task_lists.section_1.change_name"), path: change_form_name_path(@form.id), status: @task_list_statuses.name_status },
-      { task_name: I18n.t("forms.task_lists.section_1.add_or_edit_questions"), path: question_path, status: @task_list_statuses.pages_status },
-      { task_name: I18n.t("forms.task_lists.section_1.declaration"), path: declaration_path(@form.id), status: @task_list_statuses.declaration_status },
-      { task_name: I18n.t("forms.task_lists.section_1.add_what_happens_next"), path: what_happens_next_path(@form.id), status: @task_list_statuses.what_happens_next_status },
+      { task_name: I18n.t("forms.task_list_#{create_or_edit}.section_1.name"), path: change_form_name_path(@form.id), status: @task_list_statuses.name_status },
+      { task_name: I18n.t("forms.task_list_#{create_or_edit}.section_1.questions"), path: question_path, status: @task_list_statuses.pages_status },
+      { task_name: I18n.t("forms.task_list_#{create_or_edit}.section_1.declaration"), path: declaration_path(@form.id), status: @task_list_statuses.declaration_status },
+      { task_name: I18n.t("forms.task_list_#{create_or_edit}.section_1.what_happens_next"), path: what_happens_next_path(@form.id), status: @task_list_statuses.what_happens_next_status },
     ]
   end
 
   def section_2_tasks
-    hint_text = I18n.t("forms.task_lists.section_2.hint_text_html", submission_email: @form.submission_email) if @form.submission_email.present?
-    [{ task_name: I18n.t("forms.task_lists.section_2.set_email"), path: submission_email_form_path(@form.id), hint_text:, status: @task_list_statuses.submission_email_status },
-     { task_name: I18n.t("forms.task_lists.section_2.confirm_email"), path: submission_email_code_path(@form.id), status: @task_list_statuses.confirm_submission_email_status, active: @task_list_statuses.can_enter_submission_email_code }]
+    hint_text = I18n.t("forms.task_list_#{create_or_edit}.section_2.hint_text_html", submission_email: @form.submission_email) if @form.submission_email.present?
+    [{ task_name: I18n.t("forms.task_list_#{create_or_edit}.section_2.email"), path: submission_email_form_path(@form.id), hint_text:, status: @task_list_statuses.submission_email_status },
+     { task_name: I18n.t("forms.task_list_#{create_or_edit}.section_2.confirm_email"), path: submission_email_code_path(@form.id), status: @task_list_statuses.confirm_submission_email_status, active: @task_list_statuses.can_enter_submission_email_code }]
   end
 
   def section_3_tasks
     [
-      { task_name: I18n.t("forms.task_lists.section_3.privacy_policy"), path: privacy_policy_path(@form.id), status: @task_list_statuses.privacy_policy_status },
-      { task_name: I18n.t("forms.task_lists.section_3.contact_details"), path: contact_details_path(@form.id), status: @task_list_statuses.support_contact_details_status },
+      { task_name: I18n.t("forms.task_list_#{create_or_edit}.section_3.privacy_policy"), path: privacy_policy_path(@form.id), status: @task_list_statuses.privacy_policy_status },
+      { task_name: I18n.t("forms.task_list_#{create_or_edit}.section_3.contact_details"), path: contact_details_path(@form.id), status: @task_list_statuses.support_contact_details_status },
     ]
   end
 
@@ -59,7 +63,7 @@ private
     end
 
     [{
-      task_name: I18n.t("forms.task_lists.section_4.make_live"),
+      task_name: I18n.t("forms.task_list_#{create_or_edit}.section_4.make_live"),
       path: @form.ready_for_live? ? make_live_path(@form.id) : "",
       status: @task_list_statuses.make_live_status,
       active: @task_list_statuses.mandatory_tasks_completed?,
