1d49b5125dc1874a7f047fbba6023940f24e19e5
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2023-02-24 16:33:23 +0000

    Add question number to page list

--- spec/components/page_list_component/view_spec.rb
+++ spec/components/page_list_component/view_spec.rb
@@ -3,9 +3,12 @@ require "rails_helper"
 RSpec.describe PageListComponent::View, type: :component do
   let(:pages) { [] }
 
+  before do
+    render_inline(described_class.new(pages:, form_id: 0))
+  end
+
   context "when there are no pages" do
     it "is blank" do
-      render_inline(described_class.new(pages: [], form_id: 0))
       expect(page).not_to have_selector("*")
     end
   end
@@ -13,19 +16,20 @@ RSpec.describe PageListComponent::View, type: :component do
   context "when the form has a single page" do
     let(:pages) { [OpenStruct.new(id: 1, question_text: "Enter your name?")] }
 
+    it "renders the question number" do
+      expect(page).to have_css("dt.govuk-summary-list__key", text: "1")
+    end
+
     it "renders question title" do
-      render_inline(described_class.new(pages:, form_id: 0))
       expect(page).to have_content("Enter your name")
     end
 
     it "renders link" do
-      render_inline(described_class.new(pages:, form_id: 0))
       expect(page).to have_link("Edit")
     end
 
     context "when re-ordering pages feature is enabled", feature_reorder_pages: true do
       it "does not have re-ordering buttons" do
-        render_inline(described_class.new(pages:, form_id: 0))
         expect(page).not_to have_button("Move up")
         expect(page).not_to have_button("Move down")
       end
@@ -35,14 +39,17 @@ RSpec.describe PageListComponent::View, type: :component do
   context "when the form has multiple pages" do
     let(:pages) { [OpenStruct.new(id: 1, question_text: "Enter your name?"), OpenStruct.new(id: 2, question_text: "What is you pet's name?")] }
 
+    it "renders the question numbers" do
+      expect(page).to have_css("dt.govuk-summary-list__key", text: "1")
+      expect(page).to have_css("dt.govuk-summary-list__key", text: "2")
+    end
+
     context "when re-ordering pages feature is enabled", feature_reorder_pages: true do
       it "renders a move up link" do
-        render_inline(described_class.new(pages:, form_id: 0))
         expect(page).to have_button("Move up")
       end
 
       it "renders a move down link" do
-        render_inline(described_class.new(pages:, form_id: 0))
         expect(page).to have_button("Move down")
       end
     end
