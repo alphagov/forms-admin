c063ed423b026fe4004aaca99cd798897d176634
Alistair Laing <aliuk2012@users.noreply.github.com>
2023-04-04 08:56:06 +0100

    add a basic edit users page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,22 @@
 class UsersController < ApplicationController
   def index
-    render template: "users/index", locals: { users: User.all}
+    render template: "users/index", locals: { users: User.all }
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.super_admin_user = user_params[:super_admin_user]
+    @user.save!
+    redirect_to users_path
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(:super_admin_user)
   end
 end
