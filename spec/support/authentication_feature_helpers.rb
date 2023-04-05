5b34862fb6b34e8dd774620cc3217e4626535e44
Tom Iles <tom.iles@engineering.digital.dwp.gov.uk>
2023-04-05 16:46:14 +0100

    Add helpers for testing logins
    As we routes which only super_admins should be able to access, we need
    to test them in request specs.
    
    This commit adds a few helpers for testing different users.
    
    There are a few questions around how we do it but this is a start.
    
    The current User factory is extended with super_admin and some helpers,
    which borrow heavily from here:
    https://github.com/alphagov/content-tagger/blob/79047af5d0ac877fc9ad31ea78bc96d823a8eca0/spec/support/authentication_helpers.rb
    
    Co-authored-by: Alistair Laing <aliuk2012@users.noreply.github.com>

---
+++ spec/support/authentication_feature_helpers.rb
@@ -0,0 +1,30 @@
+module AuthenticationFeatureHelpers
+  def login_as(user)
+    GDS::SSO.test_user = user
+  end
+
+  def editor_user
+    @editor_user ||= FactoryBot.build(:user)
+  end
+
+  def super_admin_user
+    @super_admin_user ||= FactoryBot.build(:user, :super_admin)
+  end
+
+  def login_as_editor_user
+    login_as editor_user
+  end
+
+  def login_as_super_admin_user
+    login_as super_admin_user
+  end
+end
+
+RSpec.configure do |config|
+  config.include AuthenticationFeatureHelpers, type: :request
+  # TODO: This might be useful when we check for permissions in app, not in
+  # signon
+  #   config.before(:each, type: :request) do
+  #     login_as_editor_user
+  #   end
+end
