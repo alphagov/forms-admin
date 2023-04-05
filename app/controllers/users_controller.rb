9a4d3c5a617b782e145a850285162f2da08ed648
Alistair Laing <aliuk2012@users.noreply.github.com>
2023-04-05 10:05:30 +0100

    Add user policy to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,27 @@
 class UsersController < ApplicationController
+  before_action :can_manage_user
+
   def index
     render template: "users/index", locals: { users: User.all }
   end
 
   def edit
-    @user = User.find(params[:id])
+    user
   end
 
   def update
-    @user = User.find(params[:id])
-    @user.update!(user_params)
+    user.update!(user_params)
     redirect_to users_path
   end
 
 private
+  def user
+    @user ||= User.find(params[:id])
+  end
+
+  def can_manage_user
+    authorize User, :can_manage_user?
+  end
 
   def user_params
     params.require(:user).permit(:role)
