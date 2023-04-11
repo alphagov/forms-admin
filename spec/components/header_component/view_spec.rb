bd0337b1fc151340ccf69b15cab2a42cadd7a1d1
Alistair Laing <aliuk2012@users.noreply.github.com>
2023-04-11 16:01:25 +0100

    Update header_component to support users nav link
    Header should display a link to users index page. The component
    is not responsibly for working that out, it is down to the application_helper
    to identify whether a user is responsible for managing users or not.

--- spec/components/header_component/view_spec.rb
+++ spec/components/header_component/view_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe HeaderComponent::View, type: :component do
   describe "default status" do
     before do
       render_inline(described_class.new(is_signed_in: false,
+                                        list_of_users_path: nil,
                                         user_name: nil,
                                         user_profile_link: nil,
                                         signout_link: nil))
@@ -25,6 +26,7 @@ RSpec.describe HeaderComponent::View, type: :component do
   describe "logged in status" do
     before do
       render_inline(described_class.new(is_signed_in: true,
+                                        list_of_users_path: nil,
                                         user_name: "Joe Smith",
                                         user_profile_link: "http://signon.dev.gov.uk",
                                         signout_link: "/auth/gds/sign_out"))
@@ -46,6 +48,7 @@ RSpec.describe HeaderComponent::View, type: :component do
   context "when no profile link or signout in passed in" do
     before do
       render_inline(described_class.new(is_signed_in: true,
+                                        list_of_users_path: nil,
                                         user_name: "Joe Smith",
                                         user_profile_link: nil,
                                         signout_link: nil))
@@ -59,6 +62,32 @@ RSpec.describe HeaderComponent::View, type: :component do
     it "Signout appears without a link" do
       expect(page).not_to have_link(I18n.t("header.sign_out"))
       expect(page).to have_text(I18n.t("header.sign_out"))
+    end
+  end
+
+  context "when user has permission to view list of users" do
+    before do
+      render_inline(described_class.new(is_signed_in: true,
+                                        list_of_users_path: "https://forms.users",
+                                        user_name: "Joe Smith",
+                                        user_profile_link: "http://signon.dev.gov.uk",
+                                        signout_link: "/auth/gds/sign_out"))
+    end
+
+    it "contains the service name" do
+      expect(page).to have_text(I18n.t("header.product_name"))
+    end
+
+    it "contains the link to users pages" do
+      expect(page).to have_link(I18n.t("header.users"), href: "https://forms.users")
+    end
+
+    it "contains the profile link" do
+      expect(page).to have_link("Joe Smith", href: "http://signon.dev.gov.uk")
+    end
+
+    it "contains the sign out link" do
+      expect(page).to have_link(I18n.t("header.sign_out"), href: "/auth/gds/sign_out")
     end
   end
 end
