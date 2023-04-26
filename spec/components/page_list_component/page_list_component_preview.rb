69000cc6a50bb147c78b8c122a8af4e3d81a609b
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2023-04-26 09:08:54 +0100

    Add error styles to conditions component

--- spec/components/page_list_component/page_list_component_preview.rb
+++ spec/components/page_list_component/page_list_component_preview.rb
@@ -28,4 +28,13 @@ class PageListComponent::PageListComponentPreview < ViewComponent::Preview
              OpenStruct.new(id: 3, question_text: "How many pets do you own?", routing_conditions: [])]
     render(PageListComponent::View.new(pages:, form_id: 0))
   end
+
+  def with_pages_and_error_conditions
+    routing_conditions = [(build :condition, id: 1, routing_page_id: 1, check_page_id: 1, answer_value: nil, goto_page_id: nil),
+                          (build :condition, id: 2, routing_page_id: 1, check_page_id: 1, answer_value: "England", goto_page_id: 2)]
+    pages = [OpenStruct.new(id: 1, question_text: "Enter your name", routing_conditions:),
+             OpenStruct.new(id: 2, question_text: "What is your pet's phone number?", routing_conditions:),
+             OpenStruct.new(id: 3, question_text: "How many pets do you own?", routing_conditions: [])]
+    render(PageListComponent::View.new(pages:, form_id: 0))
+  end
 end
