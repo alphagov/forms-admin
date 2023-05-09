254bd179ebd0da4960539fc05436a34443d515ad
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2023-05-09 10:21:04 +0100

    Add validation for user role

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,12 @@ require "gds-sso/lint/user_spec"
 require "rails_helper"
 
 describe User do
+  subject(:user) { described_class.new }
+
+  it "validates" do
+    expect(user.valid?).to be true
+  end
+
   describe "role enum" do
     it "returns a list of roles" do
       expect(described_class.roles.keys).to eq(%w[super_admin editor])
@@ -10,4 +16,12 @@ describe User do
   end
 
   it_behaves_like "a gds-sso user class"
+
+  describe "role" do
+    it "is invalid if blank" do
+      user.role = nil
+
+      expect(user.valid?).to be false
+    end
+  end
 end
