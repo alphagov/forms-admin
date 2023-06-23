444f69f7ad745da242fd4950bd80560ede095aa5
Alistair Laing <aliuk2012@users.noreply.github.com>
2023-06-23 14:40:56 +0100

    Fixes minor typo in comment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -69,7 +69,7 @@ class ApplicationController < ActionController::Base
 
   # Because determining the clients real IP is hard, simply return the first
   # value of the x-forwarded_for, checking it's an IP. This will probably be
-  # enough for out basic monitoring in PaaS
+  # enough for our basic monitoring in PaaS
   def user_ip(forwarded_for = "")
     first_ip_string = forwarded_for.split(",").first
     Regexp.union([Resolv::IPv4::Regex, Resolv::IPv6::Regex]).match(first_ip_string) && first_ip_string
