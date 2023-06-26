39a9224c42ec9305902565a35e1bf4aecf7d9be0
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2023-06-26 16:24:34 +0100

    Use new forms_env setting in HostingEnvironment

--- spec/lib/hosting_environment_spec.rb
+++ spec/lib/hosting_environment_spec.rb
@@ -1,26 +1,53 @@
 require "rails_helper"
 
 RSpec.describe HostingEnvironment do
-  describe ".test_environment?" do
-    let(:paas_environment) { nil }
-    let(:rails_env) { nil }
+  describe "#environment_name" do
+    let(:settings__forms_env) { nil }
 
-    around do |example|
-      ClimateControl.modify PAAS_ENVIRONMENT: paas_environment do
-        example.run
+    before do
+      allow(Settings).to receive(:forms_env).and_return(settings__forms_env)
+    end
+
+    context "when forms_env is set" do
+      let(:settings__forms_env) { "dev" }
+
+      it "returns the value of forms_env" do
+        expect(described_class.environment_name).to eq "dev"
       end
     end
 
-    context "with PAAS_ENVIRONMENT set to dev" do
-      let(:paas_environment) { "dev" }
+    context "when forms_env is not set" do
+      it "returns unknown_environment" do
+        expect(described_class.environment_name).to eq nil
+      end
+    end
+  end
+
+  describe ".test_environment?" do
+    let(:settings__forms_env) { nil }
+
+    before do
+      allow(Settings).to receive(:forms_env).and_return(settings__forms_env)
+    end
+
+    context "with forms_env set to dev" do
+      let(:settings__forms_env) { "dev" }
 
       it "returns true" do
         expect(described_class.test_environment?).to be(true)
       end
     end
 
-    context "with PAAS_ENVIRONMENT set to production" do
-      let(:paas_environment) { "production" }
+    context "with forms_env set to paas_dev" do
+      let(:settings__forms_env) { "paas_dev" }
+
+      it "returns true" do
+        expect(described_class.test_environment?).to be(true)
+      end
+    end
+
+    context "with forms_env set to production" do
+      let(:settings__forms_env) { "production" }
 
       it "returns true" do
         expect(described_class.test_environment?).to be(false)
@@ -28,7 +55,7 @@ RSpec.describe HostingEnvironment do
     end
 
     context "when in local development" do
-      before { allow(Rails.env).to receive(:production?).and_return(false) }
+      let(:settings__forms_env) { "local" }
 
       it "returns true" do
         expect(described_class.test_environment?).to be(true)
@@ -36,9 +63,8 @@ RSpec.describe HostingEnvironment do
     end
 
     context "when in production" do
-      before { allow(Rails.env).to receive(:production?).and_return(true) }
-
       it "returns false" do
+        allow(Rails).to receive(:env).and_return(OpenStruct.new(production?: true))
         expect(described_class.test_environment?).to be(false)
       end
     end
@@ -47,19 +73,17 @@ RSpec.describe HostingEnvironment do
   describe "#friendly_environment_name" do
     before do
       allow(described_class).to receive(:environment_name).and_return(environment_name)
-      allow(described_class).to receive(:local_development?).and_return(is_local_development)
     end
 
     [
-      { environment_name: "unknown_environment", is_local_development: true, expected_key: "local" },
-      { environment_name: "dev", is_local_development: false, expected_key: "dev" },
-      { environment_name: "production", is_local_development: false, expected_key: "production" },
-      { environment_name: "aws-staging", is_local_development: false, expected_key: "aws-staging" },
-      { environment_name: "aws-user-research", is_local_development: false, expected_key: "aws-user-research" },
+      { environment_name: "local", expected_key: "local" },
+      { environment_name: "dev", expected_key: "dev" },
+      { environment_name: "paas_production", expected_key: "production" },
+      { environment_name: "paas_staging", expected_key: "staging" },
+      { environment_name: "user-research", expected_key: "user-research" },
     ].each do |scenario|
-      context "with environment name set to '#{scenario[:environment_name]}' and is_local_development set to '#{scenario[:is_local_development]}" do
+      context "with environment name set to '#{scenario[:environment_name]}'" do
         let(:environment_name) { scenario[:environment_name] }
-        let(:is_local_development) { scenario[:is_local_development] }
 
         it "returns the correct translation" do
           expect(described_class.friendly_environment_name).to eq(I18n.t("environment_names.#{scenario[:expected_key]}"))
@@ -67,9 +91,8 @@ RSpec.describe HostingEnvironment do
       end
     end
 
-    context "with environment name set to an unknown value and rails_env set to production" do
+    context "with environment name set to an unknown value" do
       let(:environment_name) { "some_unknown_environemnt_string" }
-      let(:is_local_development) { false }
 
       it "returns the environment name as default text" do
         expect(described_class.friendly_environment_name).to eq(environment_name)
