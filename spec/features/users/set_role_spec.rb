a2a183f9136b7a183ce694a58aadafa197261c97
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2023-06-29 13:57:27 +0100

    Fix test that was passing with false positive
    The test "A trial user's forms move to their organisations on role
    upgrade" was passing in error; previously the super admin user was
    logged in for the whole test, because with the `mock_gds_sso` Warden
    strategy `login_as` will not clear session data. As the super admin user
    was also a member of test-org, the tests passed.
    
    This commit stops the false positive by putting the super admin user in
    a different organisation to the trial user, and restores the expected
    behaviour by clearing session cookies in the middle of the test. This
    revealed a few other issues with the test, which we also fix in this
    commit.

--- spec/features/users/set_role_spec.rb
+++ spec/features/users/set_role_spec.rb
@@ -10,11 +10,11 @@ describe "Set or change a user's role", type: :feature do
   end
 
   let(:super_admin_user) do
-    build(:user, :with_super_admin, id: 1)
+    build(:user, :with_super_admin, id: 1, organisation_slug: "gds")
   end
 
   let(:trial_user) do
-    build(:user, :with_no_org, :with_trial, id: 2)
+    create(:user, :with_no_org, :with_trial, id: 2)
   end
 
   let(:req_headers) do
@@ -24,8 +24,15 @@ describe "Set or change a user's role", type: :feature do
     }
   end
 
+  let(:post_headers) do
+    {
+      "X-API-Token" => Settings.forms_api.auth_key,
+      "Content-Type" => "application/json",
+    }
+  end
+
   before do
-    Rails.application.load_seed
+    create :organisation, slug: "test-org"
 
     ActiveResource::HttpMock.respond_to do |mock|
       mock.get "/api/v1/forms?org=test-org", req_headers, org_forms.to_json, 200
@@ -42,27 +49,27 @@ describe "Set or change a user's role", type: :feature do
   it "A trial user's forms move to their organisation on role upgrade" do
     ActiveResource::HttpMock.respond_to do |mock|
       mock.get "/api/v1/forms?org=test-org", req_headers, (org_forms + trial_forms).to_json, 200
+      mock.patch "/api/v1/forms/update-org-for-creator?creator_id=2&org=test-org", post_headers, { success: true }.to_json, 204
     end
 
     login_as super_admin_user
-    when_i_change_a_trial_users_role_to_editor
-    login_as trial_user
+    when_i_change_the_trial_users_role_to_editor
+    reset_session!
+
+    login_as trial_user.reload
     then_i_can_see_the_trial_user_forms
     then_i_can_see_the_org_forms
   end
 
 private
 
-  def when_i_change_a_trial_users_role_to_editor
-    visit users_path
-    edit_user_path_re = %r{/users/(?<id>\d+)/edit}
-    edit_user_link = page.find_all(:link, href: edit_user_path_re).sample
-    @user = User.find(edit_user_path_re.match(edit_user_link[:href])[:id])
+  def when_i_change_the_trial_users_role_to_editor
+    visit edit_user_path(trial_user.id)
 
-    edit_user_link.click
     expect(page).to have_css "h1.govuk-heading-l", text: "Edit user"
-    expect(page).to have_text @user.name
+    expect(page).to have_text trial_user.name
 
+    select("Test Org", from: "Organisation")
     choose("Editor")
     click_button "Save"
   end
