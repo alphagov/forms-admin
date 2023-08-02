af2aedfd83337e5cb5e2ad660c13843cd2afcc34
Tom Iles <tom.iles@engineering.digital.dwp.gov.uk>
2023-08-02 11:25:37 +0100

    Add versions table
    Run paper_trail migrations
    
    ```
    bundle exec rails generate paper_trail:install --with-changes
    ```
    
    The object and object changes columns have been changed to jsonb as
    suggested in the paper_trail readme to store changes in JSON rather than
    YAML.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_26_091901) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_02_102230) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,6 +59,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_26_091901) do
     t.string "provider"
     t.index ["organisation_id"], name: "index_users_on_organisation_id"
     t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
+  end
+
+  create_table "versions", force: :cascade do |t|
+    t.string "item_type", null: false
+    t.bigint "item_id", null: false
+    t.string "event", null: false
+    t.string "whodunnit"
+    t.jsonb "object"
+    t.datetime "created_at"
+    t.jsonb "object_changes"
+    t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id"
   end
 
   add_foreign_key "users", "organisations"
