31d28d5f824ba48d4e70bcf1c2196fdb638cb37b
Alistair Laing <aliuk2012@users.noreply.github.com>
2023-08-04 12:04:20 +0100

    Save markdown and page heading to session
    This commit adds another action to the form and based on which
    action the user clicks on, they will either be taking back to the page
    and shown a preview or their data will be saved in session and taking back
    to the new pages page.

--- spec/requests/pages/additional_guidance_controller_spec.rb
+++ spec/requests/pages/additional_guidance_controller_spec.rb
@@ -3,6 +3,8 @@ require "rails_helper"
 RSpec.describe Pages::AdditionalGuidanceController, type: :request do
   let(:form) { build :form, id: 1 }
   let(:pages) { build_list :page, 5, form_id: form.id }
+  let(:page_heading) { "Page heading" }
+  let(:additional_guidance_markdown) { "## Heading level 2" }
 
   let(:req_headers) do
     {
@@ -46,28 +48,63 @@ RSpec.describe Pages::AdditionalGuidanceController, type: :request do
   end
 
   describe "#create" do
+    let(:route_to) { "preview" }
+
     before do
       ActiveResource::HttpMock.respond_to do |mock|
         mock.get "/api/v1/forms/1", req_headers, form.to_json, 200
         mock.get "/api/v1/forms/1/pages", req_headers, pages.to_json, 200
       end
-      post additional_guidance_new_path(form_id: form.id), params: { pages_additional_guidance_form: { page_heading: "Page heading", additional_guidance_markdown: "## Heading level 2" } }
+      post additional_guidance_new_path(form_id: form.id), params: { pages_additional_guidance_form: { page_heading:, additional_guidance_markdown: }, route_to: }
     end
 
-    it "reads the existing form" do
-      expect(form).to have_been_read
+    context "when previewing markdown" do
+      let(:route_to) { "preview" }
+
+      it "reads the existing form" do
+        expect(form).to have_been_read
+      end
+
+      it "renders the template" do
+        expect(response).to have_rendered("pages/additional_guidance")
+      end
+
+      it "returns 200" do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it "renders the additional guidance markdown as html" do
+        expect(response.body).to include('<h2 class="govuk-heading-l">Heading level 2</h2>')
+      end
     end
 
-    it "renders the template" do
-      expect(response).to have_rendered("pages/additional_guidance")
-    end
+    context "when saving markdown" do
+      let(:route_to) { "save_and_continue" }
 
-    it "returns 200" do
-      expect(response).to have_http_status(:ok)
-    end
+      it "reads the existing form" do
+        expect(form).to have_been_read
+      end
 
-    it "renders the additional guidance markdown as html" do
-      expect(response.body).to include('<h2 class="govuk-heading-l">Heading level 2</h2>')
+      it "saves the page_heading and additional_guidance_markdown to session" do
+        expect(session[:page][:page_heading]).to eq("Page heading")
+        expect(session[:page][:additional_guidance_markdown]).to eq("## Heading level 2")
+      end
+
+      it "redirects the user to the new question page" do
+        expect(response).to redirect_to new_page_path(form.id)
+      end
+
+      context "when data is invalid" do
+        let(:page_heading) { nil }
+
+        it "returns 422" do
+          expect(response).to have_http_status(:unprocessable_entity)
+        end
+
+        it "renders the template" do
+          expect(response).to have_rendered("pages/additional_guidance")
+        end
+      end
     end
   end
 end
