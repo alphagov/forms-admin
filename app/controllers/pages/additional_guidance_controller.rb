3890ff1411705c440d9781c9413337971063dead
Alistair Laing <aliuk2012@users.noreply.github.com>
2023-08-15 16:50:11 +0100

    Dynamically create the post url needed for additional guidance

--- app/controllers/pages/additional_guidance_controller.rb
+++ app/controllers/pages/additional_guidance_controller.rb
@@ -5,7 +5,7 @@ class Pages::AdditionalGuidanceController < PagesController
     page_heading = session.dig(:page, "page_heading")
     additional_guidance_markdown = session.dig(:page, "additional_guidance_markdown")
     additional_guidance_form = Pages::AdditionalGuidanceForm.new(page_heading:, additional_guidance_markdown:)
-    render "pages/additional_guidance", locals: view_locals(additional_guidance_form)
+    render "pages/additional_guidance", locals: view_locals(nil, additional_guidance_form)
   end
 
   def create
@@ -13,12 +13,12 @@ class Pages::AdditionalGuidanceController < PagesController
 
     case route_to
     when :preview
-      render "pages/additional_guidance", locals: view_locals(additional_guidance_form)
+      render "pages/additional_guidance", locals: view_locals(nil, additional_guidance_form)
     when :save_and_continue
       if additional_guidance_form.submit(session)
         redirect_to new_page_path(@form)
       else
-        render "pages/additional_guidance", locals: view_locals(additional_guidance_form), status: :unprocessable_entity
+        render "pages/additional_guidance", locals: view_locals(nil, additional_guidance_form), status: :unprocessable_entity
       end
     end
   end
@@ -29,7 +29,7 @@ class Pages::AdditionalGuidanceController < PagesController
     additional_guidance_form = Pages::AdditionalGuidanceForm.new(page_heading: page.page_heading,
                                                                  additional_guidance_markdown: page.additional_guidance_markdown)
 
-    render "pages/additional_guidance", locals: view_locals(additional_guidance_form)
+    render "pages/additional_guidance", locals: view_locals(page, additional_guidance_form)
   end
 
   def update
@@ -37,12 +37,12 @@ class Pages::AdditionalGuidanceController < PagesController
 
     case route_to
     when :preview
-      render "pages/additional_guidance", locals: view_locals(additional_guidance_form)
+      render "pages/additional_guidance", locals: view_locals(page, additional_guidance_form)
     when :save_and_continue
       if additional_guidance_form.submit(session)
         redirect_to edit_page_path(@form.id, page.id)
       else
-        render "pages/additional_guidance", locals: view_locals(additional_guidance_form), status: :unprocessable_entity
+        render "pages/additional_guidance", locals: view_locals(page, additional_guidance_form), status: :unprocessable_entity
       end
     end
   end
@@ -63,7 +63,8 @@ private
     GovukFormsMarkdown.render(guidance_form.additional_guidance_markdown)
   end
 
-  def view_locals(guidance_form)
-    { form: @form, page: @page, additional_guidance_form: guidance_form, preview_html: preview_html(guidance_form) }
+  def view_locals(current_page, guidance_form)
+    post_url = current_page.present? && current_page.id.present? ? additional_guidance_edit_path : additional_guidance_new_path
+    { form: @form, page: @page, additional_guidance_form: guidance_form, preview_html: preview_html(guidance_form), post_url: }
   end
 end
