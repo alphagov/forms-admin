7a48f739dd90b245a42379363f4fb706856f5ca7
Samuel Culley <samuel.culley@digital.cabinet-office.gov.uk>
2023-08-24 12:00:00 +0100

    Rename field to guidance_markdown
    Renaming the page field additional_guidance_markdown to guidance_markdown for brevity. This change is also reflected in the API as well.

--- app/forms/pages/additional_guidance_form.rb
+++ app/forms/pages/additional_guidance_form.rb
@@ -1,13 +1,13 @@
 class Pages::AdditionalGuidanceForm < BaseForm
-  attr_accessor :page_heading, :additional_guidance_markdown
+  attr_accessor :page_heading, :guidance_markdown
 
-  validates :page_heading, :additional_guidance_markdown, presence: true
+  validates :page_heading, :guidance_markdown, presence: true
 
   def submit(session)
     return false if invalid?
 
     session[:page] = {} if session[:page].blank?
     session[:page][:page_heading] = page_heading
-    session[:page][:additional_guidance_markdown] = additional_guidance_markdown
+    session[:page][:guidance_markdown] = guidance_markdown
   end
 end
--- spec/forms/pages/additional_guidance_form_spec.rb
+++ spec/forms/pages/additional_guidance_form_spec.rb
@@ -1,9 +1,9 @@
 require "rails_helper"
 
 RSpec.describe Pages::AdditionalGuidanceForm, type: :model do
-  let(:additional_guidance_form) { described_class.new(page_heading:, additional_guidance_markdown:) }
+  let(:additional_guidance_form) { described_class.new(page_heading:, guidance_markdown:) }
   let(:page_heading) { nil }
-  let(:additional_guidance_markdown) { nil }
+  let(:guidance_markdown) { nil }
 
   describe "validations" do
     it "is invalid if page heading is nil" do
@@ -13,11 +13,11 @@ RSpec.describe Pages::AdditionalGuidanceForm, type: :model do
       expect(additional_guidance_form.errors.full_messages_for(:page_heading)).to include("Page heading #{error_message}")
     end
 
-    it "is invalid if additional_guidance_markdown is nil" do
-      error_message = I18n.t("activemodel.errors.models.pages/additional_guidance_form.attributes.additional_guidance_markdown.blank")
-      additional_guidance_form.additional_guidance_markdown = nil
+    it "is invalid if guidance_markdown is nil" do
+      error_message = I18n.t("activemodel.errors.models.pages/additional_guidance_form.attributes.guidance_markdown.blank")
+      additional_guidance_form.guidance_markdown = nil
       expect(additional_guidance_form).to be_invalid
-      expect(additional_guidance_form.errors.full_messages_for(:additional_guidance_markdown)).to include("Additional guidance markdown #{error_message}")
+      expect(additional_guidance_form.errors.full_messages_for(:guidance_markdown)).to include("Guidance markdown #{error_message}")
     end
   end
 
@@ -29,18 +29,18 @@ RSpec.describe Pages::AdditionalGuidanceForm, type: :model do
       expect(additional_guidance_form.errors.any?).to eq true
     end
 
-    context "when page_heading and additional_guidance_markdown are valid" do
+    context "when page_heading and guidance_markdown are valid" do
       let(:page_heading) { "My new page heading" }
-      let(:additional_guidance_markdown) { "Extra guidance needed to answer this question" }
+      let(:guidance_markdown) { "Extra guidance needed to answer this question" }
 
       it "sets a session key called 'page' as a hash with the page heading in it" do
         additional_guidance_form.submit(session_mock)
         expect(session_mock[:page][:page_heading]).to eq page_heading
       end
 
-      it "sets a session key called 'page' as a hash with the additional_guidance_markdown in it" do
+      it "sets a session key called 'page' as a hash with the guidance_markdown in it" do
         additional_guidance_form.submit(session_mock)
-        expect(session_mock[:page][:additional_guidance_markdown]).to eq additional_guidance_markdown
+        expect(session_mock[:page][:guidance_markdown]).to eq guidance_markdown
       end
     end
   end
