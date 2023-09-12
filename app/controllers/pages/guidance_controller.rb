be1b2d2f2e607ffcff2b1ed17ebfc5740c5a20f1
Alistair Laing <aliuk2012@users.noreply.github.com>
2023-09-12 15:00:00 +0100

    Fix back link on the "add guidance" page
    The back link on the "Add guidance" page was taking
    users back to the list of questions page rather than
    back to the previous "Add/Edit Question" page

--- app/controllers/pages/guidance_controller.rb
+++ app/controllers/pages/guidance_controller.rb
@@ -5,21 +5,23 @@ class Pages::GuidanceController < PagesController
     page_heading = session.dig(:page, "page_heading")
     guidance_markdown = session.dig(:page, "guidance_markdown")
     guidance_form = Pages::GuidanceForm.new(page_heading:, guidance_markdown:)
-    render "pages/guidance", locals: view_locals(nil, guidance_form)
+    back_link = new_page_path(@form)
+    render "pages/guidance", locals: view_locals(nil, guidance_form, back_link)
   end
 
   def create
     guidance_form = Pages::GuidanceForm.new(guidance_form_params)
+    back_link = new_page_path(@form)
 
     case route_to
     when :preview
       guidance_form.valid?
-      render "pages/guidance", locals: view_locals(nil, guidance_form)
+      render "pages/guidance", locals: view_locals(nil, guidance_form, back_link)
     when :save_and_continue
       if guidance_form.submit(session)
         redirect_to new_page_path(@form)
       else
-        render "pages/guidance", locals: view_locals(nil, guidance_form), status: :unprocessable_entity
+        render "pages/guidance", locals: view_locals(nil, guidance_form, back_link), status: :unprocessable_entity
       end
     end
   end
@@ -29,22 +31,24 @@ class Pages::GuidanceController < PagesController
 
     guidance_form = Pages::GuidanceForm.new(page_heading: page.page_heading,
                                             guidance_markdown: page.guidance_markdown)
+    back_link = edit_page_path(@form, page)
 
-    render "pages/guidance", locals: view_locals(page, guidance_form)
+    render "pages/guidance", locals: view_locals(page, guidance_form, back_link)
   end
 
   def update
     guidance_form = Pages::GuidanceForm.new(guidance_form_params)
+    back_link = edit_page_path(@form, page.id)
 
     case route_to
     when :preview
       guidance_form.valid?
-      render "pages/guidance", locals: view_locals(page, guidance_form)
+      render "pages/guidance", locals: view_locals(page, guidance_form, back_link)
     when :save_and_continue
       if guidance_form.submit(session)
         redirect_to edit_page_path(@form.id, page.id)
       else
-        render "pages/guidance", locals: view_locals(page, guidance_form), status: :unprocessable_entity
+        render "pages/guidance", locals: view_locals(page, guidance_form, back_link), status: :unprocessable_entity
       end
     end
   end
@@ -71,8 +75,8 @@ private
     GovukFormsMarkdown.render(guidance_form.guidance_markdown)
   end
 
-  def view_locals(current_page, guidance_form)
+  def view_locals(current_page, guidance_form, back_link)
     post_url = current_page.present? && current_page.id.present? ? guidance_edit_path : guidance_new_path
-    { form: @form, page: @page, guidance_form:, preview_html: preview_html(guidance_form), post_url: }
+    { form: @form, page: @page, guidance_form:, preview_html: preview_html(guidance_form), post_url:, back_link: }
   end
 end
