584e1eaf8df079dcfb0ee2cb83fb9191b623e354
Alistair Laing <aliuk2012@users.noreply.github.com>
2023-10-04 07:20:55 +0100

    Reduce length of string being generated by faker
    if Faker produces longer strings than the attributes support, this can
    lead to flickering spec failures due to failed validations.
    
    https://github.com/alphagov/forms-admin/actions/runs/6392922742/job/17351148046#step:10:17
    
    https://github.com/alphagov/forms-admin/actions/runs/6396055196/job/17361064124#step:10:17

--- spec/factories/models/draft_questions.rb
+++ spec/factories/models/draft_questions.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     form_id { 1 }
     user { nil }
     sequence(:page_id) { |n| n }
-    question_text { Faker::Lorem.question }
+    question_text { Faker::Lorem.question.truncate(250) }
     hint_text { nil }
     is_optional { false }
     answer_type { Page::ANSWER_TYPES.reject { |item| Page::ANSWER_TYPES_WITH_SETTINGS.include? item }.sample }
@@ -12,11 +12,11 @@ FactoryBot.define do
     answer_settings { {} }
 
     trait :with_hints do
-      hint_text { Faker::Quote.yoda }
+      hint_text { Faker::Quote.yoda.truncate(500) }
     end
 
     trait :with_guidance do
-      page_heading { Faker::Quote.yoda }
+      page_heading { Faker::Quote.yoda.truncate(250) }
       guidance_markdown { "## List of items \n\n\n #{Faker::Markdown.ordered_list}" }
     end
   end
