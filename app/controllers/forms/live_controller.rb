c3e5d663df3ec0f2508de6c8b3220d3e16b68bee
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2023-10-10 11:27:09 +0100

    Add call to CloudWatch
    Adds a call to the CloudWatch API (using the CloudWatchService class) to get the weekly submissions.
    Also adds error handling in case the request fails.

--- app/controllers/forms/live_controller.rb
+++ app/controllers/forms/live_controller.rb
@@ -10,21 +10,24 @@ class Forms::LiveController < Forms::BaseController
     render template: "live/show_pages", locals: { form: current_live_form }
   end
 
-private
-
-  def current_live_form
-    Form.find_live(params[:form_id])
-  end
-
   def metrics_data
+    # If the form went live today, there won't be any metrics to show
     today = Time.zone.today
-    form_is_new = (today.to_date - Time.zone.parse(current_form.live_at).to_date).to_i < 1
-    # TODO: fetch real data from CloudWatch
-    weekly_submissions = 1025
+    form_is_new = Time.zone.parse(current_live_form.live_at.to_s) >= today.midnight
+
+    weekly_submissions = form_is_new ? 0 : CloudWatchService.week_submissions(form_id: current_live_form.id)
 
     {
       weekly_submissions:,
       form_is_new:,
     }
+  rescue Aws::CloudWatch::Errors::ServiceError
+    nil
+  end
+
+private
+
+  def current_live_form
+    Form.find_live(params[:form_id])
   end
 end
