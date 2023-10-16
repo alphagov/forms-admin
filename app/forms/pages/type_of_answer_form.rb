f7a9066fe59ba528808cafee235028b59435f183
Alistair Laing <aliuk2012@users.noreply.github.com>
2023-10-16 11:42:51 +0100

    Set DraftQuestion answer_type
    Currently we are adding answer type to draft_question
    and will later stop it from using session
    altogether.

--- app/forms/pages/type_of_answer_form.rb
+++ app/forms/pages/type_of_answer_form.rb
@@ -1,11 +1,17 @@
 class Pages::TypeOfAnswerForm < BaseForm
-  attr_accessor :answer_type
+  attr_accessor :answer_type, :draft_question
 
+  validates :draft_question, presence: true
   validates :answer_type, presence: true, inclusion: { in: Page::ANSWER_TYPES }
 
   def submit(session)
     return false if invalid?
 
+    # TODO: remove this once we have draft_questions being saved across the whole journey
     session[:page] = { answer_type:, answer_settings: nil }
+
+    draft_question.answer_type = answer_type
+    draft_question.answer_settings = nil
+    draft_question.save!(validate: false)
   end
 end
