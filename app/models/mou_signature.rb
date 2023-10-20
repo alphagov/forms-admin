e59aed6673d9b7e4e21b789251ada3d53d470208
chao-xian <kelvin.gan@digital.cabinet-office.gov.uk>
2023-10-20 16:28:09 +0100

    Update method name from update to add prefix
    This method only adds orgs to MouSignatures that don't have one. The name mislead me before.

--- app/models/mou_signature.rb
+++ app/models/mou_signature.rb
@@ -2,7 +2,7 @@ class MouSignature < ApplicationRecord
   belongs_to :user
   belongs_to :organisation, optional: true
 
-  def self.update_user_orgs(user)
+  def self.add_user_orgs(user)
     MouSignature.where(user:, organisation: nil).update_all(organisation_id: user.organisation.id)
   end
 end
