03866327d5a2622661caef4139a4abe5834ce40f
Tom Iles <tom.iles@engineering.digital.dwp.gov.uk>
2023-11-01 16:08:47 +0000

    Add routes, controller, locales for mou_signature
    We need to allow users to sign MOUs.
    
    This commit adds the views and controller actions to allow users to
    agree to an MOU.
    
    A single MOU can be signed by a user with any role, whether they have an
    organisation set or not.
    
    MOU's created without an organisation will have it set when the user is
    given an organisation.

---
+++ spec/features/mou/upgrade_user_changes_mou_spec.rb
@@ -0,0 +1,70 @@
+require "rails_helper"
+
+describe "Assign an organisation to a user with a signed MOU", type: :feature do
+  let(:user) { create :user, :with_trial_role, name: "Test User" }
+  let(:mou_signature) { create(:mou_signature, user:, organisation: nil, created_at: Time.zone.parse("September 1, 2023")) }
+  let(:organisation) { create :organisation }
+
+  let(:req_headers) do
+    {
+      "X-API-Token" => Settings.forms_api.auth_key,
+      "Accept" => "application/json",
+    }
+  end
+
+  let(:post_headers) do
+    {
+      "X-API-Token" => Settings.forms_api.auth_key,
+      "Content-Type" => "application/json",
+    }
+  end
+
+  before do
+    ActiveResource::HttpMock.respond_to do |mock|
+      mock.get "/api/v1/forms?creator_id=#{super_admin_user.id}", req_headers, [].to_json, 200
+    end
+
+    mou_signature
+  end
+
+  it "a logged in user can sign an MOU" do
+    login_as_super_admin_user
+    when_i_visit_the_mou_index_page
+    then_i_can_see_the_mou_page
+    then_i_see_the_mou_with_no_organisation
+    then_i_visit_the_users_page
+    then_i_update_the_user_organisation
+    when_i_visit_the_mou_index_page
+    then_i_can_see_the_mou_page
+    then_i_see_the_mou_with_organisation
+  end
+
+private
+
+  def when_i_visit_the_mou_index_page
+    visit mou_signatures_path
+  end
+
+  def then_i_can_see_the_mou_page
+    expect(page).to have_text "Memorandum of Understanding agreements"
+  end
+
+  def then_i_see_the_mou_with_no_organisation
+    expect(page).to have_text mou_signature.user.name
+    expect(page).to have_text "No organisation"
+  end
+
+  def then_i_visit_the_users_page
+    click_link mou_signature.user.name
+  end
+
+  def then_i_update_the_user_organisation
+    fill_in "Organisation", with: "#{organisation.name}\n"
+    click_button "Save"
+  end
+
+  def then_i_see_the_mou_with_organisation
+    expect(page).to have_text mou_signature.user.name
+    expect(page).to have_text organisation.name
+  end
+end
---
+++ spec/requests/mou_signatures_controller_spec.rb
@@ -0,0 +1,79 @@
+require "rails_helper"
+
+RSpec.describe MouSignaturesController, type: :request do
+  before do
+    login_as_editor_user
+  end
+
+  context "when the user has not signed the memorandum of understanding" do
+    describe "#show" do
+      it "redirects to the new route" do
+        get mou_signature_url
+        expect(response).to redirect_to(new_mou_signature_url)
+      end
+    end
+
+    describe "#new" do
+      it "returns http success" do
+        get new_mou_signature_url
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    describe "#confirmation" do
+      it "redirects to new_mou_signature_url" do
+        get confirmation_mou_signature_url
+        expect(response).to redirect_to(new_mou_signature_url)
+      end
+    end
+
+    describe "#create" do
+      it "redirects to the confirmation page" do
+        post mou_signature_url, params: {
+          mou_signature: {
+            agreed: "1",
+          },
+        }
+        expect(response).to redirect_to(confirmation_mou_signature_url)
+      end
+    end
+  end
+
+  context "when the user has already signed the memorandum of understanding" do
+    before do
+      create(:mou_signature, user: editor_user, organisation: editor_user.organisation)
+    end
+
+    describe "#show" do
+      it "returns http success" do
+        get mou_signature_url
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    describe "#new" do
+      it "redirects to the show page" do
+        get new_mou_signature_url
+        expect(response).to redirect_to(mou_signature_url)
+      end
+    end
+
+    describe "#confirmation" do
+      it "redirects to the show page" do
+        get confirmation_mou_signature_url
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    describe "#create" do
+      it "redirects to the show page" do
+        post mou_signature_url, params: {
+          mou_signature: {
+            agreed: "1",
+          },
+        }
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
+end
