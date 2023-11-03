881fc48c7ca207991163d7efb85687c7db0049cc
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2023-11-03 15:51:13 +0000

    Fix build_title optional text check
    The logic in this method was wrong - we never want selection questions
    displaying as optional, *even if* they've got 'None of the above'
    enabled. But the current logic will always show the '(optional)' text
    for selection questions, whether or not they've got 'None of the
    above' enabled.

--- spec/service/page_summary_card_data_service_spec.rb
+++ spec/service/page_summary_card_data_service_spec.rb
@@ -1,8 +1,8 @@
 require "rails_helper"
 
 describe PageSummaryCardDataService do
-  let(:page) { build :page, is_optional: optional }
-  let(:optional) { false }
+  let(:page) { build :page, is_optional: }
+  let(:is_optional) { false }
   let(:pages) do
     form = build :form, :with_pages
     form.pages
@@ -22,19 +22,43 @@ describe PageSummaryCardDataService do
       expect(service.build_data[:rows]).to eq [1, 2]
     end
 
+    context "when the page is a selection question" do
+      let(:page) { build :page, :with_selections_settings, is_optional: }
+
+      it "includes a title without (optional) added to it" do
+        expect(service.build_data[:card][:title]).to eq page.question_text.to_s
+      end
+    end
+
     context "when question is optional" do
-      let(:optional) { "true" }
+      let(:is_optional) { "true" }
 
       it "includes a title with (optional) added to it" do
         expect(service.build_data[:card][:title]).to eq "#{page.question_text} (optional)"
       end
+
+      context "when the page is a selection question" do
+        let(:page) { build :page, :with_selections_settings, is_optional: }
+
+        it "includes a title without (optional) added to it" do
+          expect(service.build_data[:card][:title]).to eq page.question_text.to_s
+        end
+      end
     end
 
     context "when question is optional is nil" do
-      let(:optional) { nil }
+      let(:is_optional) { nil }
 
       it "includes a title" do
         expect(service.build_data[:card][:title]).to eq page.question_text
+      end
+
+      context "when the page is a selection question" do
+        let(:page) { build :page, :with_selections_settings, is_optional: }
+
+        it "includes a title without (optional) added to it" do
+          expect(service.build_data[:card][:title]).to eq page.question_text.to_s
+        end
       end
     end
   end
