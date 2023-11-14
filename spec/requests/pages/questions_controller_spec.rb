alphagov/forms-admin

commit c9541b3e0bed7820abacd5bc8089245124f43917

QuestionsController actions should now all be refering to draft_question instead of sessions

app/controllers/pages/questions_controller.rb
@@ -3,7 +3,7 @@ class Pages::QuestionsController < PagesController
     answer_type = draft_question.answer_type
     question_text = session.dig(:page, :question_text)
     answer_settings = draft_question.answer_settings
-    is_optional = session.dig(:page, :is_optional) == "true"
+    is_optional = draft_question.is_optional == "true"
     page_heading = draft_question.page_heading
     guidance_markdown = draft_question.guidance_markdown
     @question_form = Pages::QuestionForm.new(form_id: current_form.id, answer_type:, question_text:, answer_settings:, is_optional:, draft_question:)
@@ -13,12 +13,14 @@ class Pages::QuestionsController < PagesController
   end
 
   def create
-    answer_settings = session.dig(:page, :answer_settings)
-    page_heading = draft_question.page_heading
-    guidance_markdown = draft_question.guidance_markdown
-
-    @question_form = Pages::QuestionForm.new(page_params.merge(answer_settings:, page_heading:, guidance_markdown:, draft_question:))
-    @page = Page.new(page_params.merge(answer_settings:, page_heading:, guidance_markdown:, answer_type: @question_form.draft_question.answer_type))
+    @question_form = Pages::QuestionForm.new(page_params.merge(answer_settings: draft_question.answer_settings,
+                                                               page_heading: draft_question.page_heading,
+                                                               guidance_markdown: draft_question.guidance_markdown,
+                                                               draft_question:))
+    @page = Page.new(page_params.merge(answer_settings: draft_question.answer_settings,
+                                       page_heading: draft_question.page_heading,
+                                       guidance_markdown: draft_question.guidance_markdown,
+                                       answer_type: draft_question.answer_type))
 
     # TODO: Move Page creation to be part of the form submit method
     if @question_form.submit && @page.save
@@ -30,32 +32,25 @@ class Pages::QuestionsController < PagesController
   end
 
   def edit
-    reset_session_if_answer_settings_not_present
-    page.load_from_session(session, %i[answer_settings is_optional])
-
-    page.page_heading = draft_question.page_heading
-    page.guidance_markdown = draft_question.guidance_markdown
-    page.answer_type = draft_question.answer_type
-
     @question_form = Pages::QuestionForm.new(form_id: current_form.id,
-                                             answer_type: page.answer_type,
+                                             answer_type: draft_question.answer_type,
                                              question_text: page.question_text,
-                                             hint_text: page.hint_text,
-                                             is_optional: page.is_optional,
-                                             answer_settings: page.answer_settings)
+                                             hint_text: draft_question.hint_text,
+                                             is_optional: draft_question.is_optional,
+                                             answer_settings: draft_question.answer_settings)
     render :edit, locals: { current_form: }
   end
 
   def update
-    page.load_from_session(session, %i[answer_settings]).load(page_params)
+    page.load(page_params)
     page.page_heading = draft_question.page_heading
     page.guidance_markdown = draft_question.guidance_markdown
     page.answer_type = draft_question.answer_type
     page.answer_settings = draft_question.answer_settings
 
-    @question_form = Pages::QuestionForm.new(page_params.merge(answer_settings: page.answer_settings,
-                                                               page_heading: page.page_heading,
-                                                               guidance_markdown: page.guidance_markdown,
+    @question_form = Pages::QuestionForm.new(page_params.merge(answer_settings: draft_question.answer_settings,
+                                                               page_heading: draft_question.page_heading,
+                                                               guidance_markdown: draft_question.guidance_markdown,
                                                                draft_question:))
 
     # TODO: Move Page creation to be part of the form submit method
@@ -72,16 +67,6 @@ private
   def page_params
     # TODO: Remove current_form from merge once we using draft question properly. the question form shouldn't need to know about form id
     params.require(:pages_question_form).permit(:question_text, :hint_text, :is_optional).merge(form_id: current_form.id)
-  end
-
-  def reset_session_if_answer_settings_not_present
-    answer_type = draft_question.answer_type
-    answer_settings = draft_question.answer_settings
-
-    if (Page::ANSWER_TYPES_WITH_SETTINGS.include? answer_type) && (answer_settings.blank? || answer_settings == {})
-      clear_questions_session_data
-      redirect_to edit_question_path(params[:form_id], params[:page_id])
-    end
   end
 
   def handle_submit_action

spec/requests/pages/questions_controller_spec.rb
@@ -37,7 +37,7 @@ RSpec.describe Pages::QuestionsController, type: :request do
           question_text: "What is your home address?",
           hint_text: "This should be the location stated in your contract.",
           is_optional: nil,
-          answer_settings: nil,
+          answer_settings: {},
           page_heading: nil,
           guidance_markdown: nil,
           answer_type: draft_question.answer_type,

commit 8d807b08d8a8aa888e1f1bf9e282192ad9b5bf2b

Display the information we have in draft question to the user
I discovered that if you added a detailed guidance existing question or edit guidance from existing question. The data would not show up on the "Edit Question page". This was due to a recent refactor and as we had no tests covering that part of the functionality it was not picked up.

This is a temporary fix, ideally once we add the "Check your question" page we will be able to completely rely on draft questions to display the current state to the user. Until then we will copy what we have in draft question over to the page model.

Caused by c9541b3e0bed7820abacd5bc8089245124f43917

app/controllers/pages/questions_controller.rb
@@ -38,6 +38,14 @@ class Pages::QuestionsController < PagesController
                                              hint_text: draft_question.hint_text,
                                              is_optional: draft_question.is_optional,
                                              answer_settings: draft_question.answer_settings)
+
+    # TODO: Remove this once we have a check your question view. The new view should also pull data directly from draft_question instead of through page model
+    page.answer_type = draft_question.answer_type
+    page.answer_settings = draft_question.answer_settings
+    page.is_optional = draft_question.is_optional
+    page.page_heading = draft_question.page_heading
+    page.guidance_markdown = draft_question.guidance_markdown
+
     render :edit, locals: { current_form: }
   end
 

spec/requests/pages/questions_controller_spec.rb
@@ -113,6 +113,8 @@ RSpec.describe Pages::QuestionsController, type: :request do
         }
       end
 
+      let(:draft_question) { create :draft_question, :with_guidance, user: editor_user, form_id: 2, page_id: 1 }
+
       before do
         ActiveResource::HttpMock.respond_to do |mock|
           mock.get "/api/v1/forms/2", req_headers, form_response.to_json, 200
@@ -132,6 +134,22 @@ RSpec.describe Pages::QuestionsController, type: :request do
 
         page_request = ActiveResource::Request.new(:get, "/api/v1/forms/2", {}, req_headers)
         expect(ActiveResource::HttpMock.requests).to include page_request
+      end
+
+      it "renders the detail guidance page_heading and guidance_markdown" do
+        expect(response.body).to include(draft_question.page_heading)
+        expect(response.body).to include(draft_question.guidance_markdown)
+      end
+
+      it "assigns all the draft question attribute values to page object attributes" do
+        page_object = assigns(:page)
+        expect(page_object.attributes).to include(
+          answer_type: draft_question.answer_type,
+          answer_settings: draft_question.answer_settings,
+          is_optional: draft_question.is_optional,
+          page_heading: draft_question.page_heading,
+          guidance_markdown: draft_question.guidance_markdown,
+        )
       end
     end
   end

spec/views/pages/edit.html.erb_spec.rb
@@ -5,7 +5,7 @@ describe "pages/edit.html.erb" do
 
   before do
     # Initialize models
-    page = Page.new(id: 1, question_text:, form_id: 1, answer_type: "email", answer_settings: nil, page_heading: nil)
+    page = Page.new(id: 1, question_text:, form_id: 1, answer_type: "email", answer_settings: nil, page_heading: "Some detailed guidance", guidance_markdown: "This is example of detailed guidance")
     question_form = Pages::QuestionForm.new(page_id: 1, question_text:, form_id: 1, answer_type: "email", answer_settings: nil)
     form = Form.new(id: 1, name: "Form 1", form_id: 1, pages: [page])
     current_user = OpenStruct.new(uid: "123456")
@@ -35,6 +35,10 @@ describe "pages/edit.html.erb" do
     ActionView::Base.default_form_builder = GOVUKDesignSystemFormBuilder::FormBuilder
 
     render template: "pages/edit"
+  end
+
+  it "contains existing detailed Guidance" do
+    expect(rendered).to have_text("Some detailed guidance")
   end
 
   context "when given a title with characters which need escaping" do