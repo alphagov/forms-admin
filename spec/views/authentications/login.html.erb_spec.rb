0b1558306a3bd1b514637868e132d91025c9ee99
Tom Iles <tom.iles@engineering.digital.dwp.gov.uk>
2023-11-30 11:38:22 +0000

    Add view tests to the page

--- spec/views/authentications/login.html.erb_spec.rb
+++ spec/views/authentications/login.html.erb_spec.rb
@@ -1,1 +1,19 @@
 require "rails_helper"
+
+describe "authentications/login.html.erb" do
+  before do
+    render template: "authentications/login"
+  end
+
+  it "has the correct title" do
+    expect(view.content_for(:title)).to have_content(t("page_titles.login"))
+  end
+
+  it "has javascript" do
+    expect(view.content_for(:body_end)).to have_selector("script", visible: :all)
+  end
+
+  it "has login form" do
+    expect(rendered).to have_selector("form[action=\"/auth/#{Settings.auth_provider}/\"]")
+  end
+end
