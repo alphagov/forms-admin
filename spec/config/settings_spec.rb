96f7a01e3764167141d2cd6195a679785206c4f2
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2023-12-07 13:22:12 +0000

    Remove email_confirmations_enabled feature flag
    This feature is enabled in all of our environments, and we've seen from
    our analytics that it's very popular, so we can safely remove the
    feature flag.

--- spec/config/settings_spec.rb
+++ spec/config/settings_spec.rb
@@ -22,7 +22,6 @@ describe "Settings" do
     features = settings[:features]
 
     include_examples expected_value_test, :metrics_for_form_creators_enabled, features, false
-    include_examples expected_value_test, :email_confirmations_enabled, features, false
   end
 
   describe "forms_api" do
