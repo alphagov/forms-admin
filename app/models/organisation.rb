1c7521e7130b611fe74906285d21b1a531507f2c
Tom Iles <tom.iles@engineering.digital.dwp.gov.uk>
2023-12-20 16:10:57 +0000

    Add new scope to organisation

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -5,4 +5,6 @@ class Organisation < ApplicationRecord
   has_many :users
 
   has_many :mou_signatures
+
+  scope :with_users, -> { where(User.where("organisation_id = organisations.id").arel.exists) }
 end
