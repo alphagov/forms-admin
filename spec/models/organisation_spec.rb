8fc630443904432cbb74ddfda07064777aa0f43d
Tom Iles <tom.iles@engineering.digital.dwp.gov.uk>
2023-12-21 09:27:50 +0000

    Add extra org to scope tests

--- spec/models/organisation_spec.rb
+++ spec/models/organisation_spec.rb
@@ -31,6 +31,7 @@ RSpec.describe Organisation, type: :model do
   describe "scopes" do
     describe ".with_users" do
       it "returns organisations with distinct users" do
+        FactoryBot.create(:organisation, slug: "org_3")
         organisation2 = FactoryBot.create(:organisation, slug: "org_2")
         organisation1 = FactoryBot.create(:organisation, slug: "org_1")
 
@@ -40,9 +41,7 @@ RSpec.describe Organisation, type: :model do
 
         organisations_with_users = described_class.with_users
 
-        expect(organisations_with_users.first).to eq(organisation1)
-        expect(organisations_with_users.last).to eq(organisation2)
-        expect(organisations_with_users.size).to eq(2)
+        expect(organisations_with_users).to eq([organisation1, organisation2])
       end
     end
   end
