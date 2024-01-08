01946c41184283c14d30f740a979e95aeb3cb466
Tom Iles <tom.iles@engineering.digital.dwp.gov.uk>
2024-01-08 15:43:15 +0000

    Add feature test for form search autocomplete

---
+++ spec/features/form/super_admin_view_forms_spec.rb
@@ -0,0 +1,43 @@
+require "rails_helper"
+
+feature "View forms", type: :feature do
+  let(:org_forms) { [build(:form, id: 1, name: "Org form")] }
+  let(:other_org) { create :organisation, id: 2, slug: "Other org" }
+  let(:other_org_user) { create :user, organisation: other_org }
+  let(:other_org_forms) { [build(:form, id: 2, organisation_id: other_org.id, name: "Other org form")] }
+  # let(:other_org_forms) { [build (:form, id: 1, name: "Apply for a juggling license", organisation_id: other_org.id)] }
+
+  let(:req_headers) do
+    {
+      "X-API-Token" => Settings.forms_api.auth_key,
+      "Accept" => "application/json",
+    }
+  end
+
+  before do
+    ActiveResource::HttpMock.respond_to do |mock|
+      mock.get "/api/v1/forms?organisation_id=#{super_admin_user.organisation_id}", req_headers, org_forms.to_json, 200
+      mock.get "/api/v1/forms?organisation_id=#{other_org.id}", req_headers, other_org_forms.to_json, 200
+    end
+
+    # Orgs only show in the autocomplete if they have at least one user
+    other_org_user
+    login_as_super_admin_user
+  end
+
+  scenario "Super admin can use autcomplete to view other org's forms" do
+    visit root_path
+    when_i_change_the_organisation
+    then_i_should_see_the_forms_for_that_organisation
+  end
+
+  def when_i_change_the_organisation
+    fill_in "search-organisation-id-field", with: other_org.name.to_s
+    page.send_keys :enter
+    click_button "Change"
+  end
+
+  def then_i_should_see_the_forms_for_that_organisation
+    expect(page).to have_content(other_org_forms.first.name)
+  end
+end
