f36871731a75f3bc088d01bb9d349308d9528e2f
Alistair Laing <aliuk2012@users.noreply.github.com>
2024-01-11 07:40:27 +0000

    add back link to check your question page

--- app/controllers/pages/check_your_question_controller.rb
+++ app/controllers/pages/check_your_question_controller.rb
@@ -1,5 +1,6 @@
 class Pages::CheckYourQuestionController < PagesController
   def show
+    @back_link_url = new_question_path(current_form)
     render "pages/check_your_question", locals: { current_form:, draft_question: }
   end
 end
