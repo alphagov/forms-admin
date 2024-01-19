ce0f89a417132094c19f0f2efd4c8c644d9c21e5
Jamie Wilkinson <jamie.wilkinson@digital.cabinet-office.gov.uk>
2024-01-19 15:32:45 +0000

    Use Sentry:TestHelper for Sentry test setup/teardown
    It seems as if changes introduced by getsentry/sentry-ruby#2206
    were interfering with Sentry test teardown. Fortunately,
    getsentry/sentry-ruby#1773 adds some test
    helpers which manage Sentry test setup/teardown, which resolves this
    issue.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -18,6 +18,7 @@ require "rspec/rails"
 # Add additional requires below this line. Rails is not loaded until this point!
 require "paper_trail/frameworks/rspec"
 require "pundit/matchers"
+require "sentry/test_helper"
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -76,4 +77,5 @@ RSpec.configure do |config|
 
   config.include ViewComponent::TestHelpers, type: :component
   config.include Capybara::RSpecMatchers, type: :component
+  config.include Sentry::TestHelper
 end
