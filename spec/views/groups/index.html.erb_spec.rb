f2a8cb24b8dbe44e2f1153f2cc22bc9862543a24
Tom Iles <tom.iles@engineering.digital.dwp.gov.uk>
2024-02-07 10:58:46 +0000

    Make Group views use govuk styling
    Only rough changes made to ease designing final version.

--- spec/views/groups/index.html.erb_spec.rb
+++ spec/views/groups/index.html.erb_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe "groups/index", type: :view do
 
   it "renders a list of groups" do
     render
-    cell_selector = Rails::VERSION::STRING >= "7" ? "div>p" : "tr>td"
+    cell_selector = ".govuk-summary-list__value"
     assert_select cell_selector, text: Regexp.new("Name".to_s), count: 2
   end
 end
