a4aefdc774e1ea6098d34b3ce5fc054dfb150490
Tom Iles <tom.iles@engineering.digital.dwp.gov.uk>
2024-02-12 15:44:35 +0000

    Add model tests for external_id

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -11,4 +11,28 @@ RSpec.describe Group, type: :model do
       expect(group).not_to be_valid
     end
   end
+
+  describe "before_create" do
+    it "sets the external_id" do
+      group = create :group
+      expect(group.external_id).to be_present
+    end
+  end
+
+  describe "#to_param" do
+    it "returns the external_id" do
+      group = create :group
+      expect(group.to_param).to eq group.external_id
+    end
+  end
+
+  describe "unique external_id" do
+    it "two models with the same external_id cannot be saved to the DB" do
+      group1 = create :group
+      group2 = create :group
+
+      group2.external_id = group1.external_id
+      expect { group2.save! }.to raise_error(ActiveRecord::RecordNotUnique)
+    end
+  end
 end
