84de0fb5361302f8a8d4cc54079e2d81b7224e48
Tom Iles <tom.iles@digital.cabinet-office.gov.uk>
2024-03-14 16:32:51 +0000

    Change back links on tasklist page for Group
    When a Form belongs to a group, the back link on the tasklist page
    should go to group#show.
    
    This commit changes the behaviour for draft forms and makes sure live
    forms, which are displayed differently still work as expected.
    
    Co-authored-by: Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
    Co-authored-by: Jamie Wilkinson <jamie.o.wilkinson@gmail.com>

--- spec/views/forms/show.html.erb_spec.rb
+++ spec/views/forms/show.html.erb_spec.rb
@@ -3,12 +3,17 @@ require "rails_helper"
 describe "forms/show.html.erb" do
   let(:user) { build :editor_user }
   let(:form) { build :form, :with_pages, id: 1, name: "Form 1", form_slug: "form-1" }
+  let(:group) { create :group, name: "Group 1" }
 
   before do
     assign(:current_user, user)
     assign(:task_status_counts, { completed: 12, total: 20 })
     without_partial_double_verification do
       allow(view).to receive(:current_form).and_return(form)
+    end
+
+    if group.present?
+      GroupForm.create!(form_id: form.id, group_id: group.id)
     end
 
     render template: "forms/show"
@@ -36,6 +41,14 @@ describe "forms/show.html.erb" do
       expect(rendered).to have_css(".govuk-tag.govuk-tag--yellow", text: "Draft")
     end
 
+    it "has a back link to the group page" do
+      expect(view.content_for(:back_link)).to have_link("Back to Group 1", href: group_path(group))
+    end
+  end
+
+  context "when a form is not in a group" do
+    let(:group) { nil }
+
     it "has a back link to the forms page" do
       expect(view.content_for(:back_link)).to have_link("Back to your forms", href: "/")
     end
@@ -54,6 +67,14 @@ describe "forms/show.html.erb" do
 
     it "does not contain a link to delete the form" do
       expect(rendered).not_to have_link("Delete draft form", href: delete_form_path(2))
+    end
+
+    context "and it's not in a group" do
+      let(:group) { nil }
+
+      it "has a back link to the live form page" do
+        expect(view.content_for(:back_link)).to have_link("Back", href: "/forms/2/live")
+      end
     end
   end
 
