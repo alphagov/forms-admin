85e8e461710461ac8b319db34bd4d4eadeea2878
Tom Iles <tom.iles@digital.cabinet-office.gov.uk>
2024-03-18 14:02:06 +0000

    Add feature test for creating Group
    To ensure the Group feature works as expected, we need a feature test.
    
    This commit adds feature tests for creating a group.
    
    It navigates directly to the goup page because it's not linked into the
    main application yet.
    
    It also uses the back button to check that the group is listed in the
    user's group page. I added this because it's an issue I hit when adding
    groups and doesn't take much longer than just creating the group.

---
+++ spec/features/groups/create_group_spec.rb
@@ -0,0 +1,45 @@
+require "rails_helper"
+
+feature "Create a new group", type: :feature do
+  before do
+    login_as_editor_user
+  end
+
+  scenario "Form creator creates a new group" do
+    when_i_visit_the_groups_page
+    and_i_click_create_a_group
+    and_i_fill_in_the_group_name
+    then_i_should_see_the_new_group
+    and_i_use_the_back_link
+    then_i_should_see_the_new_group_on_the_groups_page
+  end
+
+  def when_i_visit_the_groups_page
+    visit groups_path
+    expect(page.find("h1")).to have_text "Your groups"
+    expect_page_to_have_no_axe_errors(page)
+  end
+
+  def and_i_click_create_a_group
+    click_link "Create a group"
+    expect_page_to_have_no_axe_errors(page)
+  end
+
+  def and_i_fill_in_the_group_name
+    fill_in "Name", with: "Group 1"
+    click_button "Save"
+  end
+
+  def then_i_should_see_the_new_group
+    expect(page.find("h1")).to have_text "Group 1"
+    expect_page_to_have_no_axe_errors(page)
+  end
+
+  def and_i_use_the_back_link
+    click_link "Back to your groups"
+  end
+
+  def then_i_should_see_the_new_group_on_the_groups_page
+    expect(page).to have_link "Group 1"
+  end
+end
