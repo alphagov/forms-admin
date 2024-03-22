16b169e5cd390b2978907a7c7838cf13eed1c49a
Samuel Culley <samuel.culley@digital.cabinet-office.gov.uk>
2024-03-22 12:11:44 +0000

    Refactors live form show to use partial
    The live and archived controllers now make use of the same partial for their show action, as they are both built from the same made_live_form.
    
    Co-authored-by: Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>

--- app/controllers/forms/live_controller.rb
+++ app/controllers/forms/live_controller.rb
@@ -1,12 +1,14 @@
-class Forms::LiveController < ApplicationController
-  after_action :verify_authorized
-  def show_form
-    authorize current_form, :can_view_form?
-    render template: "live/show_form", locals: { form_metadata: current_form, form: current_live_form }
-  end
+module Forms
+  class LiveController < ApplicationController
+    after_action :verify_authorized
+    def show_form
+      authorize current_form, :can_view_form?
+      render :show_form, locals: { form_metadata: current_form, form: current_live_form }
+    end
 
-  def show_pages
-    authorize current_form, :can_view_form?
-    render template: "live/show_pages", locals: { form: current_live_form }
+    def show_pages
+      authorize current_form, :can_view_form?
+      render template: "live/show_pages", locals: { form: current_live_form }
+    end
   end
 end
