4d09ea69db29760c946d3c47644b26e743c6b0ed
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2024-03-22 09:31:24 +0200

    Add integration specs for organisation autocomplete
    I want to be able to test how the organisation autocomplete works as a
    user would see it.
    
    However, I couldn't think of a way to write these tests in a BDD way, so
    I didn't put them in the `spec/features` folder, where we prefer to do
    things that way.

---
+++ spec/integration/organisation_autocomplete_spec.rb
@@ -0,0 +1,47 @@
+require "rails_helper"
+
+RSpec.describe "Selecting an organisation using accessible autocomplete", type: :feature do
+  before do
+    create :organisation, slug: "department-for-testing", name: "Department for Testing", abbreviation: "DfT"
+    create :organisation, slug: "ministry-of-tests", name: "Ministry of Tests", abbreviation: "MOT"
+  end
+
+  context "when editing a user" do
+    before do
+      login_as_super_admin_user
+
+      user = create :user
+
+      visit edit_user_path(user)
+    end
+
+    it "autocompletes the organisation" do
+      organisation_field = find_field "Organisation"
+      organisation_field.fill_in with: "tests\n"
+      expect(organisation_field.value).to start_with "Ministry of Tests"
+    end
+  end
+
+  context "when choosing an organisation to view its forms" do
+    before do
+      login_as_super_admin_user
+
+      create :user, organisation: Organisation.find_by(slug: "ministry-of-tests")
+      create :user, organisation: Organisation.find_by(slug: "department-for-testing")
+
+      ActiveResource::HttpMock.respond_to do |mock|
+        Organisation.pluck(:id).each do |organisation_id|
+          mock.get "/api/v1/forms?organisation_id=#{organisation_id}", headers, [].to_json, 200
+        end
+      end
+
+      visit root_path
+    end
+
+    it "autocompletes the organisation" do
+      organisation_field = find_field "search[organisation_id]"
+      organisation_field.fill_in with: "tests\n"
+      expect(organisation_field.value).to start_with "Ministry of Tests"
+    end
+  end
+end
