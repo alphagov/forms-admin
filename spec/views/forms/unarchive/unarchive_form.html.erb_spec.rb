f5537a5a536cbb91b1aca2cc4dd4720c66cd76f3
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2024-03-28 16:18:44 +0000

    Add unarchive form view

---
+++ spec/views/forms/unarchive/unarchive_form.html.erb_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+
+describe "forms/unarchive/unarchive_form.html.erb" do
+  let(:current_form) { OpenStruct.new(id: 1, state: "archived", name: "Form 1", form_slug: "form-1") }
+
+  before do
+    assign(:make_live_form, Forms::MakeLiveForm.new(form: current_form))
+    render template: "forms/unarchive/unarchive_form", locals: { current_form: }
+  end
+
+  it "has the correct heading" do
+    expect(rendered).to have_css("h1", text: I18n.t("page_titles.unarchive_form"))
+  end
+
+  it "contains a radio question for choosing whether to make the form live" do
+    expect(rendered).to have_css("fieldset", text: I18n.t("helpers.label.forms_make_live_form.confirm_make_live"))
+    expect(rendered).to have_field("Yes", type: "radio")
+    expect(rendered).to have_field("No", type: "radio")
+  end
+end
