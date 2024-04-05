a398bf3af824ac2082c3702d17423e2ec36afeb9
Tom Iles <tom.iles@digital.cabinet-office.gov.uk>
2024-04-05 12:40:52 +0100

    Add link to #new in group_members#index

--- app/controllers/group_members_controller.rb
+++ app/controllers/group_members_controller.rb
@@ -2,7 +2,9 @@ class GroupMembersController < ApplicationController
   before_action :set_group, :authorize_group_members
   after_action :verify_authorized
 
-  def index; end
+  def index
+    authorize @group, :show?
+  end
 
   def new; end
 
@@ -12,7 +14,7 @@ private
     @group = Group.find_by(external_id: params[:group_id])
   end
 
-  def authorize_group_members
-    authorize @group, :show?
+  def group_member_params
+    params.require(:group_member_form).permit(:member_email_address).merge(group: @group, creator: current_user)
   end
 end
