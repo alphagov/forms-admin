758d6652c200c1d5fc2605420da07d5328e2fa0c
Tom Iles <tom.iles@digital.cabinet-office.gov.uk>
2024-04-12 12:06:57 +0100

    Add request specs for group_member

--- spec/requests/group_members_controller_spec.rb
+++ spec/requests/group_members_controller_spec.rb
@@ -1,10 +1,11 @@
 require "rails_helper"
 
 RSpec.describe "/groups/:group_id/members", type: :request do
-  let(:group) { create :group }
+  let(:group) { create :group, organisation: editor_user.organisation }
+  let(:role) { :group_admin }
 
   before do
-    create(:membership, user: editor_user, group:)
+    create(:membership, user: editor_user, group:, role:)
     login_as_editor_user
   end
 
@@ -21,6 +22,59 @@ RSpec.describe "/groups/:group_id/members", type: :request do
         get group_members_url(other_group)
 
         expect(response).to have_http_status :forbidden
+      end
+    end
+  end
+
+  describe "GET /groups/:group_id/members/new" do
+    it "renders a successful response" do
+      get new_group_member_url(group)
+      expect(response).to be_successful
+    end
+
+    context "and I'm an editor" do
+      let(:role) { :editor }
+
+      it "denies access" do
+        get new_group_member_url(group)
+        expect(response).to have_http_status :forbidden
+      end
+    end
+  end
+
+  describe "POST /groups/:group_id/members" do
+    context "with valid parameters" do
+      let(:user) { create :user, organisation: editor_user.organisation }
+
+      it "creates a new membership" do
+        expect {
+          post group_members_url(group), params: { group_member_form: { member_email_address: user.email } }
+        }.to change(Membership, :count).by(1)
+
+        expect(response).to have_http_status :redirect
+      end
+
+      context "and I'm an editor" do
+        let(:role) { :editor }
+
+        it "denies access" do
+          expect {
+            post group_members_url(group), params: { group_member_form: { member_email_address: user.email } }
+          }.not_to change(Membership, :count)
+
+          expect(response).to have_http_status :forbidden
+        end
+      end
+    end
+
+    context "with invalid parameters" do
+      it "does not create a new membership" do
+        expect {
+          post group_members_url(group), params: { group_member_form: { member_email_address: "invalid" } }
+        }.not_to change(Membership, :count)
+
+        expect(response).to have_http_status :unprocessable_entity
+        expect(response).to render_template :new
       end
     end
   end
