6440a90f83c43020b3db91670226c32784121ab1
Tom Iles <tom.iles@digital.cabinet-office.gov.uk>
2024-04-25 16:21:16 +0100

    Change group_member view to not use service
    The group_member service probably isn't pulling it's weight. This commit
    moves the logic for the action buttons into the controller and view
    instead.

--- spec/views/group_members/index.html.erb_spec.rb
+++ spec/views/group_members/index.html.erb_spec.rb
@@ -2,26 +2,27 @@ require "rails_helper"
 
 describe "group_members/index", type: :view do
   let(:organisation) { build(:organisation, slug: "Department for testing group members") }
+  let(:current_user) { create(:user, organisation:) }
+  let(:user1) { build(:user, organisation:) }
+  let(:user2) { build(:user, organisation:) }
+  let(:memberships) { [create(:membership, user: user1, role: :editor, group:), create(:membership, user: user2, role: :group_admin, group:)] }
   let(:group) { create(:group, name: "Group 1", organisation:) }
-  let(:add_editor) { false }
-
-  let(:rows) do
-    [
-      OpenStruct.new({ name: "user1", email: "user1@gov.uk", role: :editor, actions: [], membership: { id: 1 } }),
-      OpenStruct.new({ name: "user2", email: "user2@gov.uk", role: :group_admin, actions: [:delete], membership: { id: 1 } }),
-    ]
-  end
-
-  before do
-    assign(:group, group)
-    assign(:group_member_service, instance_double(GroupMemberService, rows:, show_actions?: true))
-
-    allow(Pundit).to receive(:policy).and_return(instance_double(GroupPolicy, add_editor?: add_editor))
-
-    render
-  end
+  let(:add_editor) { true }
+  let(:show_actions) { true }
 
   context "when there are members of a group" do
+    before do
+      memberships
+
+      assign(:current_user, current_user)
+      assign(:group, group)
+
+      allow(Pundit).to receive(:policy).with(current_user, group).and_return(instance_double(GroupPolicy, add_editor?: add_editor))
+      allow(Pundit).to receive(:policy).with(current_user, Membership).and_return(instance_double(MembershipPolicy, update?: true, destroy?: true))
+
+      render locals: { show_actions: }
+    end
+
     it "displays the group name" do
       expect(rendered).to have_selector("h1", text: group.name)
     end
@@ -30,38 +31,51 @@ describe "group_members/index", type: :view do
       expect(rendered).to have_selector("h1", text: t("group_members.index.title"))
     end
 
-    it "displays a table of group memberships" do
-      rows.each do |row|
-        expect(rendered).to have_selector("td", text: row.name)
-        expect(rendered).to have_selector("td", text: row.email)
-        expect(rendered).to have_selector("td", text: t("group_members.index.roles.#{row.role}.name"))
-      end
-
-      expect(rendered).to have_button(t("group_members.index.remove_member"), count: 1)
-    end
-
     it "has a back link to the group page" do
       expect(view.content_for(:back_link)).to have_link("Back to Group 1", href: group_path(group))
     end
 
-    it "does not display a link to add a member" do
-      expect(rendered).not_to have_link(href: new_group_member_path(group))
-    end
-
-    context "when the current user can add an editor" do
-      let(:add_editor) { true }
-
-      it "displays a link to add a member" do
-        expect(rendered).to have_link(t("group_members.index.add_member"), href: new_group_member_path(group))
+    it "displays a table of group memberships" do
+      group.memberships.each do |membership|
+        expect(rendered).to have_selector("table td", text: membership.user.name)
+        expect(rendered).to have_selector("table td", text: membership.user.email)
+        expect(rendered).to have_selector("table td", text: t("group_members.index.roles.#{membership.role}.name"))
+        expect(rendered).to have_selector("table th", text: t("group_members.index.table_headings.actions"))
+        expect(rendered).to have_button(t("group_members.index.remove_member"), count: 2)
+        expect(rendered).to have_button(t("group_members.index.make_editor"), count: 1)
+        expect(rendered).to have_button(t("group_members.index.make_group_admin"), count: 1)
       end
     end
-  end
 
-  context "when there are no members of a group" do
-    let(:rows) { [] }
+    it "has an add member link" do
+      expect(rendered).to have_link(t("group_members.index.add_member"), href: new_group_member_path(group))
+    end
 
-    it "displays a message" do
-      expect(rendered).to have_text(t("group_members.index.no_members"))
+    context "when show_actions is false" do
+      let(:show_actions) { false }
+
+      it "does not display actions if show_actions is false" do
+        expect(rendered).not_to have_selector("table th", text: t("group_members.index.table_headings.actions"))
+        expect(rendered).not_to have_button(t("group_members.index.remove_member"))
+        expect(rendered).not_to have_button(t("group_members.index.make_editor"))
+        expect(rendered).not_to have_button(t("group_members.index.make_group_admin"))
+      end
+    end
+
+    context "when there are no members of a group" do
+      let(:memberships) { [] }
+
+      it "displays a message" do
+        expect(rendered).to have_text(t("group_members.index.no_members"))
+      end
+    end
+
+    context "when the current user cannot add an editor" do
+      let(:add_editor) { false }
+
+      it "does not display the add member link" do
+        expect(rendered).not_to have_link(t("group_members.index.add_member"), href: new_group_member_path(group))
+      end
     end
   end
 end
