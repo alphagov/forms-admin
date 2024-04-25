a8ceadef0b1966885334a758fa9e0ffd805d703a
Tom Iles <tom.iles@digital.cabinet-office.gov.uk>
2024-04-25 16:19:14 +0100

    Add Membership#update action

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,13 +1,35 @@
 class MembershipsController < ApplicationController
+  before_action :set_membership, only: %i[destroy update]
   after_action :verify_authorized
 
   def destroy
-    membership = Membership.find(params[:id])
-    authorize membership
+    authorize @membership
 
-    membership.destroy!
+    @membership.destroy!
 
-    success_message = t(".success", member_name: membership.user.name)
-    redirect_to group_members_path(membership.group), success: success_message
+    redirect_to group_members_path(@membership.group),
+                success: t(".success", member_name: @membership.user.name)
+  end
+
+  def update
+    authorize @membership
+
+    begin
+      @membership.update!(membership_params)
+      redirect_to group_members_path(@membership.group),
+                  success: t(".success.roles.#{@membership.role}", member_name: @membership.user.name)
+    rescue ArgumentError
+      redirect_to group_members_path(@membership.group)
+    end
+  end
+
+private
+
+  def set_membership
+    @membership = Membership.find(params[:id])
+  end
+
+  def membership_params
+    params.require(:membership).permit(:role)
   end
 end
