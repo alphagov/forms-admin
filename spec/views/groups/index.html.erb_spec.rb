d6739778adf3b3c9644eb0ac17fa14f07622688c
Tom Iles <tom.iles@digital.cabinet-office.gov.uk>
2024-04-26 12:19:19 +0100

    Change the details text for org/super admins
    The text on the Group#index page explaining groups should be different
    for org and super admins.
    
    This commit adds the new text and switches it depending no the user's
    role.

--- spec/views/groups/index.html.erb_spec.rb
+++ spec/views/groups/index.html.erb_spec.rb
@@ -3,10 +3,13 @@ require "rails_helper"
 RSpec.describe "groups/index", type: :view do
   let(:trial_groups) { create_list :group, 2 }
   let(:active_groups) { create_list :group, 2, status: :active }
+  let(:current_user) { build :editor_user }
 
   before do
     assign(:trial_groups, trial_groups)
     assign(:active_groups, active_groups)
+
+    assign(:current_user, current_user)
 
     render
   end
@@ -23,5 +26,17 @@ RSpec.describe "groups/index", type: :view do
 
   it "shows a create group button" do
     expect(rendered).to have_link("Create a group", href: new_group_path)
+  end
+
+  it "shows the details text for users who are not org/super admins" do
+    expect(rendered).to have_content("If you need access to an existing form or group, ask someone who has access to that group to invite you.")
+  end
+
+  context "when the user is an admin" do
+    let(:current_user) { build :organisation_admin_user }
+
+    it "shows the details text for admins" do
+      expect(rendered).to have_content("Because you’re an organisation admin, you can access all the groups in your organisation.")
+    end
   end
 end
