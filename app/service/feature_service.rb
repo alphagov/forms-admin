a02041625ead5740e1d8cfb66a4f6a6095310d3d
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2024-05-08 12:56:02 +0300

    Add feature flags per organisation

--- app/service/feature_service.rb
+++ app/service/feature_service.rb
@@ -1,10 +1,34 @@
 class FeatureService
-  class << self
-    def enabled?(feature_name)
-      return false if Settings.features.blank?
+  attr_reader :user
 
-      segments = feature_name.to_s.split(".")
-      segments.reduce(Settings.features) { |config, segment| config[segment] }
+  class << self
+    def enabled?(...)
+      FeatureService.new(nil).enabled?(...)
+    end
+  end
+
+  def initialize(user)
+    @user = user
+  end
+
+  def enabled?(feature_name)
+    return false if Settings.features.blank?
+
+    segments = feature_name.to_s.split(".")
+    feature_config = Settings.features.dig(*segments)
+
+    if feature_config.is_a? Config::Options
+      enabled = feature_config.key?(:enabled) ? feature_config.enabled : false
+      enabled = feature_config.trial_users if feature_config.key?(:trial_users) && user.trial?
+
+      if feature_config.organisations.present? && !user.trial?
+        organisation_key = user.organisation.slug.underscore.to_sym
+        enabled = feature_config.organisations[organisation_key] if feature_config.organisations.key?(organisation_key)
+      end
+
+      enabled
+    else
+      feature_config
     end
   end
 end
