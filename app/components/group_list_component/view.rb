7bb71c638047fc1ba8742e8a0f6b32236f080115
Jamie Wilkinson <jamie.wilkinson@digital.cabinet-office.gov.uk>
2024-05-09 10:55:49 +0100

    Allow empty group list to render nothing
    Co-authored-by: Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>

--- app/components/group_list_component/view.rb
+++ app/components/group_list_component/view.rb
@@ -1,10 +1,11 @@
 module GroupListComponent
   class View < ViewComponent::Base
-    def initialize(groups:, title:, empty_message:)
+    def initialize(groups:, title:, empty_message: "", show_empty: true)
       super
       @groups = groups
       @title = title
       @empty_message = empty_message
+      @show_empty = show_empty
     end
   end
 end
