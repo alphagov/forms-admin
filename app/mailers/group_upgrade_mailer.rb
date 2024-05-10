0ae9ee7378226848570ba109df0803760f911a31
Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>
2024-05-10 16:13:12 +0100

    Add group upgrade rejected email to mailer
    Co-authored-by: Jamie Wilkinson <jamie.wilkinson@digital.cabinet-office.gov.uk>

--- app/mailers/group_upgrade_mailer.rb
+++ app/mailers/group_upgrade_mailer.rb
@@ -13,6 +13,21 @@ class GroupUpgradeMailer < GovukNotifyRails::Mailer
     mail(to: to_email)
   end
 
+  def rejected_email(to_email:, rejected_by_name:, rejected_by_email:, group_name:, group_url:)
+    set_template(Settings.govuk_notify.group_upgrade_rejected_template_id)
+
+    set_email_reply_to(Settings.govuk_notify.zendesk_reply_to_id)
+
+    set_personalisation(
+      rejected_by_name:,
+      rejected_by_email:,
+      group_name:,
+      group_url:,
+    )
+
+    mail(to: to_email)
+  end
+
   def group_upgrade_requested_email(requester_name:, requester_email_address:, to_email:, group_name:, view_request_url:)
     set_template(Settings.govuk_notify.group_upgrade_requested_template_id)
 
--- spec/mailers/group_upgrade_mailer_spec.rb
+++ spec/mailers/group_upgrade_mailer_spec.rb
@@ -29,6 +29,37 @@ describe GroupUpgradeMailer, type: :mailer do
     end
   end
 
+  describe "#rejected_email" do
+    subject(:mail) do
+      described_class.rejected_email(
+        to_email:,
+        rejected_by_name: current_user.name,
+        rejected_by_email: current_user.email,
+        group_name: group.name,
+        group_url:,
+      )
+    end
+
+    it "sends an email with the correct template" do
+      expect(mail.govuk_notify_template).to eq(Settings.govuk_notify.group_upgrade_rejected_template_id)
+    end
+
+    it "sends an email with the correct reply-to value" do
+      expect(mail.govuk_notify_email_reply_to).to eq(Settings.govuk_notify.zendesk_reply_to_id)
+    end
+
+    it "sends an email to the correct email address" do
+      expect(mail.to).to eq([to_email])
+    end
+
+    it "includes the personalisation" do
+      expect(mail.govuk_notify_personalisation[:rejected_by_name]).to eq(current_user.name)
+      expect(mail.govuk_notify_personalisation[:rejected_by_email]).to eq(current_user.email)
+      expect(mail.govuk_notify_personalisation[:group_name]).to eq(group.name)
+      expect(mail.govuk_notify_personalisation[:group_url]).to eq(group_url)
+    end
+  end
+
   describe "#group_upgrade_requested_email" do
     subject(:mail) do
       described_class.group_upgrade_requested_email(
