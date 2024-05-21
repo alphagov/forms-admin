e081d603d2cfc9b014ca9693a2fa8f43454484d7
Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>
2024-05-21 16:40:07 +0100

    Update archive feature test to use BDD style
    Update test for archiving a form to use a BDD style with methods per
    step. This makes this consistent with the way we write other feature
    tests.

--- spec/features/form/archive_a_form_spec.rb
+++ spec/features/form/archive_a_form_spec.rb
@@ -17,27 +17,48 @@ feature "Archive a form", type: :feature do
   end
 
   scenario "as a form editor" do
-    visit root_path
-    expect(page.find("h1")).to have_text "GOV.UK Forms"
-    expect_page_to_have_no_axe_errors(page)
+    given_i_am_viewing_a_live_form
+    when_i_click_archive_this_form
+    then_i_am_shown_the_archive_form_page
+    when_i_choose_yes
+    then_i_am_shown_a_page_confirming_the_form_has_been_archived
+    when_i_continue_to_form_details
+    then_the_form_has_the_archived_tag
+  end
 
-    click_link form.name
+  def given_i_am_viewing_a_live_form
+    visit live_form_path(form.id)
     expect(page.find("h1")).to have_text form.name
     expect(page).to have_css ".govuk-tag.govuk-tag--turquoise", text: "Live"
     expect_page_to_have_no_axe_errors(page)
+  end
 
+  def when_i_click_archive_this_form
     click_link_or_button "Archive this form"
+  end
+
+  def then_i_am_shown_the_archive_form_page
     expect(page.find("h1")).to have_text "Archive this form"
     expect(page).to have_text "Are you sure you want to archive this form?"
     expect_page_to_have_no_axe_errors(page)
+  end
 
+  def when_i_choose_yes
     choose "Yes"
     click_button "Save and continue"
+  end
+
+  def then_i_am_shown_a_page_confirming_the_form_has_been_archived
     expect(page.find("h1")).to have_text "Your form has been archived"
     expect_page_to_have_no_axe_errors(page)
+  end
 
+  def when_i_continue_to_form_details
     click_link_or_button "Continue to form details"
     expect(page.find("h1")).to have_text form.name
+  end
+
+  def then_the_form_has_the_archived_tag
     expect(page).to have_css ".govuk-tag.govuk-tag--orange", text: "Archived"
     expect_page_to_have_no_axe_errors(page)
   end
