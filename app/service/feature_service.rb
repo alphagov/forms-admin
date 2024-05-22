2ee806b0c27f393d1993655d49a904194df30de3
Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>
2024-05-22 16:51:29 +0100

    Update FeatureService to allow configuration per organisation
    Allow specifying overrides for a feature flag per organisation by
    providing an object in the settings YAML file as the value for the
    feature, with optional keys matching the slugs for individual
    organisations with underscores instead of dashes. If a key exists for
    the organisation for the logged in user, the value of this key is used
    over the value of the `enabled` key.
    
    Example:
    
    ```
    features:
      groups:
        enabled: false
        organisations:
          government_digital_service: true
    ```
    
    This continues to support providing a simple boolean flag for a
    feature.
    
    We will also continue supporting the `enabled?` class method for the
    FeatureService, which will ignore any overrides for the organisation
    as it does not have access to the logged in user.
    
    Co-authored-by: Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>

--- app/service/feature_service.rb
+++ app/service/feature_service.rb
@@ -1,10 +1,33 @@
 class FeatureService
-  class << self
-    def enabled?(feature_name)
-      return false if Settings.features.blank?
+  class UserRequiredError < StandardError; end
 
-      segments = feature_name.to_s.split(".")
-      segments.reduce(Settings.features) { |config, segment| config[segment] }
+  class << self
+    def enabled?(...)
+      FeatureService.new(nil).enabled?(...)
     end
+  end
+
+  def initialize(user)
+    @user = user
+  end
+
+  def enabled?(feature_name)
+    return false if Settings.features.blank?
+
+    segments = feature_name.to_s.split(".")
+    feature = Settings.features.dig(*segments)
+
+    return feature unless feature.is_a? Config::Options
+
+    if feature.organisations.present?
+      raise UserRequiredError, "Feature #{feature_name} requires user to be provided" if @user.blank?
+
+      if @user.organisation.present?
+        organisation_key = @user.organisation.slug.underscore.to_sym
+        return feature.organisations[organisation_key] if feature.organisations.key?(organisation_key)
+      end
+    end
+
+    feature.enabled
   end
 end
