04bdb12aa861f446a4b28fcc2ec3d3eb2ce74c2d
Tom Iles <tom.iles@digital.cabinet-office.gov.uk>
2024-05-23 14:52:52 +0100

    Add traits to factories Co-authored-by: Samuel Culley <samuel.culley@digital.cabinet-office.gov.uk>

--- spec/factories/models/groups.rb
+++ spec/factories/models/groups.rb
@@ -4,5 +4,9 @@ FactoryBot.define do
     organisation { association :organisation, id: 1, slug: "test-org" }
     creator { association :user, organisation: }
     status { :trial }
+
+    trait :org_has_org_admin do
+      organisation { association :organisation, :with_org_admin, id: 1, slug: "test-org" }
+    end
   end
 end
