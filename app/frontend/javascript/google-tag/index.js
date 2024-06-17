4d356854144df07dc3c82754a2365cb74026fc6b
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2024-06-17 09:41:48 +0100

    Add google analytics behind flag
    Adds a subset of the analytics code from the product pages repo to this
    one and runs it when the settings flag is enabled.
    
    We don't need most of the code that's in the product pages, because it's
    an internal tool and we don't need to implement the cookie consent
    mechanism.

---
+++ app/frontend/javascript/google-tag/index.js
@@ -0,0 +1,19 @@
+export function installAnalyticsScript (global) {
+  const GTAG_ID = 'GTM-T2SJXKKQ'
+  if (!window.ga) {
+    ;(function (w, d, s, l, i) {
+      w[l] = w[l] || []
+      w[l].push({
+        'gtm.start': new Date().getTime(),
+        event: 'gtm.js'
+      })
+
+      const j = d.createElement(s)
+      const dl = l !== 'dataLayer' ? '&l=' + l : ''
+
+      j.async = true
+      j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl
+      document.head.appendChild(j)
+    })(global, document, 'script', 'dataLayer', GTAG_ID)
+  }
+}
