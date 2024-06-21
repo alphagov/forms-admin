431df910e794cbf683583f3b8cecb032e0153a54
Tom Iles <tom.iles@digital.cabinet-office.gov.uk>
2024-06-21 16:21:53 +0100

    Add feature test for changing group name
    Add a new feature test for changing the name of an existing group as a
    group admin.
    
    Co-authored-by: Jamie Wilkinson <jamie.wilkinson@digital.cabinet-office.gov.uk>

---
+++ spec/features/groups/edit_group_spec.rb
@@ -0,0 +1,44 @@
+require "rails_helper"
+
+feature "Create a new group", type: :feature do
+  before do
+    login_as_editor_user
+  end
+
+  describe "changing the name of an existing group" do
+    let(:group) { create(:group, organisation: editor_user.organisation, creator: editor_user) }
+
+    before do
+      create(:membership, user: editor_user, group:, role: :group_admin)
+    end
+
+    scenario "group admin can change the name of a group" do
+      when_i_visit_the_group_page
+      and_i_click_change_name
+      then_i_see_the_change_name_page
+      and_i_enter_a_new_name
+      then_i_should_see_the_group_with_new_name
+    end
+  end
+
+  def when_i_visit_the_group_page
+    visit group_path(group)
+  end
+
+  def and_i_click_change_name
+    click_link "Change the name of this group"
+  end
+
+  def then_i_see_the_change_name_page
+    expect(page.find("h1")).to have_text "Change the name of this group"
+  end
+
+  def and_i_enter_a_new_name
+    fill_in "Change the name of this group", with: "new group name"
+    click_button "Save and continue"
+  end
+
+  def then_i_should_see_the_group_with_new_name
+    expect(page.find("h1")).to have_text "new group name"
+  end
+end
