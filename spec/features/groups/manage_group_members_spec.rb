31d2c6e3b6cc39996793b5de27313bb654186f58
Tom Iles <tom.iles@digital.cabinet-office.gov.uk>
2024-06-21 14:55:39 +0100

    Add feature test for org admin to manage users
    Add new scenario for the managing users.
    
    A organisation admin can add group admins directly to a group and also
    change existing group admins to editors.
    
    This commit adds a new scenario to cover both cases.
    
    Co-authored-by: Jamie Wilkinson <jamie.wilkinson@digital.cabinet-office.gov.uk>

--- spec/features/groups/manage_group_members_spec.rb
+++ spec/features/groups/manage_group_members_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-feature "Show members of a group", type: :feature do
+feature "Manage members of group", type: :feature do
   let(:organisation) { editor_user.organisation }
   let(:group) { create(:group, name: "Group 1", organisation:) }
   let(:user1) { create(:user, organisation:) }
@@ -11,10 +11,10 @@ feature "Show members of a group", type: :feature do
     create(:membership, user: editor_user, group:, role: :group_admin)
     create(:membership, user: user1, group:, role: :editor)
     create(:membership, user: user2, group:, role: :group_admin)
-    login_as_editor_user
   end
 
-  scenario "Group editor lists members of a group" do
+  scenario "group admin adds a new editor" do
+    login_as_editor_user
     when_i_visit_the_groups_page
     and_i_click_the_group_link
     and_i_click_the_edit_group_members_link
@@ -22,7 +22,21 @@ feature "Show members of a group", type: :feature do
     when_i_click_add_editor
     then_i_should_see_the_add_editor_form
     when_i_fill_in_the_add_editor_form
-    then_i_should_see_the_new_editor_in_the_list
+    then_i_should_see_the_user_as_editor
+  end
+
+  scenario "organisation admin adds a new group_admin and changes them to editor" do
+    login_as_organisation_admin_user
+    when_i_visit_the_groups_page
+    and_i_click_the_group_link
+    and_i_click_the_edit_group_members_link
+    then_i_should_see_the_members_of_the_group
+    when_i_click_add_editor_or_group_admin
+    then_i_should_see_the_add_editor_or_group_admin_form
+    when_i_fill_in_the_add_group_admin_form
+    then_i_should_see_the_new_group_admin
+    when_i_click_make_editor_for_user
+    then_i_should_see_the_user_as_editor
   end
 
   def when_i_visit_the_groups_page
@@ -50,8 +64,17 @@ feature "Show members of a group", type: :feature do
     click_link "Add an editor"
   end
 
+  def when_i_click_add_editor_or_group_admin
+    click_link "Add an editor or group admin"
+  end
+
   def then_i_should_see_the_add_editor_form
     expect(page.find("h1")).to have_text "Add an editor to this group"
+    expect_page_to_have_no_axe_errors(page)
+  end
+
+  def then_i_should_see_the_add_editor_or_group_admin_form
+    expect(page.find("h1")).to have_text "Add an editor or group admin to this group"
     expect_page_to_have_no_axe_errors(page)
   end
 
@@ -60,9 +83,36 @@ feature "Show members of a group", type: :feature do
     click_button "Add this person"
   end
 
-  def then_i_should_see_the_new_editor_in_the_list
+  def when_i_fill_in_the_add_group_admin_form
+    fill_in "Enter the email address of the person you want to add", with: user3.email
+    choose("Group admin")
+    click_button "Add this person"
+  end
+
+  def then_i_should_see_the_new_group_admin
     expect(page.find("h1")).to have_text "Group 1"
-    expect(page).to have_text user3.name
-    expect(page).to have_text user3.email
+
+    user_row = page.find("td", text: user3.name).ancestor("tr")
+    within(user_row) do
+      expect(user_row).to have_text user3.name
+      expect(user_row).to have_text user3.email
+      expect(user_row).to have_text "Group admin"
+    end
+  end
+
+  def then_i_should_see_the_user_as_editor
+    expect(page.find("h1")).to have_text "Group 1"
+
+    user_row = page.find("td", text: user3.name).ancestor("tr")
+    within(user_row) do
+      expect(user_row).to have_text user3.name
+      expect(user_row).to have_text user3.email
+      expect(user_row).to have_text "Editor"
+    end
+  end
+
+  def when_i_click_make_editor_for_user
+    user_row = page.find("td", text: user3.name).ancestor("tr")
+    user_row.click_button "Make editor"
   end
 end
