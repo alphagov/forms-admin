01779a92e8331f819dbeddb0fa38014695ce1382
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2024-07-01 12:30:36 +0300

    Delete UserUpgradeRequestMailer

--- app/mailers/user_upgrade_request_mailer.rb
+++
@@ -1,9 +0,0 @@
-class UserUpgradeRequestMailer < GovukNotifyRails::Mailer
-  def upgrade_request_email(user_email:)
-    set_template(Settings.govuk_notify.user_upgrade_template_id)
-
-    set_email_reply_to(Settings.govuk_notify.zendesk_reply_to_id)
-
-    mail(to: user_email)
-  end
-end
