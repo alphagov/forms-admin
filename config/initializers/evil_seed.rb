43873524a747f2a4e464a79f50b51b040c65a07b
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2024-07-11 15:48:35 +0300

    wip!

--- config/initializers/evil_seed.rb
+++ config/initializers/evil_seed.rb
@@ -1,14 +1,28 @@
 require "evil_seed"
 
 EvilSeed.configure do |config|
-  config.root("User") do |root|
-    root.exclude("forms")
-
-    root.exclude("versions")
+  config.root("Organisation") do |root|
+    root.exclude(/.*/)
   end
 
-  config.root("Group")
+  config.root("User") do |root|
+    root.exclude(/.*/)
+  end
 
+  config.anonymize("User") do
+    name { Faker::Name.name }
+    email { |real_email| Faker::Internet.email(name: Faker::Number.number(digits: 10).to_s, domain: real_email.split("@").last) }
+    uid { |real_uid| Digest::MD5.hexdigest(real_uid) }
+    provider { "evil-seed" }
+  end
+
+  config.root("MouSignature")
+
+  config.root("Group") do |root|
+    root.exclude(/.*/)
+  end
+
+  config.root("Membership")
   config.root("GroupForm")
 
   config.verbose = true
