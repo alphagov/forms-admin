2f1408c87979f635a1e68429409e37b75197f37b
Jamie Wilkinson <jamie.wilkinson@digital.cabinet-office.gov.uk>
2024-07-23 09:56:50 +0100

    Refactor FormListService to remove non-groups code paths
    We now only use the FormListService in the group view, so can remove
    any redundant code. Previously, we didn't display the created_by column
    in the forms table for trial users. This is no longer the case, so we've
    removed the corresponding tests.
    
    Co-authored-by: Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>

--- spec/service/form_list_service_spec.rb
+++ spec/service/form_list_service_spec.rb
@@ -1,144 +1,43 @@
 require "rails_helper"
 
 describe FormListService do
-  let(:service) { described_class.call(forms:, current_user:) }
+  let(:service) { described_class.call(forms:, group:) }
 
-  let(:forms) { build_list :form, 5, :with_id, creator_id: current_user.id }
-  let(:current_user) { create :user, :with_no_org }
+  let(:creator) { create :user }
+  let(:forms) { build_list :form, 5, :with_id, creator_id: creator.id }
+  let(:group) { build :group }
 
   describe "#data" do
     describe "caption" do
-      context "when user doesn't have an organisation" do
-        let(:current_user) { create :user, :with_no_org }
-
-        it "returns generic caption" do
-          expect(service.data).to include caption: I18n.t("home.your_forms")
-        end
-      end
-
-      context "when user has trial role" do
-        let(:current_user) { create :user, :with_trial_role }
-
-        it "returns generic caption" do
-          expect(service.data).to include caption: I18n.t("home.your_forms")
-        end
-      end
-
-      context "when user has organisation" do
-        let(:current_user) { create :editor_user }
-
-        it "returns specific organisation caption" do
-          organisation_name = current_user.organisation.name
-          expect(service.data).to include caption: I18n.t("home.form_table_caption", organisation_name:)
-        end
-      end
-
-      context "when organisation is given as a keyword argument" do
-        let(:service) { described_class.call(forms:, current_user:, organisation:) }
-
-        let(:organisation) { OpenStruct.new(name: "Organisation 1") }
-
-        it "returns specific organisation caption" do
-          organisation_name = organisation.name
-          expect(service.data).to include caption: I18n.t("home.form_table_caption", organisation_name:)
-        end
+      it "returns caption containing group name" do
+        expect(service.data).to include caption: I18n.t("groups.form_table_caption", group_name: group.name)
       end
     end
 
     describe "head" do
-      context "when user is editor" do
-        let(:current_user) { create :editor_user }
-
-        it "contains a 'Name', `Created by` and 'Status' column heading" do
-          expect(service.data[:head]).to eq([I18n.t("home.form_name_heading"),
-                                             { text: I18n.t("home.created_by") },
-                                             { text: I18n.t("home.form_status_heading"), numeric: true }])
-        end
-      end
-
-      context "when user is super admin" do
-        let(:current_user) { create :super_admin_user }
-
-        it "contains a 'Name', `Created by` and 'Status' column heading" do
-          expect(service.data[:head]).to eq([I18n.t("home.form_name_heading"),
-                                             { text: I18n.t("home.created_by") },
-                                             { text: I18n.t("home.form_status_heading"), numeric: true }])
-        end
-      end
-
-      context "when user is trial" do
-        let(:current_user) { create :user, :with_trial_role }
-
-        it "contains a 'Name' and 'Status' column heading" do
-          expect(service.data[:head]).to eq([I18n.t("home.form_name_heading"), { text: I18n.t("home.created_by") }, { text: I18n.t("home.form_status_heading"), numeric: true }])
-        end
+      it "contains a 'Name', `Created by` and 'Status' column heading" do
+        expect(service.data[:head]).to eq([I18n.t("home.form_name_heading"),
+                                           { text: I18n.t("home.created_by") },
+                                           { text: I18n.t("home.form_status_heading"), numeric: true }])
       end
     end
 
     describe "rows" do
-      context "when user is trial user" do
-        let(:current_user) { create :user, :with_trial_role }
-
-        it "has a row for each form passed to the service" do
-          expect(service.data[:rows].size).to eq forms.size
-        end
-
-        it "returns the correct data for each form" do
-          service.data[:rows].each_with_index do |row, index|
-            form = forms[index]
-            expect(row).to eq([
-              { text: "<a class=\"govuk-link\" href=\"/forms/#{form.id}\">#{form.name}</a>" },
-              { text: current_user.name.to_s },
-              {
-                numeric: true,
-                text: "<div class='app-form-states'><strong class=\"govuk-tag govuk-tag--yellow\">Draft</strong>\n</div>",
-              },
-            ])
-          end
-        end
+      it "has a row for each form passed to the service" do
+        expect(service.data[:rows].size).to eq forms.size
       end
 
-      context "when user is editor" do
-        let(:current_user) { create :editor_user }
-
-        it "contains 3 columns" do
-          expect(service.data[:rows].first.size).to eq 3
-        end
-
-        it "returns the created by name" do
-          service.data[:rows].each_with_index do |row, index|
-            form = forms[index]
-            expect(row).to eq([
-              { text: "<a class=\"govuk-link\" href=\"/forms/#{form.id}\">#{form.name}</a>" },
-              { text: current_user.name },
-              {
-                numeric: true,
-                text: "<div class='app-form-states'><strong class=\"govuk-tag govuk-tag--yellow\">Draft</strong>\n</div>",
-              },
-            ])
-          end
-        end
-      end
-
-      context "when user is super admin" do
-        let(:current_user) { create :super_admin_user }
-
-        it "contains 3 columns" do
-          expect(service.data[:rows].first.size).to eq 3
-        end
-
-        it "returns the created by name" do
-          service.data[:rows].each_with_index do |row, index|
-            form = forms[index]
-            expect(row).to eq([
-              { text: "<a class=\"govuk-link\" href=\"/forms/#{form.id}\">#{form.name}</a>" },
-              { text: current_user.name },
-              {
-                numeric: true,
-                text: "<div class='app-form-states'><strong class=\"govuk-tag govuk-tag--yellow\">Draft</strong>\n</div>",
-              },
-            ])
-          end
+      it "returns the correct data for each form" do
+        service.data[:rows].each_with_index do |row, index|
+          form = forms[index]
+          expect(row).to eq([
+            { text: "<a class=\"govuk-link\" href=\"/forms/#{form.id}\">#{form.name}</a>" },
+            { text: creator.name.to_s },
+            {
+              numeric: true,
+              text: "<div class='app-form-states'><strong class=\"govuk-tag govuk-tag--yellow\">Draft</strong>\n</div>",
+            },
+          ])
         end
       end
     end
