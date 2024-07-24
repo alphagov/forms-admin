alphagov/forms-admin

commit 5a05d268a11805ae1b3b2952ed2154bb230e59ff

Ensure DeleteConfirmationController actions are authorized by Pundit

app/controllers/forms/delete_confirmation_controller.rb
@@ -1,5 +1,7 @@
 module Forms
   class DeleteConfirmationController < ApplicationController
+    after_action :verify_authorized
+
     def delete
       load_page_variables
     end

commit 3c18034403f1fe6b7c65d8f5ab567ec2985cbd19

Add spec for when user chooses not to delete form
We weren't testing the path when a user presses the delete button for a
form but then chooses not to delete it at the confirmation page.

Not testing this path led to a bug being introduced in commit 5a05d268
as we were asking Pundit to verify that we authorize the user for all
actions, but weren't testing all paths for all actions.

spec/requests/forms/delete_confirmation_controller_spec.rb
@@ -46,5 +46,25 @@ RSpec.describe Forms::DeleteConfirmationController, type: :request do
         expect(form).to have_been_deleted
       end
     end
+
+    context "when the user has decided not to delete the form" do
+      before do
+        ActiveResourceMock.mock_resource(form,
+                                         {
+                                           read: { response: form, status: 200 },
+                                           delete: { response: {}, status: 200 },
+                                         })
+
+        delete destroy_form_path(form_id: 2, forms_delete_confirmation_input: { confirm: "no" })
+      end
+
+      it "redirects you to the form page" do
+        expect(response).to redirect_to(form_path(2))
+      end
+
+      it "does not delete the form on the API" do
+        expect(form).not_to have_been_deleted
+      end
+    end
   end
 end