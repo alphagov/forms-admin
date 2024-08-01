cf23953d45cdb5097e417999f5a4ed6b3884d0d9
Tom Iles <tom.iles@digital.cabinet-office.gov.uk>
2024-08-01 14:52:28 +0100

    Add reports#users view
    Add a new view which will display a table of organisations with a count
    of the users which belong to them.

---
+++ spec/views/reports/users.html.erb_spec.rb
@@ -0,0 +1,39 @@
+require "rails_helper"
+
+describe "reports/useres.html.erb" do
+  let(:data) do
+    {
+      caption: "table caption",
+      head: [
+        { text: "org name" },
+        { text: "user count", numeric: true },
+      ],
+      rows: [
+        [{ text: "org 1" }, { text: 2, numeric: true }],
+        [{ text: "org 2" }, { text: 1, numeric: true }],
+      ],
+    }
+  end
+
+  before do
+    render template: "reports/users", locals: { data: }
+  end
+
+  describe "page title" do
+    it "matches the heading" do
+      expect(view.content_for(:title)).to eq "Number of users per organisation"
+    end
+  end
+
+  it "has a back link to the live form page" do
+    expect(view.content_for(:back_link)).to have_link("Back to reports", href: reports_path)
+  end
+
+  it "contains page heading" do
+    expect(rendered).to have_css("h1.govuk-heading-l", text: "Number of users per organisation")
+  end
+
+  it "contains the table" do
+    expect(rendered).to have_table(with_rows: [["org 1", "2"], ["org 2", "1"]])
+  end
+end
