7deba00bdc00f5afd4d63426cdd03f382a9d7365
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2024-08-16 13:52:09 +0300

    Log actual user when super admin is acting as another user
    We want to make sure that when the "Act as user" feature is used that
    our logs don't confuse the actions taken by the actual user with
    something done by the user they are acting as. This is especially
    important for some operations such as signing an MoU, as it could have
    legal ramifications if our logs make it look like a person has done
    something that was actually done by a super admin.
    
    This commit changes our logging so that if a super admin is acting as
    another user we log the super admin's details.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -43,9 +43,19 @@ class ApplicationController < ActionController::Base
     CurrentLoggingAttributes.trace_id = request.env["HTTP_X_AMZN_TRACE_ID"].presence
     CurrentLoggingAttributes.user_ip = user_ip(request.env.fetch("HTTP_X_FORWARDED_FOR", ""))
     if current_user.present?
-      CurrentLoggingAttributes.user_id = current_user.id
-      CurrentLoggingAttributes.user_email = current_user.email
-      CurrentLoggingAttributes.user_organisation_slug = current_user.organisation&.slug
+      if acting_as?
+        CurrentLoggingAttributes.user_id = actual_user.id
+        CurrentLoggingAttributes.user_email = actual_user.email
+        CurrentLoggingAttributes.user_organisation_slug = actual_user.organisation&.slug
+
+        CurrentLoggingAttributes.acting_as_user_id = acting_as_user.id
+        CurrentLoggingAttributes.acting_as_user_email = acting_as_user.email
+        CurrentLoggingAttributes.acting_as_user_organisation_slug = acting_as_user.organisation&.slug
+      else
+        CurrentLoggingAttributes.user_id = current_user.id
+        CurrentLoggingAttributes.user_email = current_user.email
+        CurrentLoggingAttributes.user_organisation_slug = current_user.organisation&.slug
+      end
     end
     CurrentLoggingAttributes.form_id = params[:form_id] if params[:form_id].present?
     CurrentLoggingAttributes.page_id = params[:page_id] if params[:page_id].present?
@@ -78,6 +88,18 @@ class ApplicationController < ActionController::Base
 
   def current_user
     warden.user if user_signed_in
+  end
+
+  def acting_as?
+    session[:acting_as_user_id].present?
+  end
+
+  def actual_user
+    @actual_user ||= User.find(session[:original_user_id]) if acting_as?
+  end
+
+  def acting_as_user
+    current_user if acting_as?
   end
 
   def authenticate_user!
