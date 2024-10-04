dc3e2564f357e45dffbace12332920e4be7f3903
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2024-10-04 15:31:14 +0100

    Remove custom linting plugin
    This linting plugin didn't provide much value (users can run the
    frontend lint in a separate terminal) and it slowed down the vite build.
    
    This was especially annoying when starting bin/dev, since the main idea
    behind Vite is to have a fast development server.
    
    One consequence of removing this plugin is that vitest no longer
    automatically runs the linting task when running the tests. We were
    relying on this in the build_and_test github workflow, so this commit
    also readds the linting task to the github action.

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,9 @@
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
 import * as path from 'node:path'
-import lintPlugin from './app/frontend/plugins/vite/vite-plugin-lint'
 
 export default defineConfig({
-  plugins: [RubyPlugin(), { ...lintPlugin(), apply: 'serve' }],
+  plugins: [RubyPlugin()],
   build: { emptyOutDir: true },
   css: {
     preprocessorOptions: {
