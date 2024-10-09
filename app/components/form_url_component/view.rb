3cad3c398b95a913a0e8740229fd01606d224d16
Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>
2024-10-09 12:35:36 +0100

    Refactor FormUrlComponent for reuse
    This component display the URL to a form with a button to copy the URL
    to the clipboard.
    
    Allow passing in the heading text and button text to the
    FormUrlComponent so that we can re-use it to display the link to the
    preview of a form.
    
    Co-authored-by: David Biddle <david.biddle@digital.cabinet-office.gov.uk>

--- app/components/form_url_component/view.rb
+++ app/components/form_url_component/view.rb
@@ -2,9 +2,17 @@
 
 module FormUrlComponent
   class View < ViewComponent::Base
-    def initialize(runner_link)
+    def initialize(runner_link:, heading_text: nil, button_text: nil)
       super
       @runner_link = runner_link
+      @heading_text = heading_text
+      @button_text = button_text
+    end
+
+    def before_render
+      super
+      @heading_text = t("form_url_component.form_url") if @heading_text.nil?
+      @button_text = t("form_url_component.copy_to_clipboard") if @button_text.nil?
     end
   end
 end
