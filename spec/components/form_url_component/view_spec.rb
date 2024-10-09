c29844929edb2e57a2eccec0814f12d80df9a086
Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>
2024-10-09 12:35:36 +0100

    Refactor FormUrlComponent for reuse
    This component display the URL to a form with a button to copy the URL
    to the clipboard.
    
    Allow passing in the heading text and button text to the
    FormUrlComponent so that we can re-use it to display the link to the
    preview of a form.
    
    Co-authored-by: David Biddle <david.biddle@digital.cabinet-office.gov.uk>

--- spec/components/form_url_component/view_spec.rb
+++ spec/components/form_url_component/view_spec.rb
@@ -1,15 +1,38 @@
 require "rails_helper"
 
 RSpec.describe FormUrlComponent::View, type: :component do
-  before do
-    render_inline(described_class.new("https://example.com"))
+  context "when translations are not passed to the component" do
+    before do
+      render_inline(described_class.new(runner_link: "https://example.com"))
+    end
+
+    it "renders the heading with default text" do
+      expect(page).to have_css("h2", text: "Form URL")
+    end
+
+    it "renders the link" do
+      expect(page).to have_text("https://example.com")
+    end
+
+    it "has data-copy-button-text attribute set to default button text" do
+      expect(page).to have_css("[data-copy-button-text='Copy URL to clipboard']")
+    end
   end
 
-  it "renders the heading" do
-    expect(page).to have_css("h2", text: "Form URL")
-  end
+  context "when translations are passed to the component" do
+    let(:heading_text) { "Some heading" }
+    let(:button_text) { "A button" }
 
-  it "renders the link" do
-    expect(page).to have_text("https://example.com")
+    before do
+      render_inline(described_class.new(runner_link: "https://example.com", heading_text:, button_text:))
+    end
+
+    it "renders the heading with the provided heading text" do
+      expect(page).to have_css("h2", text: heading_text)
+    end
+
+    it "has data-copy-button-text attribute set to the provided button text" do
+      expect(page).to have_css("[data-copy-button-text='#{button_text}']")
+    end
   end
 end
