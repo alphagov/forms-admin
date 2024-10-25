16874fc1b2dcfd00e00068efbf6c61105700035c
Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>
2024-10-25 11:27:21 +0100

    Add long_lists_enabled column to groups
    This will be a temporary column to feature flag the ability to create
    "select an option from a list" type questsions with more than 30
    options, which will use an autocomplete component.
    
    We initially want to restrict usage to one organisation to test with,
    and having the flag on the group will allow us to do this.
    
    We opted to do this over using the organisation level feature flagging
    in the settings that we previously used as this had the following
    issues:
    - We could not enable for an organisation in dev/staging environments
    without affecting production.
    - For testing, we would want it enabled for the GDS organisation but
    the GDS organisation has real forms and users.
    - While this feature is only enabled for one organisation, if we
    enabled it for all forms for our organisation, this would be
    unrepresentative of most forms.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_08_22_145640) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_25_102427) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,6 +54,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_08_22_145640) do
     t.string "status", default: "trial"
     t.bigint "creator_id"
     t.bigint "upgrade_requester_id"
+    t.boolean "long_lists_enabled", default: false
     t.index ["creator_id"], name: "index_groups_on_creator_id"
     t.index ["external_id"], name: "index_groups_on_external_id", unique: true
     t.index ["name", "organisation_id"], name: "index_groups_on_name_and_organisation_id", unique: true
