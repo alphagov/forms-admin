c9bd6562a1e69bf5b987861dad2c8c40fc3c305a
Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>
2024-10-30 17:03:06 +0000

    Show new journey when "long_lists_enabled" is enabled
    When the "long_lists_enabled" flag is enabled for the groups, redirect
    to the new page to choose whether people can select only one or
    multiple options after the question text page.

--- app/controllers/pages/question_text_controller.rb
+++ app/controllers/pages/question_text_controller.rb
@@ -12,7 +12,11 @@ class Pages::QuestionTextController < PagesController
     @back_link_url = type_of_answer_new_path(current_form)
 
     if @question_text_input.submit
-      redirect_to selections_settings_new_path(current_form)
+      if current_form.group.long_lists_enabled
+        redirect_to long_lists_selection_type_new_path(current_form)
+      else
+        redirect_to selections_settings_new_path(current_form)
+      end
     else
       render :question_text, locals: { current_form: }
     end
