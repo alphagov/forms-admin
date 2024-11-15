dc41be0e9d42071c43a4591dee5633654d5d513a
Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>
2024-11-15 16:05:36 +0000

    Move authorisation to before_action
    Move authentication in the ReportsController to a before_action to
    reduce duplication as all actions require the same permissions.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,35 +1,33 @@
 class ReportsController < ApplicationController
+  before_action :check_user_has_permission
   after_action :verify_authorized
 
-  def index
-    authorize Report, :can_view_reports?
-  end
+  def index; end
 
   def features
-    authorize Report, :can_view_reports?
-
     data = Report.find("features")
 
     render template: "reports/features", locals: { data: }
   end
 
   def users
-    authorize Report, :can_view_reports?
-
     data = UsersReportService.new.user_data
 
     render locals: { data: }
   end
 
   def add_another_answer
-    authorize Report, :can_view_reports?
-
     data = Report.find("features")
 
     render template: "reports/add_another_answer", locals: { data: }
   end
 
   def last_signed_in_at
+  def last_signed_in_at; end
+
+private
+
+  def check_user_has_permission
     authorize Report, :can_view_reports?
   end
 end
