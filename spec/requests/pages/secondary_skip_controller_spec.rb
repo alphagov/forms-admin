fe553021224feed14fd32162fbf53258abcf6a07
Tom Iles <tom.iles@digital.cabinet-office.gov.uk>
2024-11-21 15:45:16 +0000

    Wrap #new tests in describe block
    Move the tests against the new actions into a describe block to match
    the other actions.

--- spec/requests/pages/secondary_skip_controller_spec.rb
+++ spec/requests/pages/secondary_skip_controller_spec.rb
@@ -37,61 +37,59 @@ RSpec.describe Pages::SecondarySkipController, type: :request do
     end
   end
 
-  context "when the branch_routing feature is not enabled", feature_branch_routing: false do
-    describe "#new" do
+  describe "#new" do
+    context "when the branch_routing feature is not enabled", feature_branch_routing: false do
       it "returns a 404" do
         get new_secondary_skip_path(form_id: 2, page_id: 1)
         expect(response).to have_http_status(:not_found)
       end
     end
-  end
 
-  context "when the branch_routing feature is enabled", :feature_branch_routing do
-    describe "#new" do
+    context "when the branch_routing feature is enabled", :feature_branch_routing do
       it "returns 200" do
         get new_secondary_skip_path(form_id: 2, page_id: 1)
         expect(response).to have_http_status(:success)
       end
-    end
 
-    context "when no condition exists on the page" do
-      let(:pages) do
-        build_list(:page, 5).each_with_index do |page, index|
-          page.id = index + 1
+      context "when no condition exists on the page" do
+        let(:pages) do
+          build_list(:page, 5).each_with_index do |page, index|
+            page.id = index + 1
+          end
+        end
+
+        it "redirects to the page list" do
+          get new_secondary_skip_path(form_id: 2, page_id: 1)
+          expect(response).to redirect_to(form_pages_path(form.id))
         end
       end
 
-      it "redirects to the page list" do
-        get new_secondary_skip_path(form_id: 2, page_id: 1)
-        expect(response).to redirect_to(form_pages_path(form.id))
-      end
-    end
-
-    context "when a secondary skip condition already exists on the page" do
-      let(:existing_secondary_skip) do
-        build(
-          :condition,
-          id: 2,
-          routing_page_id: pages[2].id,
-          check_page_id: pages[0].id,
-          goto_page_id: pages[4].id,
-          secondary_skip: true,
-        )
-      end
-
-      before do
-        pages[2].routing_conditions = [existing_secondary_skip]
-
-        ActiveResource::HttpMock.respond_to do |mock|
-          mock.get "/api/v1/forms/2", headers, form.to_json, 200
-          mock.get "/api/v1/forms/2/pages", headers, pages.to_json, 200
-          mock.get "/api/v1/forms/2/pages/1", headers, pages.first.to_json, 200
+      context "when a secondary skip condition already exists on the page" do
+        let(:existing_secondary_skip) do
+          build(
+            :condition,
+            id: 2,
+            routing_page_id: pages[2].id,
+            check_page_id: pages[0].id,
+            goto_page_id: pages[4].id,
+            secondary_skip: true,
+          )
         end
-      end
 
-      it "redirects to the show routes page" do
-        get new_secondary_skip_path(form_id: 2, page_id: 1)
-        expect(response).to redirect_to(show_routes_path(form_id: 2, page_id: 1))
+        before do
+          pages[2].routing_conditions = [existing_secondary_skip]
+
+          ActiveResource::HttpMock.respond_to do |mock|
+            mock.get "/api/v1/forms/2", headers, form.to_json, 200
+            mock.get "/api/v1/forms/2/pages", headers, pages.to_json, 200
+            mock.get "/api/v1/forms/2/pages/1", headers, pages.first.to_json, 200
+          end
+        end
+
+        it "redirects to the show routes page" do
+          get new_secondary_skip_path(form_id: 2, page_id: 1)
+          expect(response).to redirect_to(show_routes_path(form_id: 2, page_id: 1))
+        end
       end
     end
   end
