1e5bde14f7b5b49aaeda898bd2085a5b737433ee
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2024-11-29 09:00:32 +0200

    Update schema
    Ran `bin/rails db:migrate` to get the latest version of the database
    schema dump.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_24_094215) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_30_150000) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "conditions", force: :cascade do |t|
+    t.bigint "check_page_id", comment: "The question page this condition looks at to compare answers"
+    t.bigint "routing_page_id", comment: "The question page at which this conditional route takes place"
+    t.bigint "goto_page_id", comment: "The question page which this conditions will skip forwards to"
+    t.string "answer_value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.boolean "skip_to_end", default: false
+    t.index ["check_page_id"], name: "index_conditions_on_check_page_id"
+    t.index ["goto_page_id"], name: "index_conditions_on_goto_page_id"
+    t.index ["routing_page_id"], name: "index_conditions_on_routing_page_id"
+  end
 
   create_table "draft_questions", force: :cascade do |t|
     t.integer "form_id"
@@ -43,6 +56,33 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_24_094215) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["form_id"], name: "index_form_submission_emails_on_form_id"
+  end
+
+  create_table "forms", force: :cascade do |t|
+    t.text "name"
+    t.text "submission_email"
+    t.text "privacy_policy_url"
+    t.text "form_slug"
+    t.text "support_email"
+    t.text "support_phone"
+    t.text "support_url"
+    t.text "support_url_text"
+    t.text "declaration_text"
+    t.boolean "question_section_completed", default: false
+    t.boolean "declaration_section_completed", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "creator_id"
+    t.text "what_happens_next_markdown"
+    t.string "state"
+    t.string "payment_url"
+    t.string "external_id"
+    t.string "submission_type", default: "email", null: false
+    t.boolean "share_preview_completed", default: false, null: false
+    t.string "s3_bucket_name"
+    t.string "s3_bucket_aws_account_id"
+    t.string "s3_bucket_region"
+    t.index ["external_id"], name: "index_forms_on_external_id", unique: true
   end
 
   create_table "groups", force: :cascade do |t|
@@ -106,6 +146,23 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_24_094215) do
     t.index ["slug"], name: "index_organisations_on_slug", unique: true
   end
 
+  create_table "pages", force: :cascade do |t|
+    t.text "question_text"
+    t.text "hint_text"
+    t.text "answer_type"
+    t.integer "next_page"
+    t.boolean "is_optional", null: false
+    t.jsonb "answer_settings"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "form_id"
+    t.integer "position"
+    t.text "page_heading"
+    t.text "guidance_markdown"
+    t.boolean "is_repeatable", default: false, null: false
+    t.index ["form_id"], name: "index_pages_on_form_id"
+  end
+
   create_table "schema_info", id: false, force: :cascade do |t|
     t.integer "version", default: 0, null: false
   end
@@ -153,5 +210,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_24_094215) do
   add_foreign_key "mou_signatures", "organisations"
   add_foreign_key "mou_signatures", "users"
   add_foreign_key "organisations", "groups", column: "default_group_id"
+  add_foreign_key "pages", "forms"
   add_foreign_key "users", "organisations"
 end
