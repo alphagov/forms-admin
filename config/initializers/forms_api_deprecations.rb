25cd7401b8501e729d5e9cb19c377e566721d8b4
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2024-12-27 12:09:54 +0200

    wip! Add deprecation warning when http mock is used for forms model endpoint

--- config/initializers/forms_api_deprecations.rb
+++ config/initializers/forms_api_deprecations.rb
@@ -4,6 +4,14 @@ module FormsApiDeprecations
       @deprecator ||= ActiveSupport::Deprecation.new("v2", "GOV.UK Forms API")
     end
 
+    def warn_if_forms_model_path(path, message, callstack = nil)
+      callstack ||= caller_locations(2)
+
+      if forms_model_path?(path)
+        deprecator.warn(message, callstack)
+      end
+    end
+
     def warn_unless_called_from_repository(message, callstack)
       unless called_from_repository?(callstack)
         deprecator.warn(message, callstack)
@@ -12,6 +20,10 @@ module FormsApiDeprecations
 
     def called_from_repository?(callstack)
       callstack.any? { |location| location.path.match? %r{app/services/\w+_repository.rb$} }
+    end
+
+    def forms_model_path?(path)
+      path.starts_with? "/api/v1/forms"
     end
   end
 end
@@ -27,4 +39,18 @@ if Rails.env.local?
   end
 
   ActiveResource::Connection.prepend(ActiveResourceRequestMonkeypatch)
+
+  module ActiveResourceHttpMockMonkeypatch
+    %i[post patch put get delete head].each do |method|
+      module_eval <<-RUBY, __FILE__, __LINE__ + 1
+        def #{method}(path, ...)
+          FormsApiDeprecations.warn_if_forms_model_path(path, "Prefer to stub forms models by mocking the repository service instead of using HttpMock")
+
+          super
+        end
+      RUBY
+    end
+  end
+
+  ActiveResource::HttpMock::Responder.prepend(ActiveResourceHttpMockMonkeypatch)
 end
