5bf990dbf90e25e94d8545f6c35b66a014e4af64
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2024-12-31 14:23:56 +0200

    Fix failure message for matcher #have_been_updated_to
    When this matcher failed it was printing the original record as the
    expected response, when the expected response is actually the updated
    record. This commit fixes this.
    
    This commit also tweaks the formatting of the failure message slightly
    so that the array of requests is formatted in the same way as the
    expected response, to make visual comparison slightly easier.

--- spec/support/matchers/active_resource/helper_methods.rb
+++ spec/support/matchers/active_resource/helper_methods.rb
@@ -2,9 +2,10 @@ class HelperMethods
   def self.format_failure_message(resource, expected_request, _requests)
     msg = []
     msg << "Expected #{resource.class} to have been updated."
-    msg << "Expected #{expected_request} to have been made."
+    msg << "Expected request:"
+    msg << expected_request
     msg << "The following requests have been made:"
-    msg << ActiveResource::HttpMock.requests.to_s
+    msg << ActiveResource::HttpMock.requests.map(&:to_s)
     msg.join("\r\n")
   end
 end
