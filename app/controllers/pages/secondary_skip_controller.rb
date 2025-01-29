e4f1eb1b4869b8a592d0fe0db8d139cd634b1b0f
Tom Iles <tom.iles@digital.cabinet-office.gov.uk>
2025-01-29 20:11:59 +0000

    Change calls to FeatureService with branch_routing
    Every call to check if the branch_routing feature is enabled now
    includes the current form's group.
    
    This change will allow us to set the feature flag to be enabled per
    group rather than for every environment.

--- app/controllers/pages/secondary_skip_controller.rb
+++ app/controllers/pages/secondary_skip_controller.rb
@@ -79,7 +79,7 @@ private
   end
 
   def ensure_branch_routing_feature_enabled
-    raise ActionController::RoutingError, "branch_routing feature not enabled" unless Settings.features.branch_routing
+    raise ActionController::RoutingError, "branch_routing feature not enabled" unless FeatureService.new(group: current_form.group).enabled?(:branch_routing)
   end
 
   def ensure_page_has_skip_condition
