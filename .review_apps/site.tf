21f85373f9580daab257ecb73499f0c20e2cc731
Andy Hunt <andy.hunt@digital.cabinet-office.gov.uk>
2025-01-31 14:44:55 +0000

    Introduce .review_apps directory
    The .review_apps directory will contain Terraform code for deploying a review
    copy of forms-admin. For now, it contains Terraform version cofiguration and a
    small amount of documentation in README.md.

---
+++ .review_apps/site.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_version = "~> 1.9.8"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.72.1"
+    }
+  }
+
+  backend "s3" {
+    bucket = "gds-forms-integration-tfstate"
+    region = "eu-west-2"
+    # key is set when initializing Terraform
+    # e.g. `terraform init -backend-config="key=review-apps/forms-admin/pr-123.tfstate"`
+  }
+}
+
+provider "aws" {
+  allowed_account_ids = ["842676007477"]
+  region              = "eu-west-2"
+
+  default_tags = {
+    tags = {
+      Environment = "review"
+      Deployment  = "github.com/alphagov/forms-admin/.review_apps"
+    }
+  }
+}
