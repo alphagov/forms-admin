7b223e91da3756c6a4d31c43f58d6df3f244e16f
Andy Hunt <andy.hunt@digital.cabinet-office.gov.uk>
2025-01-31 15:08:40 +0000

    .review_apps: fetch "review" Teraform state file from the state file bucket
    `integration/review` from `forms-deploy` exports a bunch of values, like subnet
    ids and security groups, which will be needed by `.review_apps` to deploy to
    AWS ECS.

---
+++ .review_apps/dependencies.tf
@@ -0,0 +1,24 @@
+##
+# Terraform remote state data resources are
+# used to read the content of a Terraform state
+# file.
+#
+# This is common pattern in the `forms-deploy`
+# codebase, and is used to share information
+# between different Terraform roots without
+# having to do any external wiring of outputs
+# to inputs.
+#
+# In this instance, we will be sharing things
+# like the subnet and security groups ids that
+# are necessary for deploying to AWS ECS. 
+##
+data "terraform_remote_state" "review" {
+  backend = "s3"
+
+  config = {
+    key            = "review.tfstate"
+    bucket         = "gds-forms-integration-tfstate"
+    region         = "eu-west-2"
+  }
+}
