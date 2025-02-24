0c752b68ea422ec6b4250156a062d43d78cce60b
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2025-02-24 11:36:10 +0200

    Remove unneeded #conditions_for_page_with_index method

--- app/components/page_list_component/view.rb
+++ app/components/page_list_component/view.rb
@@ -62,35 +62,9 @@ module PageListComponent
       page_position(check_page)
     end
 
-    def conditions_for_page_with_index(page)
-      routing_conditions_with_index.fetch(page.id, [])
-    end
-
-    def routing_conditions_with_index
-      @routing_conditions_with_index ||= process_routing_conditions
-    end
-
     def skip_condition_route_page_text(condition)
       routing_page = @pages.find { |page| page.id == condition.routing_page_id }
       I18n.t("page_conditions.skip_condition_route_page_text", route_page_question_text: routing_page.question_text, route_page_question_number: routing_page.position)
-    end
-
-    # Create hash of page_id => [condition, index]
-    # where index is the index of the condition in the array of conditions for
-    # the page referenced by check_page_id
-    def process_routing_conditions
-      all_form_conditions = @pages.flat_map(&:routing_conditions).compact_blank
-
-      all_form_conditions
-        .group_by(&:check_page_id)
-        .values
-        .flat_map { |conditions|
-          conditions.map.with_index(1) do |condition, index|
-            [condition.routing_page_id, [condition, index]] # inclde routing_page_id, so we can group by it
-          end
-        }
-        .group_by(&:first)
-        .transform_values { |pairs| pairs.map(&:last) } # drop routing_page_id from the value of the hash - it is now the key
     end
   end
 end
