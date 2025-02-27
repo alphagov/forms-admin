eebb2aee4ac7475eb449e7caed1ea77cd3ed9d41
Tom Iles <tom.iles@digital.cabinet-office.gov.uk>
2025-02-27 16:33:57 +0000

    Refactor MailchimpListSynchronizer class
    The current MailchimpListSynchronizer#synchronize method takes an array
    of email addresses to update the mailchimp list.
    
    It uses sets to decide which emails need to be added to mailchimp and
    which need to be removed.
    
    Because we plan to add other information to the mailchimp lists, such as
    role, the method needs to change.
    
    This refactor introduces a MailchimpMember class to hold the email
    address and status.
    
    This allows it to be extended with other attributes in the future.
    
    The logic for updating and removing members has been kept but will allow
    updating records where attributes have changed in the future.

--- lib/mailchimp_list_synchronizer.rb
+++ lib/mailchimp_list_synchronizer.rb
@@ -1,93 +1,138 @@
 require "digest"
 require "MailchimpMarketing"
 
+MailchimpMember = Data.define(:email, :status) do
+  def unsubscribed?
+    status == "unsubscribed"
+  end
+
+  def archivable?
+    %w[subscribed cleaned pending transactional].include?(status)
+  end
+
+  def subscriber_hash
+    Digest::MD5.hexdigest email.downcase
+  end
+end
+
 class MailchimpListSynchronizer
-  attr_reader :list_id
+  attr_reader :client, :list_id
+
+  PAGE_SIZE = 1000
 
   def initialize(list_id:)
     @list_id = list_id
+    @client = setup_client(Settings.mailchimp.api_key, Settings.mailchimp.api_prefix)
+  end
+
+  def setup_client(api_key, server)
+    MailchimpMarketing::Client.new.tap do |client|
+      client.set_config(
+        api_key: api_key,
+        server: server,
+      )
+    end
   end
 
   def synchronize(users_to_synchronize:)
-    mailchimp_api_key = Settings.mailchimp.api_key
-    mailchimp_server_prefix = Settings.mailchimp.api_prefix
-    puts "Mailchimp server prefix: #{mailchimp_server_prefix}"
-    mailchimp = MailchimpMarketing::Client.new
-    mailchimp.set_config({
-      api_key: mailchimp_api_key,
-      server: mailchimp_server_prefix,
-    })
-    puts "List id: #{list_id}"
+    desired_members = users_to_synchronize.map { |email| MailchimpMember.new(email: email, status: "subscribed") }
 
-    target_list = mailchimp.lists.get_list(list_id, include_total_contacts: true)
-    target_list or raise
+    Rails.logger.debug "Mailchimp server prefix: #{Settings.mailchimp.api_prefix}"
+    Rails.logger.debug "List id: #{list_id}"
 
-    puts "Found Mailchimp list: #{target_list['name']}"
-    puts "Mailchimp list has #{target_list['stats']['member_count']} active members"
-    puts "Mailchimp list has #{target_list['stats']['total_contacts']} total members"
+    desired_members_hash = desired_members.index_by(&:subscriber_hash)
+    existing_members_hash = fetch_all_members.index_by(&:subscriber_hash)
 
-    currently_subscribed_members = []
-    members_who_can_be_archived = []
-    unsubscribed_members = []
+    add_and_update_members(existing_members_hash, desired_members_hash)
+    archive_removed_members(existing_members_hash, desired_members_hash)
+  end
 
-    # Set up API pagination
-    total_list_size = target_list["stats"]["total_contacts"]
+  def add_and_update_members(existing_members, desired_members)
+    Rails.logger.debug "Subscribing any new users..."
+    members_to_update = []
+
+    desired_members.each do |subscriber_hash, desired_member|
+      existing_member = existing_members[subscriber_hash]
+
+      next if existing_member&.unsubscribed?
+      next if existing_member == desired_member
+
+      members_to_update << desired_member
+    end
+
+    Rails.logger.debug "There are #{members_to_update} to subscribe"
+
+    members_to_update.each do |member|
+      update_member(member)
+    end
+  end
+
+  def archive_removed_members(existing_members, desired_members)
+    Rails.logger.debug "Archiving any removed users..."
+    members_to_remove = []
+
+    existing_members.each do |subscriber_hash, existing_member|
+      if !desired_members[subscriber_hash] && existing_member.archivable?
+        members_to_remove << existing_member
+      end
+    end
+
+    Rails.logger.debug "There are #{members_to_remove.size} to archive"
+
+    members_to_remove.each do |member|
+      remove_member(member)
+    end
+  end
+
+  def fetch_all_members
+    return enum_for(:fetch_all_members) unless block_given?
+
     offset = 0
-    page_size = 1000 # maximum page size is 1000 results, default is 10
+    mailchimp_list = client.lists.get_list(list_id, include_total_contacts: true)
+    total_size = mailchimp_list["stats"]["total_contacts"]
 
-    while offset < total_list_size
-      api_response = mailchimp.lists.get_list_members_info(list_id, count: page_size, offset:)
+    Rails.logger.debug "Found Mailchimp list: #{mailchimp_list['name']}"
+    Rails.logger.debug "Mailchimp list has #{mailchimp_list['stats']['member_count']} active members"
+    Rails.logger.debug "Mailchimp list has #{mailchimp_list['stats']['total_contacts']} total members"
 
-      api_response["members"].each do |member|
-        currently_subscribed_members.push(member["email_address"]) if member["status"] == "subscribed"
-        members_who_can_be_archived.push(member["email_address"]) if %w[subscribed cleaned pending transactional].include?(member["status"])
-        unsubscribed_members.push(member["email_address"]) if member["status"] == "unsubscribed"
+    while offset < total_size
+      response = client.lists.get_list_members_info(list_id, count: PAGE_SIZE, offset: offset)
+
+      response["members"].each do |member_data|
+        yield MailchimpMember.new(
+          email: member_data["email_address"],
+          status: member_data["status"],
+        )
       end
 
-      offset += page_size
+      offset += PAGE_SIZE
     end
+  end
 
-    users_to_synchronize_set = users_to_synchronize
-                             .to_set
+  def update_member(member)
+    member_data = {
+      "email_address" => member.email,
+      "status" => member.status,
+    }
 
-    currently_subscribed_members_set = currently_subscribed_members
-                             .to_set
+    client.lists.set_list_member(
+      list_id,
+      member.subscriber_hash,
+      member_data,
+    )
+  rescue MailchimpMarketing::ApiError => e
+    log_mailchimp_error("subscribe", member.subscriber_hash, e)
+  end
 
-    unsubscribed_members_set = unsubscribed_members.to_set
+  def remove_member(member)
+    client.lists.delete_list_member(list_id, member.subscriber_hash)
+  rescue MailchimpMarketing::ApiError => e
+    log_mailchimp_error("archive", member.subscriber_hash, e)
+  end
 
-    members_who_can_be_archived_set = members_who_can_be_archived.to_set
-
-    users_to_subscribe = (users_to_synchronize_set - currently_subscribed_members_set) - unsubscribed_members_set
-    users_to_archive = members_who_can_be_archived_set - users_to_synchronize_set
-
-    puts "There are #{users_to_subscribe.size} to subscribe"
-    puts "There are #{users_to_archive.size} to archive"
-
-    puts "Subscribing any new users..."
-    users_to_subscribe.each do |email|
-      subscriber_hash = Digest::MD5.hexdigest email.downcase
-      mailchimp.lists.set_list_member(
-        list_id,
-        subscriber_hash,
-        {
-          "email_address" => email,
-          "status" => "subscribed",
-        },
-      )
-    rescue MailchimpMarketing::ApiError => e
-      warn "Could not subscribe user with subscriber hash #{subscriber_hash} to list #{list_id}."
-      warn "#{e.title}: #{e.detail}"
-      warn "Continuing"
-    end
-
-    puts "Archiving any removed users..."
-    users_to_archive.each do |email|
-      subscriber_hash = Digest::MD5.hexdigest email.downcase
-      mailchimp.lists.delete_list_member(list_id, subscriber_hash)
-    rescue MailchimpMarketing::ApiError => e
-      warn "Could not archive user with subscriber hash #{subscriber_hash} from list #{list_id}"
-      warn "#{e.title}: #{e.detail}"
-      warn "Continuing"
-    end
+  def log_mailchimp_error(action, subscriber_hash, error)
+    warn "Could not #{action} user with subscriber hash #{subscriber_hash} from list #{list_id}"
+    warn "#{error.title}: #{error.detail}"
+    warn "Continuing"
   end
 end
