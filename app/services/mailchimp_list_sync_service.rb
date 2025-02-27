88d0201199d73e7ea48bc33bb58c6e78cc166622
Tom Iles <tom.iles@digital.cabinet-office.gov.uk>
2025-02-27 15:27:29 +0000

    Reformat mailchimp_list_sync_service
    Split out the code for selecting users who need to sync with mailchimp
    into methods.
    
    This will make changing these methods easier in the future.

--- app/services/mailchimp_list_sync_service.rb
+++ app/services/mailchimp_list_sync_service.rb
@@ -1,15 +1,25 @@
 class MailchimpListSyncService
   def synchronize_lists
     Rails.logger.debug "Synchronizing active users mailing list"
-    active_users_list = Settings.mailchimp.active_users_list
-    active_user_email_addresses = User.where(has_access: true).pluck(:email)
 
-    MailchimpListSynchronizer.synchronize(list_id: active_users_list, users_to_synchronize: active_user_email_addresses)
+    MailchimpListSynchronizer.synchronize(
+      list_id: Settings.mailchimp.active_users_list,
+      users_to_synchronize: active_users.pluck(:email),
+    )
 
     Rails.logger.debug "Synchronizing MOU signers mailing list"
-    mou_signers_list = Settings.mailchimp.mou_signers_list
-    mou_signer_email_addresses = MouSignature.all.map(&:user).filter { |user| user.has_access == true }.pluck(:email)
 
-    MailchimpListSynchronizer.synchronize(list_id: mou_signers_list, users_to_synchronize: mou_signer_email_addresses)
+    MailchimpListSynchronizer.synchronize(
+      list_id: Settings.mailchimp.mou_signers_list,
+      users_to_synchronize: mou_signers.pluck(:email),
+    )
+  end
+
+  def active_users
+    User.where(has_access: true)
+  end
+
+  def mou_signers
+    MouSignature.all.map(&:user).filter { |user| user.has_access == true }
   end
 end
