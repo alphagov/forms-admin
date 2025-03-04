889ca2ff05c715de47ac4aa08f7dc402fe86ab6d
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2025-03-04 16:51:46 +0000

    Add global feature flag for file upload
    This should make turning the feature on easier - rather than running a
    task to enable it for all of the groups, we can turn it on globally with
    an environment variable.
    
    This also means that if we need to turn the feature off again, we won't
    have to reconfigure any test groups.
    modified:   config/settings.yml #	modified:
    spec/requests/pages/type_of_answer_controller_spec.rb #

--- spec/requests/pages/type_of_answer_controller_spec.rb
+++ spec/requests/pages/type_of_answer_controller_spec.rb
@@ -37,16 +37,32 @@ RSpec.describe Pages::TypeOfAnswerController, type: :request do
     end
 
     context "when file upload is disabled for the group" do
-      it "does not show the file answer type option" do
-        expect(response.body).not_to include("File")
+      context "when the file upload feature is not enabled globally", feature_file_upload: false do
+        it "does not show the file answer type option" do
+          expect(response.body).not_to include("File")
+        end
+      end
+
+      context "when the file upload feature is enabled globally", :feature_file_upload do
+        it "shows the file answer type option" do
+          expect(response.body).to include("File")
+        end
       end
     end
 
     context "when file upload is enabled for the group" do
       let(:file_upload_enabled) { true }
 
-      it "shows the file answer type option" do
-        expect(response.body).to include("File")
+      context "when the file upload feature is not enabled globally", feature_file_upload: false do
+        it "shows the file answer type option" do
+          expect(response.body).to include("File")
+        end
+      end
+
+      context "when the file upload feature is enabled globally", :feature_file_upload do
+        it "shows the file answer type option" do
+          expect(response.body).to include("File")
+        end
       end
     end
   end
