b8b38fcce4f462c60c8306f96fd3b6334bbb1e45
Tom Iles <tom.iles@digital.cabinet-office.gov.uk>
2025-03-18 13:41:13 +0000

    Show routing error summaries
    Co-authored-by: Samuel Culley <samuel.culley@digital.cabinet-office.gov.uk>

--- app/controllers/pages/routes_controller.rb
+++ app/controllers/pages/routes_controller.rb
@@ -3,11 +3,11 @@ class Pages::RoutesController < PagesController
     back_link_url = form_pages_path(current_form.id)
     pages = FormRepository.pages(current_form)
     routes = PageRoutesService.new(form: current_form, pages:, page:).routes
-
+    errors = routes.flat_map { |route| route.validation_errors.map { |validation_error| error_construct(route: route, validation_error: validation_error) } }
     # to be eligible for route question has to have one question after it, so should always have next_page
     next_page = pages.find(proc { raise "Cannot find page with id #{page.next_page.inspect}" }) { _1.id == page.next_page }
 
-    render locals: { current_form:, page:, pages:, next_page:, routes:, back_link_url: }
+    render locals: { current_form:, page:, pages:, next_page:, routes:, back_link_url:, errors: }
   end
 
   def delete
@@ -33,5 +33,28 @@ private
 
   def delete_confirmation_input_params
     params.require(:pages_routes_delete_confirmation_input).permit(:confirm).merge(form: current_form, page: page)
+  end
+
+  def error_construct(route:, validation_error:)
+    case validation_error.name
+    when "answer_value_doesnt_exist"
+      OpenStruct.new(link: "#check-#{route.id}", message: I18n.t("page_route_card.errors.answer_value_doesnt_exist"))
+    when "cannot_route_to_next_page"
+      if route.secondary_skip?
+        OpenStruct.new(link: "#goto-#{route.id}", message: I18n.t("page_route_card.errors.cannot_route_to_next_page_secondary_skip"))
+      else
+        OpenStruct.new(link: "#goto-#{route.id}", message: I18n.t("page_route_card.errors.cannot_route_to_next_page"))
+      end
+    when "cannot_have_goto_page_before_routing_page"
+      if route.secondary_skip?
+        OpenStruct.new(link: "#goto-#{route.id}", message: I18n.t("page_route_card.errors.cannot_have_goto_page_before_routing_page_secondary_skip"))
+      else
+        OpenStruct.new(link: "#goto-#{route.id}", message: I18n.t("page_route_card.errors.cannot_have_goto_page_before_routing_page", question_number: question_number(route.check_page_id)))
+      end
+    end
+  end
+
+  def question_number(page_id)
+    current_form.pages.find { |page| page.id == page_id }.position
   end
 end
