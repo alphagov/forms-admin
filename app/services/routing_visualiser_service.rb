72f2adcf2247ccab4d999b22cd0eb31bf91e9670
Andy Hunt <andy.hunt@digital.cabinet-office.gov.uk>
2025-04-02 14:57:41 +0100

    Replace Graphviz graph language with Mermaid
    Mermaid produces a nicer output that will be easier to style.
    
    Also makes the code a bit easier to make sense of by separating understanding
    the graph from translating it to another language.

--- app/services/routing_visualiser_service.rb
+++ app/services/routing_visualiser_service.rb
@@ -1,82 +1,192 @@
 # frozen_string_literal: true
 require_relative "../helpers/application_helper"
-require 'open3'
+require "open3"
+
+class QuestionGraph
+  attr_accessor :root
+
+  def initialize(root)
+    @root = root
+  end
+end
+
+class Node
+  include ApplicationHelper
+
+  attr_accessor :id, :text, :children, :page
+
+  def initialize(page)
+    @children = []
+    @id = page.id
+    @text = question_text_with_optional_suffix(page)
+    @page = page
+  end
+end
+
+class Edge
+  attr_accessor :reason, :node
+
+  def initialize(node, reason)
+    @node = node
+    @reason = reason
+  end
+end
 
 class RoutingVisualiserService
   class << self
-    include ApplicationHelper
     def generate(form)
-      dot_file_lines = [
-        "digraph g {",
-        "splines=ortho",
-        "node [shape=box, rankjustify=min]",
-        "start[label=Start]",
-        "end[label=\"Check your answers before submitting\"]",
-      ]
+      graph = build_graph(form)
 
-      next_page_ids = {}
+      to_visit = [graph.root]
+
+      diagram_lines = [
+        "flowchart TD",
+      ]
+      visited_node_ids =[]
+
+      while to_visit.any?
+        node = to_visit.shift
+
+        next if visited_node_ids.include?(node.id)
+
+        visited_node_ids << node.id
+
+        diagram_lines << "page_#{node.id}[\"#{node.text}\"]"
+
+        node.children.each do |edge|
+          to_visit << edge.node
+
+          # rubocop:disable Style/ConditionalAssignment cleaner with a simple if
+          if edge.reason.nil?
+            diagram_lines << "page_#{node.id} --> page_#{edge.node.id}"
+          else
+            diagram_lines << "page_#{node.id} -- #{edge.reason} --> page_#{edge.node.id}"
+          end
+          # rubocop:enable Style/ConditionalAssignment
+        end
+      end
+
+      diagram_lines.join("\n")
+    end
+
+  private
+
+    def build_graph(form)
+      # Page ids and indices in the array are different
+      # but the convention is that barring any routing rules
+      # the next page in the array is the next question
+      #
+      # Build up a map of id -> id to make it easy to look
+      # up what the next question id WOULD be.
+      next_page_ids = []
       form.pages.each_index do |idx|
         page = form.pages[idx]
-        dot_file_lines << "page_#{page.id}[label=\"#{question_text_with_optional_suffix(page)}\"]"
-
-        if idx.zero?
-          dot_file_lines << "start -> page_#{page.id}"
-        end
-
-
         if idx < form.pages.count - 1
           next_page = form.pages[idx + 1]
           next_page_ids[page.id] = next_page.id
         end
       end
 
-      # rubocop:disable Style/CombinableLoops
-      # subsequent loops are important because all the nodes need defining upfront
-      form.pages.each do |page|
-        # This is not part of a route
-        # and goes straight to the next question
-        if page.routing_conditions.empty?
-          next_page_id = next_page_ids[page.id]
+      nodes = form.pages.to_h do |page|
+        [page.id, Node.new(page)]
+      end
 
-          # This is the end of the form
-          # rubocop:disable Style/ConditionalAssignment
-          if next_page_id.nil?
-            dot_file_lines << "page_#{page.id} -> end"
-          else
-            dot_file_lines << "page_#{page.id} -> page_#{next_page_ids[page.id]}"
+      check_your_answers_page = OpenStruct.new(
+        id: 9999,
+        show_optional_suffix: false,
+        question_text: "Check your answers",
+        routing_conditions: [],
+      )
+
+      check_your_answers_node = Node.new(check_your_answers_page)
+
+      root_node = nil
+
+      ## Go through all nodes and attach their children
+      nodes.each_pair do |id, node|
+        if root_node.nil?
+          root_node = node
+        end
+
+        if !has_routing(node.page)
+          next_q = nodes[next_page_ids[node.id]]
+          if next_q
+            node.children << Edge.new(next_q, nil)
           end
-          # rubocop:enable Style/ConditionalAssignment
         else
-          page.routing_conditions.each do |condition|
-            # Go straight to the end
-            if condition.goto_page_id.nil? && !condition.exit_page_markdown.nil? # This is an exit page
-              dot_file_lines << "exit_page_from_page_#{page.id}[label=\"#{condition.exit_page_markdown}\"]"
-              dot_file_lines << "page_#{page.id} -> exit_page_from_page_#{page.id}[xlabel=\"If the answer is '#{condition.answer_value}\'\"]"
+          node.page.routing_conditions.each do |condition|
+            if is_exit_page(condition)
+              exit_page_node = exit_page_node(id, condition.exit_page_markdown)
+              node.children << Edge.new(exit_page_node, branch_label(condition))
 
-              next_page_id = next_page_ids[page.id]
-              dot_file_lines << "page_#{page.id} -> page_#{next_page_id}[xlabel=\"Any other answer\"]"
-            elsif condition.answer_value.nil? # This is the end of the branch
-              dot_file_lines << "page_#{condition.routing_page_id} -> page_#{condition.goto_page_id}"
+              next_q = nodes[next_page_ids[node.id]]
+              if next_q
+                node.children << Edge.new(next_q, default_label)
+              end
+            elsif is_end_of_branch(condition)
+              end_of_branch_node = nodes[condition.goto_page_id]
+              node.children << Edge.new(end_of_branch_node, nil)
             else
-              # This is the start of the branch
-              branch_label = "If the answer is '#{condition.answer_value}\'"
+              # Start of a branch
+              branch_question = condition.skip_to_end ? check_your_answers_node : nodes[condition.goto_page_id]
+              node.children << Edge.new(branch_question, branch_label(condition))
 
-              branch_target_node_id = condition.skip_to_end ? "end" : "page_#{condition.goto_page_id}"
-
-              dot_file_lines << "page_#{condition.routing_page_id} -> #{branch_target_node_id}[xlabel=\"#{branch_label}\"]"
-              dot_file_lines << "page_#{condition.routing_page_id} -> page_#{next_page_ids[page.id]}[xlabel=\"Any other answer\"]"
-
-              dot_file_lines << "{rank=same; #{branch_target_node_id}; page_#{next_page_ids[page.id]}}"
+              next_q = nodes[next_page_ids[node.id]]
+              if next_q
+                node.children << Edge.new(next_q, default_label)
+              end
             end
           end
         end
       end
-      # rubocop:enable Style/CombinableLoops
 
-      dot_file_lines << "}"
-      dot_file = dot_file_lines.join("\n")
-      stdout, status = Open3.capture2("dot -Tdot", stdin_data: dot_file)
-      stdout
+      # Go through all nodes and attach any non-exit-page leaf nodes to the check your answers node
+      nodes.each_value do |node|
+        next if is_exit_page_node(node)
+        next unless node.children.empty?
+
+        node.children << Edge.new(check_your_answers_node, nil)
+      end
+
+      QuestionGraph.new(root_node)
+    end
+
+    def has_routing(page)
+      !page.routing_conditions.empty?
+    end
+
+    def is_exit_page(condition)
+      condition.goto_page_id.nil? && !condition.exit_page_markdown.nil?
+    end
+
+    def is_end_of_branch(condition)
+      condition.answer_value.nil?
+    end
+
+    def is_exit_page_node(node)
+      if node.id.is_a? String
+        node.id.starts_with? "exit_page_id"
+      end
+
+      false
+    end
+
+    def branch_label(condition)
+      "If the answer is '#{condition.answer_value}'"
+    end
+
+    def default_label
+      "Any other answer"
+    end
+
+    def exit_page_node(page_id, exit_page_markdown)
+      page = OpenStruct.new(
+        id: "exit_page_id_#{page_id}",
+        question_text: exit_page_markdown,
+        show_optional_suffix: false,
+        routing_conditions: [],
+      )
+      Node.new(page)
     end
   end
 end
