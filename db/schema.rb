a053a5e2268960e3a10de49e212517024238e989
Tom Iles <tom.iles@digital.cabinet-office.gov.uk>
2025-04-04 16:01:44 +0100

    Add research contact status to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_02_115233) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_03_131603) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -189,6 +189,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_02_115233) do
     t.string "provider"
     t.datetime "terms_agreed_at"
     t.datetime "last_signed_in_at"
+    t.string "research_contact_status", default: "not_asked"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["organisation_id"], name: "index_users_on_organisation_id"
     t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
