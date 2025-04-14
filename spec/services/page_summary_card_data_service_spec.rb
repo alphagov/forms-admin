8e4958bead74eda2fbb2ad45c1d07e77defa4dfd
Tom Iles <tom.iles@digital.cabinet-office.gov.uk>
2025-04-14 15:25:05 +0100

    Add page numbers to live page view
    Add page numbers to the summary of a page in the live view.

--- spec/services/page_summary_card_data_service_spec.rb
+++ spec/services/page_summary_card_data_service_spec.rb
@@ -4,8 +4,7 @@ describe PageSummaryCardDataService do
   let(:page) { build :page, is_optional: }
   let(:is_optional) { false }
   let(:pages) do
-    form = build :form, :with_pages
-    form.pages
+    [page, *build_list(:page, 5)]
   end
   let(:service) { described_class.call(page:, pages:) }
 
@@ -15,7 +14,7 @@ describe PageSummaryCardDataService do
     end
 
     it "includes a title" do
-      expect(service.build_data[:card][:title]).to eq page.question_text
+      expect(service.build_data[:card][:title]).to eq "1. #{page.question_text}"
     end
 
     it "includes an array of rows" do
@@ -26,7 +25,7 @@ describe PageSummaryCardDataService do
       let(:page) { build :page, :with_selection_settings, is_optional: }
 
       it "includes a title without (optional) added to it" do
-        expect(service.build_data[:card][:title]).to eq page.question_text.to_s
+        expect(service.build_data[:card][:title]).to eq "1. #{page.question_text}"
       end
     end
 
@@ -34,14 +33,14 @@ describe PageSummaryCardDataService do
       let(:is_optional) { "true" }
 
       it "includes a title with (optional) added to it" do
-        expect(service.build_data[:card][:title]).to eq "#{page.question_text} (optional)"
+        expect(service.build_data[:card][:title]).to eq "1. #{page.question_text} (optional)"
       end
 
       context "when the page is a selection question" do
         let(:page) { build :page, :with_selection_settings, is_optional: }
 
         it "includes a title without (optional) added to it" do
-          expect(service.build_data[:card][:title]).to eq page.question_text.to_s
+          expect(service.build_data[:card][:title]).to eq "1. #{page.question_text}"
         end
       end
     end
@@ -50,14 +49,14 @@ describe PageSummaryCardDataService do
       let(:is_optional) { nil }
 
       it "includes a title" do
-        expect(service.build_data[:card][:title]).to eq page.question_text
+        expect(service.build_data[:card][:title]).to eq "1. #{page.question_text}"
       end
 
       context "when the page is a selection question" do
         let(:page) { build :page, :with_selection_settings, is_optional: }
 
         it "includes a title without (optional) added to it" do
-          expect(service.build_data[:card][:title]).to eq page.question_text.to_s
+          expect(service.build_data[:card][:title]).to eq "1. #{page.question_text}"
         end
       end
     end
