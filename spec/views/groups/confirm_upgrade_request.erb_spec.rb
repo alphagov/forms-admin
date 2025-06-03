65abf59daf89fee22698cbbf4cde81492ebbbebf
Tom Iles <tom.iles@digital.cabinet-office.gov.uk>
2025-06-03 10:48:05 +0100

    Update text on upgrade request page
    New content for upgrade request page which now includes a list of the
    group's organisation's admin emails.

--- spec/views/groups/confirm_upgrade_request.erb_spec.rb
+++ spec/views/groups/confirm_upgrade_request.erb_spec.rb
@@ -1,7 +1,12 @@
 require "rails_helper"
 
 describe "groups/confirm_upgrade_request.html.erb" do
-  let(:group) { create(:group) }
+  let(:admin_email) { "admin_user@example.gov.uk" }
+  let(:group) do
+    create(:group, :org_has_org_admin) do |group|
+      group.organisation.admin_users.first.update(email: admin_email)
+    end
+  end
 
   before do
     assign(:group, group)
@@ -18,5 +23,9 @@ describe "groups/confirm_upgrade_request.html.erb" do
 
   it "renders the group name in the heading caption" do
     expect(rendered).to have_css(".govuk-caption-l", text: group.name)
+  end
+
+  it "renders a list of the group's organisation's admin emails" do
+    expect(rendered).to have_css("ul.govuk-list.govuk-list--bullet li", text: admin_email)
   end
 end
