59e33eece8a54cd03bacad55a68798b0c9780b2d
alice-carr <alice.carr@digital.cabinet-office.gov.uk>
2025-06-10 16:16:43 +0100

    Include page heading from guidance in the read only file upload question when the form is live
    When a file upload question has guidance, we want to include the guidance page header in the title of the card so that it provides context to the question text

--- app/services/page_summary_card_data_service.rb
+++ app/services/page_summary_card_data_service.rb
@@ -24,6 +24,14 @@ class PageSummaryCardDataService
 private
 
   def build_title
+    # if a file upload question has guidance text, we want to display the guidance text in the title. Otherwise we display the question text
+    if @page.answer_type == "file" && @page.guidance_markdown.present?
+      if @page.is_optional?
+        return "#{page_number(@page)}. #{@page.page_heading} (optional)"
+      end
+      return "#{page_number(@page)}. #{@page.page_heading}" if @page.answer_type == "file" && @page.guidance_markdown.present?
+    end
+
     return "#{page_number(@page)}. #{@page.question_text}" unless @page.is_optional? && @page.answer_type != "selection"
 
     "#{page_number(@page)}. #{@page.question_text} (optional)"
