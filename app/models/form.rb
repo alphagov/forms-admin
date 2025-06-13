c09d1adefeea737a474b65fdbbe0db8fd9a20929
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2025-06-13 11:21:32 +0300

    Add enums
    We need to make sure that enum values are validated in the forms-admin
    database; this commit lets ActiveRecord know about them.

--- app/models/form.rb
+++ app/models/form.rb
@@ -1,5 +1,12 @@
 class Form < ApplicationRecord
   has_many :pages, -> { order(position: :asc) }, dependent: :destroy
+
+  enum :submission_type, {
+    email: "email",
+    email_with_csv: "email_with_csv",
+    s3: "s3",
+  }
+
   after_create :set_external_id
 
 private
