d99dadaaec24895a7adcf701e5f52d96f04d52e4
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2025-06-16 15:14:13 +0300

    Fix Report model
    When `ActiveResource::Base` encounters nested attributes in a JSON
    response, it tries to instantiate a class with the classified name of
    the parent attribute; i.e. when it sees `form: {...}` in a report
    response it tries to instantiate a `Form` object. When we didn't have a
    Form class this was fine (because ActiveResource falls back to creating
    a new class and then instantiating that), but now we have added the
    `Form` model it's trying to use that class , which doesn't work because
    that class is not designed for use with ActiveResource.
    
    Instead we create another `Form` class namespaced to `Report`,
    which will shadow the Form model and work as expected.

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,4 +3,9 @@ class Report < ActiveResource::Base
   self.prefix = "/api/v1/"
   self.include_format_in_path = false
   headers["X-API-Token"] = Settings.forms_api.auth_key
+
+  class Form < ActiveResource::Base
+    self.site = Report.site
+    self.prefix = Report.prefix
+  end
 end
