1391ebe223820c94d0ae832dbb8f86b613478ff6
Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>
2025-06-19 16:48:55 +0100

    Update redirect for security.txt file
    We are no longer part of the Cabinet Office so update the redirect to
    go to the general top-level government security.txt file

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "/up" => "rails/health#show", as: :rails_health_check
 
-  get "/security.txt" => redirect("https://vdp.cabinetoffice.gov.uk/.well-known/security.txt")
-  get "/.well-known/security.txt" => redirect("https://vdp.cabinetoffice.gov.uk/.well-known/security.txt")
+  get "/security.txt" => redirect("https://vulnerability-reporting.service.security.gov.uk/.well-known/security.txt")
+  get "/.well-known/security.txt" => redirect("https://vulnerability-reporting.service.security.gov.uk/.well-known/security.txt")
 
   root "groups#index"
 
