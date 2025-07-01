1bbe80d5975c85f3c5e5660c0d575de18581e382
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2025-07-01 15:08:38 +0300

    Add replication to database for page repository

--- app/services/page_repository.rb
+++ app/services/page_repository.rb
@@ -1,7 +1,9 @@
 class PageRepository
   class << self
     def find(page_id:, form_id:)
-      Api::V1::PageResource.find(page_id, params: { form_id: })
+      page = Api::V1::PageResource.find(page_id, params: { form_id: })
+      save_to_database!(page)
+      page
     end
 
     def create!(form_id:,
@@ -13,21 +15,26 @@ class PageRepository
                 page_heading:,
                 guidance_markdown:,
                 answer_type:)
-      Api::V1::PageResource.create!(form_id:,
-                                    question_text:,
-                                    hint_text:,
-                                    is_optional:,
-                                    is_repeatable:,
-                                    answer_settings:,
-                                    page_heading:,
-                                    guidance_markdown:,
-                                    answer_type:)
+      page = Api::V1::PageResource.create!(
+        form_id:,
+        question_text:,
+        hint_text:,
+        is_optional:,
+        is_repeatable:,
+        answer_settings:,
+        page_heading:,
+        guidance_markdown:,
+        answer_type:,
+      )
+      save_to_database!(page)
+      page
     end
 
     def save!(record)
       page = Api::V1::PageResource.new(record.attributes, true)
       page.prefix_options = record.prefix_options
       page.save!
+      save_to_database!(page)
       page
     end
 
@@ -37,7 +44,8 @@ class PageRepository
 
       begin
         page.destroy # rubocop:disable Rails/SaveBang
-      rescue ActiveResource::ResourceNotFound
+        Page.destroy(record.id)
+      rescue ActiveResource::ResourceNotFound, ActiveRecord::RecordNotFound
         # ActiveRecord::Persistence#destroy doesn't raise an error
         # if record has already been destroyed, let's emulate that
       end
@@ -48,7 +56,20 @@ class PageRepository
     def move_page(record, direction)
       page = Api::V1::PageResource.new(record.attributes, true)
       page.prefix_options = record.prefix_options
-      page.move_page(direction)
+
+      response = page.move_page(direction)
+      page.from_json(response.body)
+
+      save_to_database!(page)
+
+      page
+    end
+
+  private
+
+    def save_to_database!(record)
+      attributes = record.database_attributes
+      Page.upsert(attributes)
     end
   end
 end
