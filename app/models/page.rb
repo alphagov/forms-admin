a36efba14f5255465a026b6ec5621b32dfa36b7d
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2025-07-17 14:47:19 +0300

    Reorder page model code
    Make order of lines match up with forms-api for easier diffing.

--- app/models/page.rb
+++ app/models/page.rb
@@ -34,6 +34,17 @@ class Page < ApplicationRecord
     true
   end
 
+  def answer_type_changed_from_selection
+    answer_type_previously_was&.to_sym == :selection && answer_type&.to_sym != :selection
+  end
+
+  def answer_settings_changed_from_only_one_option
+    from_only_one_option = ActiveModel::Type::Boolean.new.cast(answer_settings_previously_was.try(:[], "only_one_option"))
+    to_multiple_options = !ActiveModel::Type::Boolean.new.cast(answer_settings.try(:[], "only_one_option"))
+
+    from_only_one_option && to_multiple_options
+  end
+
 private
 
   def destroy_secondary_skip_conditions
@@ -48,16 +59,5 @@ private
       .flat_map(&:check_conditions)
       .filter { |condition| condition.check_page_id != condition.routing_page_id }
       .each(&:destroy!)
-  end
-
-  def answer_type_changed_from_selection
-    answer_type_previously_was&.to_sym == :selection && answer_type&.to_sym != :selection
-  end
-
-  def answer_settings_changed_from_only_one_option
-    from_only_one_option = ActiveModel::Type::Boolean.new.cast(answer_settings_previously_was.try(:[], "only_one_option"))
-    to_multiple_options = !ActiveModel::Type::Boolean.new.cast(answer_settings.try(:[], "only_one_option"))
-
-    from_only_one_option && to_multiple_options
   end
 end
