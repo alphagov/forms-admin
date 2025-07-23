3dc8bcf13772873bbc6bde24fd587e4494f51f6c
Laurence de Bruxelles <laurence.debruxelles@digital.cabinet-office.gov.uk>
2025-07-23 14:25:22 +0300

    Add group methods to Form model
    We need to be able to access the group of a form on the ActiveRecord
    model. We could probably do this with a real association, but for now we
    just copy across the methods defined on the ActiveResource model.

--- app/models/form.rb
+++ app/models/form.rb
@@ -53,6 +53,14 @@ class Form < ApplicationRecord
 
   delegate :task_statuses, to: :task_status_service
 
+  def group
+    group_form&.group
+  end
+
+  after_destroy do
+    group_form&.destroy
+  end
+
 private
 
   def set_external_id
@@ -61,5 +69,9 @@ private
 
   def task_status_service
     @task_status_service ||= TaskStatusService.new(form: self)
+  end
+
+  def group_form
+    GroupForm.find_by_form_id(id)
   end
 end
