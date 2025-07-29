248822ef6d1768610709ec54f54e0399e3f51e39
Laurence de Bruxelles <laurence.debruxelles@dsit.gov.uk>
2025-07-29 14:07:57 +0300

    Add made_live_form factory
    This factory represents a form resource for a live or archived version of a form retrieved from the API
    
    Co-authored-by: Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>
    Co-authored-by: Laurence de Bruxelles <laurence.debruxelles@dsit.gov.uk>
    Co-authored-by: Samuel Culley <samuel.culley@digital.cabinet-office.gov.uk>

---
+++ spec/factories/models/made_live_form.rb
@@ -0,0 +1,23 @@
+FactoryBot.define do
+  factory :made_live_form, class: "Api::V1::FormResource" do
+    sequence(:name) { |n| "Form #{n}" }
+    sequence(:form_slug) { |n| "form-#{n}" }
+    submission_email { Faker::Internet.email(domain: "example.gov.uk") }
+    submission_type { "email" }
+    privacy_policy_url { Faker::Internet.url(host: "gov.uk") }
+    language { "en" }
+    support_email { nil }
+    support_phone { nil }
+    support_url { nil }
+    support_url_text { nil }
+    what_happens_next_markdown { "We usually respond to applications within 10 working days." }
+    declaration_text { nil }
+    question_section_completed { true }
+    declaration_section_completed { true }
+    share_preview_completed { true }
+    creator_id { nil }
+    start_page { nil }
+    pages { [] }
+    live_at { Time.zone.now.to_s }
+  end
+end
