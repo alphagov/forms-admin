af2e8db62f8f0f072bf3651f4c32b2b69422be49
Samuel Culley <samuel.culley@digital.cabinet-office.gov.uk>
2025-07-31 11:30:09 +0100

    Fix the routes controller specs

--- spec/requests/pages/routes_controller_spec.rb
+++ spec/requests/pages/routes_controller_spec.rb
@@ -1,11 +1,10 @@
 require "rails_helper"
 
 describe Pages::RoutesController, type: :request do
-  let(:form) { build :form, :ready_for_routing, id: 1 }
+  let(:form) { create :form, :ready_for_routing }
   let(:pages) { form.pages }
   let(:page) do
     pages.first.tap do |first_page|
-      first_page.id = 101
       first_page.is_optional = false
       first_page.answer_type = "selection"
       first_page.answer_settings = DataStruct.new(
@@ -15,8 +14,6 @@ describe Pages::RoutesController, type: :request do
       )
     end
   end
-
-  let(:selected_page) { page }
 
   let(:group) { create(:group, organisation: standard_user.organisation) }
   let(:user) { standard_user }
@@ -32,9 +29,9 @@ describe Pages::RoutesController, type: :request do
 
   describe "#show" do
     before do
-      allow(PageRepository).to receive(:find).with(page_id: "101", form_id: 1).and_return(selected_page)
+      allow(PageRepository).to receive(:find).with(page_id: page.id, form_id: form.id).and_return(page)
 
-      get show_routes_path(form_id: form.id, page_id: selected_page.id)
+      get show_routes_path(form_id: form.id, page_id: page.id)
     end
 
     it "Reads the form" do
@@ -48,7 +45,6 @@ describe Pages::RoutesController, type: :request do
     context "when the page is at the end of the form" do
       let(:page) do
         pages.last.tap do |last_page|
-          last_page.id = 101
           last_page.is_optional = false
           last_page.answer_type = "selection"
           last_page.answer_settings = DataStruct.new(
@@ -67,9 +63,9 @@ describe Pages::RoutesController, type: :request do
 
   describe "#delete" do
     before do
-      allow(PageRepository).to receive(:find).with(page_id: "101", form_id: 1).and_return(selected_page)
+      allow(PageRepository).to receive(:find).with(page_id: page.id, form_id: form.id).and_return(page)
 
-      get delete_routes_path(form_id: form.id, page_id: selected_page.id)
+      get delete_routes_path(form_id: form.id, page_id: page.id)
     end
 
     it "renders the delete confirmation for routes template" do
@@ -79,29 +75,29 @@ describe Pages::RoutesController, type: :request do
   end
 
   describe "#destroy" do
-    let(:condition) { build :condition, id: 1, routing_page_id: selected_page.id, check_page_id: selected_page.id, goto_page_id: pages.last.id, answer_value: "Option 1" }
+    let(:condition) { create :condition, routing_page_id: page.id, check_page_id: page.id, goto_page_id: pages.last.id, answer_value: "Option 1" }
     let(:secondary_skip_page) { form.pages[2] }
-    let(:secondary_skip) { build :condition, id: 2, routing_page_id: secondary_skip_page.id, check_page_id: selected_page.id, goto_page_id: pages[3].id }
+    let!(:secondary_skip) { create :condition, routing_page_id: secondary_skip_page.id, check_page_id: page.id, goto_page_id: pages[3].id }
 
     before do
-      allow(PageRepository).to receive(:find).with(page_id: "101", form_id: 1).and_return(selected_page)
+      allow(PageRepository).to receive(:find).with(page_id: page.id, form_id: form.id).and_return(page)
       allow(ConditionRepository).to receive(:find).and_return(condition)
       allow(ConditionRepository).to receive(:destroy)
 
-      selected_page.routing_conditions = [condition]
-      secondary_skip_page.routing_conditions = [secondary_skip]
+      page.reload
+      secondary_skip_page.reload
     end
 
     context "when confirmed" do
       it "redirects to page list" do
-        delete destroy_routes_path(form_id: form.id, page_id: selected_page.id, pages_routes_delete_confirmation_input: { confirm: "yes" })
+        delete destroy_routes_path(form_id: form.id, page_id: page.id, pages_routes_delete_confirmation_input: { confirm: "yes" })
         expect(response).to redirect_to form_pages_path(form_id: form.id)
       end
 
       it "calls destroy on conditions" do
         expect(ConditionRepository).to receive(:destroy).with(have_attributes(id: condition.id))
         expect(ConditionRepository).to receive(:destroy).with(have_attributes(id: secondary_skip.id))
-        delete destroy_routes_path(form_id: form.id, page_id: selected_page.id, pages_routes_delete_confirmation_input: { confirm: "yes" })
+        delete destroy_routes_path(form_id: form.id, page_id: page.id, pages_routes_delete_confirmation_input: { confirm: "yes" })
       end
 
       context "but one of the routes is already deleted" do
@@ -109,13 +105,13 @@ describe Pages::RoutesController, type: :request do
           # forms-api may choose to delete the secondary skip when the condition is deleted
           allow(ConditionRepository).to receive(:destroy).and_call_original
           ActiveResource::HttpMock.respond_to do |mock|
-            mock.delete "/api/v1/forms/#{form.id}/pages/#{selected_page.id}/conditions/#{condition.id}", delete_headers, nil, 204
+            mock.delete "/api/v1/forms/#{form.id}/pages/#{page.id}/conditions/#{condition.id}", delete_headers, nil, 204
             mock.delete "/api/v1/forms/#{form.id}/pages/#{secondary_skip_page.id}/conditions/#{secondary_skip.id}", delete_headers, nil, 404
           end
         end
 
         it "does not render an error page" do
-          delete destroy_routes_path(form_id: form.id, page_id: selected_page.id, pages_routes_delete_confirmation_input: { confirm: "yes" })
+          delete destroy_routes_path(form_id: form.id, page_id: page.id, pages_routes_delete_confirmation_input: { confirm: "yes" })
           expect(response).not_to be_client_error
         end
       end
@@ -123,7 +119,7 @@ describe Pages::RoutesController, type: :request do
 
     context "when given invalid params" do
       it "renders the delete page" do
-        delete destroy_routes_path(form_id: form.id, page_id: selected_page.id, pages_routes_delete_confirmation_input: { confirm: nil })
+        delete destroy_routes_path(form_id: form.id, page_id: page.id, pages_routes_delete_confirmation_input: { confirm: nil })
 
         expect(response).to have_http_status(:unprocessable_entity)
         expect(response).to render_template("pages/routes/delete")
@@ -132,13 +128,13 @@ describe Pages::RoutesController, type: :request do
 
     context "when not confirmed" do
       it "redirects to routes page" do
-        delete destroy_routes_path(form_id: form.id, page_id: selected_page.id, pages_routes_delete_confirmation_input: { confirm: "no" })
-        expect(response).to redirect_to show_routes_path(form_id: form.id, page_id: selected_page.id)
+        delete destroy_routes_path(form_id: form.id, page_id: page.id, pages_routes_delete_confirmation_input: { confirm: "no" })
+        expect(response).to redirect_to show_routes_path(form_id: form.id, page_id: page.id)
       end
 
       it "does no call destroy on conditions" do
         expect(ConditionRepository).not_to receive(:destroy)
-        delete destroy_routes_path(form_id: form.id, page_id: selected_page.id, pages_routes_delete_confirmation_input: { confirm: "no" })
+        delete destroy_routes_path(form_id: form.id, page_id: page.id, pages_routes_delete_confirmation_input: { confirm: "no" })
       end
     end
   end
