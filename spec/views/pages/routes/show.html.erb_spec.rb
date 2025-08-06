5c8ddb7e19573c6e2c8817d5f4982d9e2c369086
Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>
2025-08-06 11:15:05 +0100

    Fix page routes show spec
    Co-authored-by: Samuel Culley <samuel.culley@digital.cabinet-office.gov.uk>
    Co-authored-by: Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>

--- spec/views/pages/routes/show.html.erb_spec.rb
+++ spec/views/pages/routes/show.html.erb_spec.rb
@@ -1,10 +1,9 @@
 require "rails_helper"
 
 describe "pages/routes/show.html.erb" do
-  let(:form) { build :form, id: 1, pages: }
-  let(:pages) { [page, next_page] }
-  let(:page) { build :page, id: 1, routing_conditions: [build(:condition)] }
-  let(:next_page) { build :page, id: 2 }
+  let(:form) { create :form, :ready_for_routing }
+  let(:pages) { form.pages }
+  let(:page) { pages.first }
   let(:routes) { PageRoutesService.new(form:, pages:, page:).routes }
   let(:errors) { [] }
 
@@ -19,71 +18,87 @@ describe "pages/routes/show.html.erb" do
     ]
   end
 
-  let(:route_summary_card_data_service) { instance_double(RouteSummaryCardDataPresenter, summary_card_data: route_cards, errors:, routes:, next_page:, pages:, page:, form:) }
+  let(:route_summary_card_data_service) { instance_double(RouteSummaryCardDataPresenter, summary_card_data: route_cards, errors:, routes:, next_page: pages.second, pages:, page:, form:) }
 
   before do
     allow(FormRepository).to receive_messages(pages:)
     allow(form).to receive(:group).and_return(build(:group))
-    render template: "pages/routes/show", locals: { current_form: form, page:, back_link_url: "/back", route_summary_card_data_presenter: route_summary_card_data_service }
   end
 
-  it "has the correct title" do
-    expect(view.content_for(:title)).to have_content("Question 1’s routes")
-  end
+  context "when there are no routes" do
+    before do
+      render template: "pages/routes/show", locals: { current_form: form, page:, back_link_url: "/back", route_summary_card_data_presenter: route_summary_card_data_service }
+    end
 
-  it "has the correct back link" do
-    expect(view.content_for(:back_link)).to have_link(I18n.t("pages.go_to_your_questions"), href: "/back")
-  end
+    it "has the correct title" do
+      expect(view.content_for(:title)).to have_content("Question 1’s routes")
+    end
 
-  it "has the correct heading and caption" do
-    expect(rendered).to have_selector("h1", text: form.name)
-    expect(rendered).to have_selector("h1", text: I18n.t("page_titles.routes_show", question_number: page.position))
-  end
+    it "has the correct back link" do
+      expect(view.content_for(:back_link)).to have_link(I18n.t("pages.go_to_your_questions"), href: "/back")
+    end
 
-  it "shows the page title as a summary list" do
-    expect(rendered).to have_css(".govuk-summary-list__key", text: "Question #{page.position}")
-    expect(rendered).to have_css(".govuk-summary-list__value", text: page.question_text)
-  end
+    it "has the correct heading and caption" do
+      expect(rendered).to have_selector("h1", text: form.name)
+      expect(rendered).to have_selector("h1", text: I18n.t("page_titles.routes_show", question_number: page.position))
+    end
 
-  it "shows the correct route cards" do
-    expect(rendered).to have_css(".govuk-summary-list__key", text: "route 1 key")
-    expect(rendered).to have_css(".govuk-summary-list__value", text: "route 1 value")
-  end
+    it "shows the page title as a summary list" do
+      expect(rendered).to have_css(".govuk-summary-list__key", text: "Question #{page.position}")
+      expect(rendered).to have_css(".govuk-summary-list__value", text: page.question_text)
+    end
 
-  it "has a back to questions link" do
-    expect(rendered).to have_link(I18n.t("pages.go_to_your_questions"), href: form_pages_path(form.id))
-  end
+    it "shows the correct route cards" do
+      expect(rendered).to have_css(".govuk-summary-list__key", text: "route 1 key")
+      expect(rendered).to have_css(".govuk-summary-list__value", text: "route 1 value")
+    end
 
-  context "when the page has a single skip route" do
-    include_context "with pages with routing"
-
-    let(:route_summary_card_data_service) { RouteSummaryCardDataPresenter.new form:, page: }
-    let(:page) { page_with_skip_route }
+    it "has a back to questions link" do
+      expect(rendered).to have_link(I18n.t("pages.go_to_your_questions"), href: form_pages_path(form.id))
+    end
 
     it "does not have a link to delete all routes" do
       expect(rendered).not_to have_link(I18n.t("page_route_card.delete_route"), href: delete_routes_path(form.id, page.id))
     end
   end
 
-  context "when the page does not have a secondary skip route" do
-    include_context "with pages with routing"
-
+  context "when the page has a single route" do
     let(:route_summary_card_data_service) { RouteSummaryCardDataPresenter.new form:, page: }
-    let(:page) { page_with_skip_route }
+
+    before do
+      create :condition, routing_page_id: pages.first.id, check_page_id: pages.first.id, answer_value: "Option 1", goto_page_id: pages.third.id
+      pages.each(&:reload)
+
+      render template: "pages/routes/show", locals: { current_form: form, page:, back_link_url: "/back", route_summary_card_data_presenter: route_summary_card_data_service }
+    end
+
+    it "does not have a link to delete all routes" do
+      expect(rendered).not_to have_link(I18n.t("page_route_card.delete_route"), href: delete_routes_path(form.id, page.id))
+    end
 
     it "has an any other answer section" do
       expect(rendered).to have_css "h2.govuk-heading-m", text: "If people select any other answer"
     end
 
+    it "has a link to set questions to skip" do
+      expect(rendered).to have_link(
+        "Set questions to skip",
+        class: "govuk-button--secondary",
+        href: new_secondary_skip_path(form.id, page.id),
+      )
+    end
+
     describe "any other answer section" do
       it "shows the number of the next question in the form" do
-        expect(rendered).to have_text "People who select any other answer will continue to question 11 and through the rest of the form"
+        expect(rendered).to have_text "People who select any other answer will continue to question #{pages.second.position} and through the rest of the form"
       end
 
       context "when the page is the last question" do
-        let(:pages) do
-          pages_with_routing.delete(page)
-          pages_with_routing.append(page)
+        let(:page) { pages.last }
+
+        before do
+          create :condition, routing_page_id: pages.last.id, check_page_id: pages.last.id, answer_value: "Option 1", goto_page_id: nil, skip_to_end: true
+          pages.each(&:reload)
         end
 
         it "shows the check your answers page as the next question in the form" do
@@ -98,34 +113,36 @@ describe "pages/routes/show.html.erb" do
           )
         end
       end
+    end
+  end
 
-      it "has a link to set questions to skip" do
-        expect(rendered).to have_link(
-          "Set questions to skip",
-          class: "govuk-button--secondary",
-          href: new_secondary_skip_path(form.id, page.id),
-        )
-      end
+  context "when the page has a route that leads to an exit page" do
+    before do
+      create :condition, :with_exit_page, routing_page_id: page.id, check_page_id: page.id, answer_value: "Option 1"
+      pages.each(&:reload)
 
-      context "when the route leads to an exit page" do
-        let(:page) { page_with_exit_page }
+      render template: "pages/routes/show", locals: { current_form: form, page:, back_link_url: "/back", route_summary_card_data_presenter: route_summary_card_data_service }
+    end
 
-        it "does not show the link to set questions to skip" do
-          expect(rendered).not_to have_link(
-            "Set questions to skip",
-            class: "govuk-button--secondary",
-            href: new_secondary_skip_path(form.id, page.id),
-          )
-        end
-      end
+    it "does not show the link to set questions to skip" do
+      expect(rendered).not_to have_link(
+        "Set questions to skip",
+        class: "govuk-button--secondary",
+        href: new_secondary_skip_path(form.id, page.id),
+      )
     end
   end
 
   context "when the page has a skip and a secondary skip" do
-    include_context "with pages with routing"
-
     let(:route_summary_card_data_service) { RouteSummaryCardDataPresenter.new form:, page: }
-    let(:page) { page_with_skip_and_secondary_skip }
+
+    before do
+      create :condition, routing_page_id: pages.first.id, check_page_id: pages.first.id, answer_value: "Option 1", goto_page_id: pages.third.id
+      create :condition, routing_page_id: pages.second.id, check_page_id: pages.first.id, goto_page_id: pages.fourth.id
+      pages.each(&:reload)
+
+      render template: "pages/routes/show", locals: { current_form: form, page:, back_link_url: "/back", route_summary_card_data_presenter: route_summary_card_data_service }
+    end
 
     it "has a link to delete all routes" do
       expect(rendered).to have_link(I18n.t("page_route_card.delete_route"), href: delete_routes_path(form.id, page.id))
@@ -139,6 +156,10 @@ describe "pages/routes/show.html.erb" do
 
   context "when there is an error" do
     let(:errors) { [OpenStruct.new(link: "goto-1", message: "Error text")] }
+
+    before do
+      render template: "pages/routes/show", locals: { current_form: form, page:, back_link_url: "/back", route_summary_card_data_presenter: route_summary_card_data_service }
+    end
 
     it "shows the error message" do
       expect(rendered).to have_text("Error text")
