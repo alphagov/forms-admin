32c9512f3729212e60925b1c24ff1bc860a13ad4
Samuel Culley <samuel.culley@digital.cabinet-office.gov.uk>
2025-08-07 14:45:12 +0100

    Fix route summary card data presenter spec
    Co-authored-by: Samuel Culley <samuel.culley@digital.cabinet-office.gov.uk>
    Co-authored-by: Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>

--- spec/presenters/route_summary_card_data_presenter_spec.rb
+++ spec/presenters/route_summary_card_data_presenter_spec.rb
@@ -5,14 +5,9 @@ describe RouteSummaryCardDataPresenter do
 
   subject(:service) { described_class.new(form:, page:) }
 
-  include_context "with pages with routing"
-
-  let(:form) { build :form, id: 99, pages: }
-  let(:page) { page_with_skip_route }
-
-  let(:routes) do
-    PageRoutesService.new(form:, pages:, page:).routes
-  end
+  let(:form) { create :form, :ready_for_routing }
+  let(:pages) { form.pages }
+  let(:page) { pages.first }
 
   before do
     allow(FormRepository).to receive_messages(pages: form.pages)
@@ -21,22 +16,28 @@ describe RouteSummaryCardDataPresenter do
 
   describe "#summary_card_data" do
     context "with conditional routes" do
-      it "returns an array of route cards" do
-        result = service.summary_card_data
-        expect(result.length).to eq(1) # 1 conditional route
+      before do
+        pages.each(&:reload)
+      end
 
-        # conditional route
-        expect(result[0][:card][:title]).to eq("Route 1")
-        expect(result[0][:card][:actions].first).to have_link("Edit", href: "/forms/99/pages/10/conditions/3")
-        expect(result[0][:rows][0][:value][:text]).to eq("Skip")
-        expect(result[0][:rows][1][:value][:text]).to eq("12. Question")
+      context "when there is a valid condition" do
+        let!(:condition) { create :condition, routing_page_id: page.id, check_page_id: page.id, goto_page_id: pages.last.id, answer_value: "Option 1" }
+
+        it "returns an array of route cards" do
+          result = service.summary_card_data
+          expect(result.length).to eq(1) # 1 conditional route
+
+          # conditional route
+          expect(result[0][:card][:title]).to eq("Route 1")
+          expect(result[0][:card][:actions].first).to have_link("Edit", href: "/forms/#{form.id}/pages/#{page.id}/conditions/#{condition.id}")
+          expect(result[0][:rows][0][:value][:text]).to eq("Option 1")
+          expect(result[0][:rows][1][:value][:text]).to eq("#{pages.last.position}. #{pages.last.question_text}")
+        end
       end
 
       context "when the goto_page does not exist" do
         before do
-          page.routing_conditions.first.tap do |condition|
-            condition.goto_page_id = 999
-          end
+          create :condition, routing_page_id: page.id, check_page_id: page.id, goto_page_id: 127, answer_value: "Option 1"
         end
 
         it "uses placeholder text instead of question text" do
@@ -47,25 +48,22 @@ describe RouteSummaryCardDataPresenter do
     end
 
     context "when the condition has an exit page data" do
+      let!(:condition) { create :condition, :with_exit_page, routing_page_id: page.id, check_page_id: page.id, answer_value: "Option 1" }
+
       before do
-        page.routing_conditions.first.tap do |condition|
-          condition.exit_page_heading = "Exit page"
-          condition.exit_page_markdown = "This is the exit page"
-        end
+        pages.each(&:reload)
       end
 
       it "uses the exit page heading in the summary card" do
         result = service.summary_card_data
-        expect(result[0][:rows][1][:value][:text]).to eq("Exit page")
+        expect(result[0][:rows][1][:value][:text]).to eq(condition.exit_page_heading)
       end
     end
 
     context "with skip to end condition" do
       before do
-        page.routing_conditions.first.tap do |condition|
-          condition.goto_page_id = nil
-          condition.skip_to_end = true
-        end
+        create :condition, routing_page_id: page.id, check_page_id: page.id, answer_value: "Option 1", skip_to_end: true
+        pages.each(&:reload)
       end
 
       it 'shows "Check your answers" as destination' do
@@ -75,29 +73,36 @@ describe RouteSummaryCardDataPresenter do
     end
 
     context "with conditional routes and secondary skip routes" do
-      let(:page) { page_with_skip_and_secondary_skip }
+      let(:condition) { create :condition, routing_page_id: page.id, check_page_id: page.id, goto_page_id: pages.fourth.id, answer_value: "Option 1" }
+      let(:secondary_skip_condition) { create :condition, routing_page_id: pages.third.id, check_page_id: page.id, goto_page_id: pages.last.id }
+
+      before do
+        condition
+        secondary_skip_condition
+        pages.each(&:reload)
+      end
 
       it "shows the destination of the secondary skip route" do
         result = service.summary_card_data
-        expect(result[1][:rows][0][:value][:text]).to eq("3. Question in branch 1")
-        expect(result[1][:rows][1][:value][:text]).to eq("4. Question at the end of branch 1 (start of a secondary skip)")
-        expect(result[1][:rows][2][:value][:text]).to eq("8. Question after a branch route (end of a secondary skip)")
+        expect(result[1][:rows][0][:value][:text]).to eq("#{pages.second.position}. #{pages.second.question_text}")
+        expect(result[1][:rows][1][:value][:text]).to eq("#{pages.third.position}. #{pages.third.question_text}")
+        expect(result[1][:rows][2][:value][:text]).to eq("#{pages.last.position}. #{pages.last.question_text}")
       end
 
       it "shows the edit secondary skip link" do
         result = service.summary_card_data
-        expect(result[1][:card][:actions].first).to have_link("Edit", href: "/forms/99/pages/2/routes/any-other-answer/questions-to-skip")
+        expect(result[1][:card][:actions].first).to have_link("Edit", href: "/forms/#{form.id}/pages/#{page.id}/routes/any-other-answer/questions-to-skip")
       end
 
       it "shows the delete secondary skip link" do
         result = service.summary_card_data
-        expect(result[1][:card][:actions].second).to have_link("Delete", href: "/forms/99/pages/2/routes/any-other-answer/questions-to-skip/delete")
+        expect(result[1][:card][:actions].second).to have_link("Delete", href: "/forms/#{form.id}/pages/#{page.id}/routes/any-other-answer/questions-to-skip/delete")
       end
 
       context "when the route's check answer does not exist" do
-        it "shows an error message" do
-          branch_route_1.validation_errors << OpenStruct.new(name: "answer_value_doesnt_exist")
+        let(:condition) { create :condition, routing_page_id: page.id, check_page_id: page.id, goto_page_id: pages.fourth.id, answer_value: "Non-existent-option" }
 
+        it "shows an error message" do
           result = service.summary_card_data
           expect(result[0][:rows][0][:value][:text]).to include("The answer that route 1 is based on no longer exists - edit or delete this route")
           expect(result[0][:rows][0][:value][:text]).to include("class=\"govuk-summary-list__value--error\"")
@@ -105,9 +110,9 @@ describe RouteSummaryCardDataPresenter do
       end
 
       context "when route 1 does not skip any questions" do
-        it "shows an error message" do
-          branch_route_1.validation_errors << OpenStruct.new(name: "cannot_route_to_next_page")
+        let(:condition) { create :condition, routing_page_id: page.id, check_page_id: page.id, goto_page_id: pages.second.id, answer_value: "Option 1" }
 
+        it "shows an error message" do
           result = service.summary_card_data
           expect(result[0][:rows][1][:value][:text]).to include("Route 1 is not skipping any questions - edit or delete this route")
           expect(result[0][:rows][1][:value][:text]).to include("class=\"govuk-summary-list__value--error\"")
@@ -115,9 +120,10 @@ describe RouteSummaryCardDataPresenter do
       end
 
       context "when route 1 routes to a previous question" do
-        it "shows an error message" do
-          branch_route_1.validation_errors << OpenStruct.new(name: "cannot_have_goto_page_before_routing_page")
+        let(:page) { pages.second }
+        let(:condition) { create :condition, routing_page_id: page.id, check_page_id: page.id, goto_page_id: pages.first.id, answer_value: "Option 1" }
 
+        it "shows an error message" do
           result = service.summary_card_data
           expect(result[0][:rows][1][:value][:text]).to include("The question route 1 skips to cannot be before question 2 - edit or delete this route")
           expect(result[0][:rows][1][:value][:text]).to include("class=\"govuk-summary-list__value--error\"")
@@ -125,9 +131,9 @@ describe RouteSummaryCardDataPresenter do
       end
 
       context "when the any other answer route does not skip a question" do
-        it "shows an error message" do
-          branch_any_other_answer_route.validation_errors << OpenStruct.new(name: "cannot_route_to_next_page")
+        let(:secondary_skip_condition) { create :condition, routing_page_id: pages.third.id, check_page_id: page.id, goto_page_id: pages.fourth.id }
 
+        it "shows an error message" do
           result = service.summary_card_data
           expect(result[1][:rows][2][:value][:text]).to include("The route for any other answer is not skipping any questions - edit or delete this route")
           expect(result[1][:rows][2][:value][:text]).to include("class=\"govuk-summary-list__value--error\"")
@@ -135,9 +141,9 @@ describe RouteSummaryCardDataPresenter do
       end
 
       context "when the any other answer route skips to a previous question" do
-        it "shows an error message" do
-          branch_any_other_answer_route.validation_errors << OpenStruct.new(name: "cannot_have_goto_page_before_routing_page")
+        let(:secondary_skip_condition) { create :condition, routing_page_id: pages.third.id, check_page_id: page.id, goto_page_id: pages.second.id }
 
+        it "shows an error message" do
           result = service.summary_card_data
           expect(result[1][:rows][2][:value][:text]).to include("The question the route for any other answer skips to cannot be before the question it skips from - edit or delete this route")
           expect(result[1][:rows][2][:value][:text]).to include("class=\"govuk-summary-list__value--error\"")
@@ -146,8 +152,6 @@ describe RouteSummaryCardDataPresenter do
     end
 
     context "with no conditional routes" do
-      let(:page) { page_with_no_routes }
-
       it "returns an empty array" do
         result = service.summary_card_data
         expect(result).to eq []
@@ -174,49 +178,62 @@ describe RouteSummaryCardDataPresenter do
   describe "#next_page" do
     it "returns the next page" do
       allow(FormRepository).to receive(:pages).and_return(pages)
-      expect(service.next_page).to eq(pages[10])
+      expect(service.next_page).to eq(pages.second)
     end
   end
 
   describe "#errors" do
-    let(:page) { page_with_skip_and_secondary_skip }
+    let(:condition) { create :condition, routing_page_id: page.id, check_page_id: page.id, goto_page_id: pages.fourth.id, answer_value: "Option 1" }
+    let(:secondary_skip_condition) { create :condition, routing_page_id: pages.third.id, check_page_id: page.id, goto_page_id: pages.last.id }
+
+    before do
+      condition
+      secondary_skip_condition
+      pages.each(&:reload)
+    end
 
     it "returns an array of errors" do
       expect(service.errors).to be_empty
     end
 
     context "when there is a check error" do
+      let(:condition) { create :condition, routing_page_id: page.id, check_page_id: page.id, goto_page_id: pages.fourth.id, answer_value: "Non-existent-answer" }
+
       it "contains the check error link and message" do
-        branch_route_1.validation_errors << OpenStruct.new(name: "answer_value_doesnt_exist")
-        expect(service.errors).to eq([OpenStruct.new(link: "#check-#{branch_route_1.id}", message: I18n.t("page_route_card.errors.answer_value_doesnt_exist"))])
+        expect(service.errors).to eq([OpenStruct.new(link: "#check-#{condition.id}", message: I18n.t("page_route_card.errors.answer_value_doesnt_exist"))])
       end
     end
 
     context "when there is a next page error" do
+      let(:condition) { create :condition, routing_page_id: page.id, check_page_id: page.id, goto_page_id: pages.second.id, answer_value: "Option 1" }
+
       it "contains the next page error link and message" do
-        branch_route_1.validation_errors << OpenStruct.new(name: "cannot_route_to_next_page")
-        expect(service.errors).to eq([OpenStruct.new(link: "#goto-#{branch_route_1.id}", message: I18n.t("page_route_card.errors.cannot_route_to_next_page"))])
+        expect(service.errors).to eq([OpenStruct.new(link: "#goto-#{condition.id}", message: I18n.t("page_route_card.errors.cannot_route_to_next_page"))])
       end
     end
 
     context "when there is a next page error for the secondary skip" do
+      let(:secondary_skip_condition) { create :condition, routing_page_id: pages.third.id, check_page_id: page.id, goto_page_id: pages.fourth.id }
+
       it "contains the secondary skip next page error link and message" do
-        branch_any_other_answer_route.validation_errors << OpenStruct.new(name: "cannot_route_to_next_page")
-        expect(service.errors).to eq([OpenStruct.new(link: "#goto-#{branch_any_other_answer_route.id}", message: I18n.t("page_route_card.errors.cannot_route_to_next_page_secondary_skip"))])
+        expect(service.errors).to eq([OpenStruct.new(link: "#goto-#{secondary_skip_condition.id}", message: I18n.t("page_route_card.errors.cannot_route_to_next_page_secondary_skip"))])
       end
     end
 
     context "when there is a goto page before routing page error" do
+      let(:page) { pages.second }
+      let(:condition) { create :condition, routing_page_id: page.id, check_page_id: page.id, goto_page_id: pages.first.id, answer_value: "Option 1" }
+
       it "contains the goto page before routing page error link and message" do
-        branch_route_1.validation_errors << OpenStruct.new(name: "cannot_have_goto_page_before_routing_page")
-        expect(service.errors).to eq([OpenStruct.new(link: "#goto-#{branch_route_1.id}", message: I18n.t("page_route_card.errors.cannot_have_goto_page_before_routing_page", question_number: 2))])
+        expect(service.errors).to eq([OpenStruct.new(link: "#goto-#{condition.id}", message: I18n.t("page_route_card.errors.cannot_have_goto_page_before_routing_page", question_number: 2))])
       end
     end
 
     context "when there is a goto page before routing page error for the secondary skip" do
+      let(:secondary_skip_condition) { create :condition, routing_page_id: pages.third.id, check_page_id: page.id, goto_page_id: pages.second.id }
+
       it "contains the secondary skip goto page before routing page error link and message" do
-        branch_any_other_answer_route.validation_errors << OpenStruct.new(name: "cannot_have_goto_page_before_routing_page")
-        expect(service.errors).to eq([OpenStruct.new(link: "#goto-#{branch_any_other_answer_route.id}", message: I18n.t("page_route_card.errors.cannot_have_goto_page_before_routing_page_secondary_skip"))])
+        expect(service.errors).to eq([OpenStruct.new(link: "#goto-#{secondary_skip_condition.id}", message: I18n.t("page_route_card.errors.cannot_have_goto_page_before_routing_page_secondary_skip"))])
       end
     end
   end
