2f0c6d724414d39f43cdb3a0dbad26529a29ee22
chao-xian <kelvin.gan@digital.cabinet-office.gov.uk>
2025-08-14 11:12:12 +0100

    Add restricting moving archived form
    Ensure they cannot be moved to Trial groups by filtering the list of Groups the user sees.

--- app/input_objects/forms/group_select.rb
+++ app/input_objects/forms/group_select.rb
@@ -8,7 +8,7 @@ class Forms::GroupSelect < BaseInput
 
     groups = Group.for_organisation(group.organisation).excluding(group)
 
-    if form_record.has_live_version
+    if form_record.has_live_version || form_record.has_been_archived
       groups = groups.where(status: :active)
     end
 
