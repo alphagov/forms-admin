71ae7ad02cc143a56d160924ee4ffac7e8edf892
Samuel Culley <samuel.culley@digital.cabinet-office.gov.uk>
2025-08-21 11:35:42 +0100

    Add FormDocumentSyncService
    The FormDocumentSyncService will take a Form object, and create or
    update the appropriate FormDocument for it.
    
    In the case that a Form is in a live state, it will either create a new
    FormDocument or update the and existing one with the Form's current
    content.
    
    In the case that a Form is archived, it will either update the existing
    live FormDocument to set it to be archived, or it will retrieve the
    content for the live form from the V2 API and then create a new archived
    FormDocument using the content from the API response.
    
    Co-authored-by: Laurence de Bruxelles <laurence.debruxelles@dsit.gov.uk>

--- spec/factories/models/form_documents.rb
+++ spec/factories/models/form_documents.rb
@@ -1,6 +1,14 @@
 FactoryBot.define do
   factory :form_document do
     form { association :form }
-    tag { :draft }
+    tag { "draft" }
+
+    trait :live do
+      tag { "live" }
+    end
+
+    trait :archived do
+      tag { "archived" }
+    end
   end
 end
