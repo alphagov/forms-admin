0e87768a14c208e5da35cd34540cd810347223bd
chao-xian <kelvin.gan@digital.cabinet-office.gov.uk>
2025-08-26 17:40:50 +0100

    Fix Drop down and auto-complete selects

--- app/presenters/forms/group_select_presenter.rb
+++ app/presenters/forms/group_select_presenter.rb
@@ -21,8 +21,8 @@ class Forms::GroupSelectPresenter
   def hint
     return I18n.t("helpers.hint.forms_group_select.no_active_groups") if @form.is_live? && @groups.empty?
     return I18n.t("helpers.hint.forms_group_select.no_groups") if @groups.empty?
-    return I18n.t("helpers.hint.forms_group_select.group") if (@form.is_live? || @form.is_archived?) && !@groups.empty?
+    return I18n.t("helpers.hint.forms_group_select.group") if (@form.is_live? || @form.is_archived?) && @groups.present?
 
-    nil
+    ""
   end
 end
