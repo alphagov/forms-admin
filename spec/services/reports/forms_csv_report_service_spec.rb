aee19a035a2ecb38e3b1f67a81aaf986ab0d5f74
Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>
2025-09-03 13:03:41 +0100

    Build FormDocuments using factories in forms_csv_report_service_spec
    Previously, we were loading a pre-made API response body in the format
    that forms-api would return from a JSON document.
    
    To prepare for using FormDocuments retrieved from the forms-admin
    database rather than forms-api, replace use of the JSON document with
    creating FormDocuments from Forms created using the factories.
    
    Co-authored-by: Laurence de Bruxelles <laurence.debruxelles@dsit.gov.uk>

--- spec/services/reports/forms_csv_report_service_spec.rb
+++ spec/services/reports/forms_csv_report_service_spec.rb
@@ -5,15 +5,27 @@ RSpec.describe Reports::FormsCsvReportService do
     described_class.new(form_documents)
   end
 
-  let(:form_documents) { JSON.parse(file_fixture("form_documents_response.json").read) }
+  let(:form_documents) { forms.map { |form| form.live_form_document.as_json } }
+  let(:form) do
+    create(:form, :live, :with_support, submission_type: "email_with_csv", payment_url: "https://www.gov.uk/payments/organisation/service", pages: [
+      create(:page, :with_address_settings, is_repeatable: true),
+      create(:page, :with_date_settings),
+      create(:page, answer_type: "email"),
+      create(:page, :with_full_name_settings),
+      create(:page, answer_type: "national_insurance_number"),
+      create(:page, answer_type: "number"),
+      create(:page, answer_type: "phone_number"),
+      create(:page, :with_selection_settings, is_optional: true),
+      create(:page, :with_single_line_text_settings, is_repeatable: true),
+    ])
+  end
+  let(:forms) { [form, create(:form, :live)] }
 
   let(:group) { create(:group) }
 
   before do
-    form_documents.each do |form_document|
-      form = create(:form)
+    forms.each do |form|
       GroupForm.create!(form: form, group: group)
-      form_document["form_id"] = form.id
     end
   end
 
@@ -21,37 +33,37 @@ RSpec.describe Reports::FormsCsvReportService do
     it "returns a CSV with a header row and a row for each form" do
       csv = csv_reports_service.csv
       rows = CSV.parse(csv)
-      expect(rows.length).to eq 5
+      expect(rows.length).to eq 3
     end
 
     it "has expected values" do
       csv = csv_reports_service.csv
       rows = CSV.parse(csv)
-      expect(rows[1]).to eq([
-        form_documents[0]["form_id"].to_s,
+      expect(rows[1]).to contain_exactly(
+        form.id.to_s,
         "live",
-        "All question types form",
-        "all-question-types-form",
+        form.name,
+        form.form_slug,
         group.organisation.name,
         group.organisation.id.to_s,
         group.name,
         group.external_id,
-        "2025-01-02T16:24:31.203Z",
-        "2025-01-02T16:24:31.255Z",
+        form.created_at.iso8601(6),
+        form.updated_at.iso8601(6),
         "9",
         "false",
         "false",
         "false",
-        "https://www.gov.uk/payments/your-payment-link",
-        nil,
-        nil,
-        "your.email+fakedata84701@gmail.com.gov.uk",
-        "08000800",
-        "https://www.gov.uk/help/privacy-notice",
-        "Test",
+        form.payment_url,
+        form.support_url,
+        form.support_url_text,
+        form.support_email,
+        form.support_phone,
+        form.privacy_policy_url,
+        form.what_happens_next_markdown,
         "email_with_csv",
         "en",
-      ])
+      )
     end
   end
 end
