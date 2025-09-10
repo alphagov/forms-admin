fe8c1131394e7e8806154d4de07d888f3d9066a7
Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>
2025-09-10 12:08:43 +0100

    Use draft FormDocuments for API response
    Previously, we were not storing draft FormDocuments in the database
    and instead constructed them on-the-fly when a request was made to the
    API. Now we store draft FormDocuments in the database, which allows us
    to return them directly from the database in the API response.

--- app/controllers/api/form_documents_controller.rb
+++ app/controllers/api/form_documents_controller.rb
@@ -4,10 +4,6 @@ class Api::FormDocumentsController < ApplicationController
   end
 
   def get_form_document
-    if params[:tag] == "draft"
-      return Form.find(params[:form_id]).as_form_document
-    end
-
     FormDocument.find_by!(form_id: params[:form_id], tag: params[:tag]).content
   end
 end
