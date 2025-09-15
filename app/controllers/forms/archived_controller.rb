45a4ef1d23cbf9c5cc664305727d3483570577e3
Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>
2025-09-15 14:56:42 +0100

    Render 404 page when form is in the wrong state
    When GET requests are made to /forms/:id/live and /forms/:id/archived
    routes, check that the form is in the correct state (live or archived)
    to render the page. If not, render a 404 page.
    
    Previously, no check was made and an unhandled error was raised when
    trying to load the form due to a FormDocument with the desired tag not
    existing.

--- app/controllers/forms/archived_controller.rb
+++ app/controllers/forms/archived_controller.rb
@@ -1,5 +1,6 @@
 module Forms
   class ArchivedController < WebController
+    before_action :check_form_state
     after_action :verify_authorized
 
     def show_form
@@ -14,6 +15,12 @@ module Forms
 
     def current_archived_form
       @current_archived_form ||= FormDocument::Content.from_form_document(current_form.archived_form_document)
+    end
+
+  private
+
+    def check_form_state
+      render template: "errors/not_found", status: :not_found unless current_form.has_been_archived
     end
   end
 end
