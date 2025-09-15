13e46cc9a13b26d73d72ed5fde23871b03c9821c
chao-xian <kelvin.gan@digital.cabinet-office.gov.uk>
2025-09-15 15:01:52 +0100

    Update GroupSelectPresenter for archived forms
    As archived forms can move anywhere, we don't need the hint for active groups and we can show the status of the groups available to be consistent with moving Drafts.

--- app/presenters/forms/group_select_presenter.rb
+++ app/presenters/forms/group_select_presenter.rb
@@ -12,13 +12,13 @@ class Forms::GroupSelectPresenter
   end
 
   def group_name(group)
-    return group.name if @form.has_live_version || @form.has_been_archived
+    return group.name if @form.has_live_version
 
     "#{group.name} (#{group.status.humanize})"
   end
 
   def legend
-    return I18n.t("helpers.legend.forms_group_select.no_active_groups") if (@form.is_live? || @form.is_archived?) && @groups.empty?
+    return I18n.t("helpers.legend.forms_group_select.no_active_groups") if @form.is_live? && @groups.empty?
     return I18n.t("helpers.legend.forms_group_select.no_groups") if @groups.empty?
 
     I18n.t("helpers.legend.forms_group_select.group")
@@ -27,7 +27,7 @@ class Forms::GroupSelectPresenter
   def hint
     return I18n.t("helpers.hint.forms_group_select.no_active_groups") if @form.is_live? && @groups.empty?
     return I18n.t("helpers.hint.forms_group_select.no_groups") if @groups.empty?
-    return I18n.t("helpers.hint.forms_group_select.group") if (@form.is_live? || @form.is_archived?) && @groups.present?
+    return I18n.t("helpers.hint.forms_group_select.group") if @form.is_live? && @groups.present?
 
     ""
   end
