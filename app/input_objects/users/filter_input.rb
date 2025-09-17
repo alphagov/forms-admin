2464f400e130121d31601b1a05608509039afd53
Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>
2025-09-17 14:55:01 +0100

    Add clear filters link
    Show a link to load the users page without filters if any filters are
    applied.

--- app/input_objects/users/filter_input.rb
+++ app/input_objects/users/filter_input.rb
@@ -1,5 +1,9 @@
 module Users
   class FilterInput < BaseInput
     attr_accessor :email, :name, :organisation_id
+
+    def has_filters?
+      [email, name, organisation_id].any?(&:present?)
+    end
   end
 end
