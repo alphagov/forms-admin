edecd54a81900cc835585a50a5c707089e907f91
Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>
2025-09-22 16:46:03 +0100

    Handle a form in the wrong state when visiting /live or /archived
    When visiting /live or /live/pages routes, check that the form is in
    a live state. If it has been archived, redirect to the equivalent
    page for an archived form. If the form has not been made live, render
    the 404 page.
    
    Do the equivalent for /archived routes, redirecting to /live if the
    form is live, and rendering the 404 page if the form has never been
    made live.

--- app/controllers/forms/archived_controller.rb
+++ app/controllers/forms/archived_controller.rb
@@ -4,13 +4,23 @@ module Forms
 
     def show_form
       authorize current_form, :can_view_form?
+
+      return redirect_to live_form_path if current_form.is_live?
+      raise NotFoundError unless current_form.is_archived?
+
       render :show_form, locals: { form_metadata: current_form, form_document: current_archived_form }
     end
 
     def show_pages
       authorize current_form, :can_view_form?
+
+      return redirect_to live_form_pages_path if current_form.is_live?
+      raise NotFoundError unless current_form.is_archived?
+
       render :show_pages, locals: { form_document: current_archived_form }
     end
+
+  private
 
     def current_archived_form
       @current_archived_form ||= FormDocument::Content.from_form_document(current_form.archived_form_document)
--- spec/requests/forms/archived_controller_spec.rb
+++ spec/requests/forms/archived_controller_spec.rb
@@ -20,6 +20,31 @@ RSpec.describe Forms::ArchivedController, type: :request do
     it "renders the show archived form template" do
       expect(response).to render_template(:show_form)
     end
+
+    context "when the form is archive_with_draft" do
+      let(:form) { create(:form, :archived_with_draft) }
+
+      it "renders the show archived form template" do
+        expect(response).to render_template(:show_form)
+      end
+    end
+
+    context "when the form is live" do
+      let(:form) { create(:form, :live) }
+
+      it "redirects to the live form page" do
+        expect(response).to have_http_status(:found)
+        expect(response).to redirect_to(live_form_path(form))
+      end
+    end
+
+    context "when the form is draft" do
+      let(:form) { create(:form) }
+
+      it "returns 404" do
+        expect(response).to have_http_status(:not_found)
+      end
+    end
   end
 
   describe "#show_pages" do
@@ -29,6 +54,31 @@ RSpec.describe Forms::ArchivedController, type: :request do
 
     it "renders the show archived form pages template" do
       expect(response).to render_template(:show_pages)
+    end
+
+    context "when the form is archive_with_draft" do
+      let(:form) { create(:form, :archived_with_draft) }
+
+      it "renders the show archived form pages template" do
+        expect(response).to render_template(:show_pages)
+      end
+    end
+
+    context "when the form is live" do
+      let(:form) { create(:form, :live) }
+
+      it "redirects to the live form pages page" do
+        expect(response).to have_http_status(:found)
+        expect(response).to redirect_to(live_form_pages_path(form))
+      end
+    end
+
+    context "when the form is draft" do
+      let(:form) { create(:form) }
+
+      it "returns 404" do
+        expect(response).to have_http_status(:not_found)
+      end
     end
   end
 end
