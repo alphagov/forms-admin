762f13590f733c55649abd6d3b58435ecda7f521
Thomas Iles <tomiles@gmail.com>
2025-10-02 16:08:53 +0100

    Add RevertDraftFormService to delete form controller
    Change the delete draft controller so that drafts can be deleted from
    forms which are in a live_with_draft state.
    
    We check use a different method to "delete" the draft from a
    live form and redirect to the live form page.
    
    We also redirect with a failure if this is called and the form is not a
    draft or a live_with_draft form.

--- app/controllers/forms/delete_confirmation_controller.rb
+++ app/controllers/forms/delete_confirmation_controller.rb
@@ -14,25 +14,37 @@ module Forms
       load_page_variables
       @delete_confirmation_input = DeleteConfirmationInput.new(delete_confirmation_input_params)
 
-      unless @delete_confirmation_input.valid?
-        return render :delete
+      return render :delete unless @delete_confirmation_input.valid?
+      return redirect_to @back_url unless @delete_confirmation_input.confirmed?
+
+      result = delete_form_or_draft
+
+      if result[:success]
+        redirect_to result[:redirect_url],
+                    status: :see_other,
+                    success: t(".success", form_name: current_form.name)
+      # if not in a state we can delete in, redirect back with a failure message
+      else
+        flash[:message] = t(".failure")
+        redirect_to @back_url
       end
-
-      unless @delete_confirmation_input.confirmed?
-        return redirect_to @back_url
-      end
-
-
-      unless current_form.destroy
-        flash[:message] = "Deletion unsuccessful"
-        return redirect_to @back_url
-      end
-
-      success_url = group_path(current_form.group)
-      redirect_to success_url, status: :see_other, success: t(".success", form_name: current_form.name)
     end
 
   private
+
+    def delete_form_or_draft
+      if current_form.draft?
+        success_url = group_path(current_form.group)
+        success = current_form.destroy
+        { success: success, redirect_url: success_url }
+      elsif current_form.live_with_draft?
+        success_url = live_form_path(current_form.id)
+        success = RevertDraftFormService.new(current_form).revert_draft_from_form_document(:live)
+        { success: success, redirect_url: success_url }
+      else
+        { success: false }
+      end
+    end
 
     def delete_confirmation_input_params
       params.require(:forms_delete_confirmation_input).permit(:confirm)
