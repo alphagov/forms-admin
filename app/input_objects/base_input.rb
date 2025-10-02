44665eb8f3d7559ae87bd75d5b54341de1705d98
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2025-10-02 12:57:48 +0100

    Pass form_name to analytics in draft_question inputs
    Passes the form_name from the draft question to the analytics event for
    any input that includes a draft_question attribute.
    
    This fixes an issue where some inputs weren't including the form name in
    the analytics event.

--- app/input_objects/base_input.rb
+++ app/input_objects/base_input.rb
@@ -8,10 +8,16 @@ private
 
   def set_validation_error_logging_attributes
     CurrentLoggingAttributes.validation_errors = errors.map { |error| "#{error.attribute}: #{error.type}" } if errors.any?
-    form_name = form.name if defined?(form) && form.present?
 
     errors.each do |error|
       AnalyticsService.track_validation_errors(input_object_name: self.class.name, field: error.attribute, error_type: error.type, form_name:)
     end
+  end
+
+  def form_name
+    return form.name if defined?(form) && form.present?
+    return draft_question.form_name if defined?(draft_question) && draft_question.present?
+
+    nil
   end
 end
