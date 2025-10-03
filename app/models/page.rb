f221291143a645333c534a6e25ff3438ddd09d32
Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>
2025-10-03 12:55:28 +0100

    Refactor Page#update_form method
    We can call the `Form#save_draft!` method to take care of the Form
    state change if the form is live/archived rather than duplicating this
    logic.

--- app/models/page.rb
+++ app/models/page.rb
@@ -97,12 +97,8 @@ class Page < ApplicationRecord
 private
 
   def update_form
-    # TODO: https://trello.com/c/dg9CFPgp/1503-user-triggers-state-change-from-live-to-livewithdraft
-    # Will not be needed when users can trigger this event themselves through the UI
-    form.create_draft_from_live_form! if form.live?
-    form.create_draft_from_archived_form! if form.archived?
-
-    form.update!(question_section_completed: false)
+    form.question_section_completed = false
+    form.save_draft!
   end
 
   def guidance_fields_presence
