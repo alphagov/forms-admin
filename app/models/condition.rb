b101c391c118d3f2793b31a93d95b1f4137a005a
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2025-10-08 17:21:26 +0100

    Configure condition with translatable fields

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -1,5 +1,7 @@
 class Condition < ApplicationRecord
   include ConditionMethods
+  extend Mobility
+
   belongs_to :routing_page, class_name: "Page"
   belongs_to :check_page, class_name: "Page", optional: true
   belongs_to :goto_page, class_name: "Page", optional: true
@@ -7,6 +9,10 @@ class Condition < ApplicationRecord
   has_one :form, through: :routing_page
 
   before_destroy :destroy_postconditions
+
+  translates :answer_value,
+             :exit_page_markdown,
+             :exit_page_heading
 
   def self.create_and_update_form!(...)
     condition = Condition.new(...)
