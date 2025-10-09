94da7e7352525555004ead6d4800139d63c2ddc3
Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>
2025-10-09 10:21:46 +0100

    Retrieve all conditions for a form directly
    Rather than retrieving the conditions per page of a form when we need all conditions for a form, look them up in a single query by joining on the pages table.

--- app/models/form.rb
+++ app/models/form.rb
@@ -8,6 +8,7 @@ class Form < ApplicationRecord
   has_one :live_form_document, -> { where tag: "live" }, class_name: "FormDocument"
   has_one :archived_form_document, -> { where tag: "archived" }, class_name: "FormDocument"
   has_one :draft_form_document, -> { where tag: "draft" }, class_name: "FormDocument"
+  has_many :conditions, through: :pages, source: :routing_conditions
 
   enum :submission_type, {
     email: "email",
@@ -89,7 +90,6 @@ class Form < ApplicationRecord
   def qualifying_route_pages
     max_routes_per_page = 2
 
-    conditions = pages.flat_map(&:routing_conditions).compact_blank
     condition_counts = conditions.group_by(&:check_page_id).transform_values(&:length)
 
     pages.filter do |page|
