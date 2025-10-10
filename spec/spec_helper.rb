b2974d5c2feb144914f372e84afed7d7e99c6c09
Laurence de Bruxelles <laurence.debruxelles@dsit.gov.uk>
2025-10-10 11:02:59 +0300

    Skip tests tagged as :benchmark by default

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -76,15 +76,18 @@ RSpec.configure do |config|
   #   # https://relishapp.com/rspec/rspec-core/docs/configuration/zero-monkey-patching-mode
   #   config.disable_monkey_patching!
   #
-  #   # Many RSpec users commonly either run the entire suite or an individual
-  #   # file, and it's useful to allow more verbose output when running an
-  #   # individual spec file.
-  #   if config.files_to_run.one?
-  #     # Use the documentation formatter for detailed output,
-  #     # unless a formatter has already been configured
-  #     # (e.g. via a command-line flag).
-  #     config.default_formatter = "doc"
-  #   end
+  # Many RSpec users commonly either run the entire suite or an individual
+  # file, and it's useful to allow more verbose output when running an
+  # individual spec file.
+  if config.files_to_run.one?
+    # Use the documentation formatter for detailed output,
+    # unless a formatter has already been configured
+    # (e.g. via a command-line flag).
+    # config.default_formatter = "doc"
+  else
+    # Don't run tests tagged as benchmarks by default
+    config.filter_run_excluding :benchmark
+  end
   #
   #   # Print the 10 slowest examples and example groups at the
   #   # end of the spec run, to help surface which specs are running
