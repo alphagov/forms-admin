c740eb48350d7260397aa1f3929d7434e9c6fe98
Samuel Culley <samuel.culley@digital.cabinet-office.gov.uk>
2025-10-22 09:39:26 +0100

    Remove unneeded references to ActiveResource

--- lib/data_struct.rb
+++ lib/data_struct.rb
@@ -1,6 +1,6 @@
 # When an OpenStruct is converted to json, it inludes @table.
 # We inherit and overide as_json here to use to contain answer_settings, which
-# is a json hash converted into an object by ActiveResource. Using a plain hash
+# is a json hash converted into an object. Using a plain hash
 # for answer_settings means there is no .access to attributes.
 class DataStruct < OpenStruct
   def self.recursive_new(object)
