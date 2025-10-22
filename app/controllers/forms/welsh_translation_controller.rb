f0f2db50d53fd14c324477e9ec8583bf65f30624
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2025-10-22 09:11:28 +0100

    Add actions for Welsh translation page
    Adds placeholder actions for adding Welsh translations to forms.
    
    Also updates the path helper from `/translations` to `welsh-translation`
    and renames the controller to WelshTranslationController.

--- app/controllers/forms/welsh_translation_controller.rb
+++ app/controllers/forms/welsh_translation_controller.rb
@@ -1,7 +1,9 @@
 module Forms
-  class TranslationsController < WebController
+  class WelshTranslationController < WebController
     after_action :verify_authorized
 
     def new; end
+
+    def create; end
   end
 end
