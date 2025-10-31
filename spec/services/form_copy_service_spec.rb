389125c14ba456fa08418a248c0a6ba81e03e1fc
chao-xian <kelvin.gan@digital.cabinet-office.gov.uk>
2025-10-31 11:59:40 +0000

    WIP

--- spec/services/form_copy_service_spec.rb
+++ spec/services/form_copy_service_spec.rb
@@ -4,20 +4,29 @@ RSpec.describe FormCopyService do
   let(:source_form) { create(:form) }
   let(:source_form_document) { create(:form_document, :live, form: source_form) }
   let(:service) { described_class.new(source_form) }
+  let(:copied_form) { service.copy }
 
   describe "#copy" do
+    before do
+      source_form_document
+    end
+
     it "creates a new form" do
       expect {
-        service.copy
+        copied_form
       }.to change(Form, :count).by(1)
+
+      expect(copied_form).to be_a(Form)
+      expect(copied_form.id).not_to eq(source_form.id)
     end
 
     it "creates a new draft form document" do
       expect {
-        service.copy
+        copied_form
       }.to change(FormDocument, :count).by(1)
 
-      new_form_document = FormDocument.last
+      new_form_document = copied_form.draft_form_document
+      expect(new_form_document).to be_present
       expect(new_form_document.tag).to eq("draft")
     end
 
@@ -25,44 +34,33 @@ RSpec.describe FormCopyService do
       content = { "name" => "Test Form", "pages" => [] }
       source_form_document.update!(content:)
 
-      service.copy
-
-      new_form_document = FormDocument.last
+      new_form_document = copied_form.draft_form_document
       expect(new_form_document.content).to eq(content)
     end
 
     it "copies the language from the source form document" do
       source_form_document.update!(language: "cy")
 
-      service.copy
-
-      new_form_document = FormDocument.last
+      new_form_document = copied_form.draft_form_document
       expect(new_form_document.language).to eq("cy")
     end
 
     it "returns the new form" do
-      result = service.copy
-
-      expect(result).to be_a(Form)
-      expect(result).to eq(Form.last)
+      expect(copied_form).to be_a(Form)
+      expect(copied_form).to be_persisted
+      expect(copied_form.id).not_to eq(source_form.id)
     end
 
     it "associates the draft form document with the new form" do
-      result = service.copy
-
-      new_form_document = FormDocument.last
-      expect(new_form_document.form).to eq(result)
+      new_form_document = copied_form.draft_form_document
+      expect(new_form_document.form).to eq(copied_form)
     end
 
     context "when copying from a draft form document" do
       let(:source_form_document) { create(:form_document, :draft, form: source_form) }
 
-      before do
-        service.copy
-      end
-
       it "creates a draft form document for the new form" do
-        new_form_document = FormDocument.last
+        new_form_document = copied_form.draft_form_document
         expect(new_form_document.tag).to eq("draft")
       end
     end
@@ -70,12 +68,8 @@ RSpec.describe FormCopyService do
     context "when copying from an archived form document" do
       let(:source_form_document) { create(:form_document, :archived, form: source_form) }
 
-      before do
-        service.copy
-      end
-
       it "creates a draft form document for the new form" do
-        new_form_document = FormDocument.last
+        new_form_document = copied_form.draft_form_document
         expect(new_form_document.tag).to eq("draft")
       end
     end
