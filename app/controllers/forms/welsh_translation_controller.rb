2a2df3ba11f43df62f001b6e3c401546de1e3888
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2025-10-31 10:13:37 +0000

    Split page translation logic into separate object

--- app/controllers/forms/welsh_translation_controller.rb
+++ app/controllers/forms/welsh_translation_controller.rb
@@ -6,14 +6,14 @@ module Forms
       authorize current_form, :can_view_form?
       return redirect_to form_path(current_form) unless welsh_enabled?
 
-      @welsh_translation_input = WelshTranslationInput.new(form: current_form).assign_form_values
+      @welsh_translation_input = WelshTranslationInput.new(form: current_form, pages: welsh_page_translation_inputs_from_page).assign_form_values
     end
 
     def create
       authorize current_form, :can_view_form?
       return redirect_to form_path(current_form) unless welsh_enabled?
 
-      @welsh_translation_input = WelshTranslationInput.new(**welsh_translation_input_params)
+      @welsh_translation_input = WelshTranslationInput.new(**welsh_translation_input_params, pages: welsh_page_translation_inputs_from_params)
 
       if @welsh_translation_input.submit
         success_message = if @welsh_translation_input.mark_complete == "true"
@@ -29,11 +29,23 @@ module Forms
     end
 
     def welsh_translation_input_params
-      params.require(:forms_welsh_translation_input).permit(:mark_complete, pages: %i[id question_text_cy hint_text_cy page_heading_cy guidance_markdown_cy]).merge(form: current_form)
+      params.require(:forms_welsh_translation_input).permit(:mark_complete).merge(form: current_form)
+    end
+
+    def page_translation_input_params
+      params.require(:forms_welsh_translation_input).permit(pages: %i[id question_text_cy hint_text_cy page_heading_cy guidance_markdown_cy])
     end
 
     def welsh_enabled?
       FeatureService.new(group: current_form.group).enabled?(:welsh)
+    end
+
+    def welsh_page_translation_inputs_from_page
+      current_form.pages.map { |page| PageTranslationInput.new(id: page.id).assign_page_values }
+    end
+
+    def welsh_page_translation_inputs_from_params
+      page_translation_input_params[:pages].each_value.map { |page| PageTranslationInput.new(**page) }
     end
   end
 end
--- app/input_objects/forms/welsh_translation_input.rb
+++ app/input_objects/forms/welsh_translation_input.rb
@@ -6,22 +6,13 @@ class Forms::WelshTranslationInput < Forms::MarkCompleteInput
   def submit
     return false if invalid?
 
-    pages.each_value do |page|
-      form_page = form.pages.find_by(id: page["id"])
-      form_page.question_text_cy = page["question_text_cy"]
-      form_page.hint_text_cy = page["hint_text_cy"]
-      form_page.page_heading_cy = page["page_heading_cy"]
-      form_page.guidance_markdown_cy = page["guidance_markdown_cy"]
-
-      form_page.save!
-    end
+    pages.each(&:submit)
 
     form.welsh_completed = mark_complete
     form.save_draft!
   end
 
   def assign_form_values
-    self.pages = form.pages
     self.mark_complete = form.try(:welsh_completed)
     self
   end
