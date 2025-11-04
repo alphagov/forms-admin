a62681b46def678f25ca1c02cb82a16642fe0dab
Laurence de Bruxelles <laurence.debruxelles@dsit.gov.uk>
2025-11-04 08:39:35 +0200

    Refactor Forms::ReceiveCsvController

--- app/controllers/forms/receive_csv_controller.rb
+++ app/controllers/forms/receive_csv_controller.rb
@@ -10,11 +10,9 @@ module Forms
     def create
       authorize current_form, :can_view_form?
       @receive_csv_input = ReceiveCsvInput.new(receive_csv_input_params)
-      previous_submission_type = @receive_csv_input.form.submission_type
-      new_submission_type = @receive_csv_input.submission_type
 
       if @receive_csv_input.submit
-        redirect_to form_path(@receive_csv_input.form.id), success: success_message(previous_submission_type, new_submission_type)
+        redirect_to form_path(@receive_csv_input.form.id), success: success_message(@receive_csv_input.form)
       else
         render :new, status: :unprocessable_content
       end
@@ -26,12 +24,12 @@ module Forms
       params.require(:forms_receive_csv_input).permit(:submission_type).merge(form: current_form)
     end
 
-    def success_message(previous_submission_type, new_submission_type)
-      return nil if previous_submission_type == new_submission_type
+    def success_message(form)
+      return nil unless form.submission_type_previously_changed?
 
-      return t("banner.success.form.receive_csv_enabled") if new_submission_type == "email_with_csv"
+      return t("banner.success.form.receive_csv_enabled") if form.submission_type.include? "csv"
 
-      t("banner.success.form.receive_csv_disabled") if new_submission_type == "email"
+      t("banner.success.form.receive_csv_disabled")
     end
   end
 end
