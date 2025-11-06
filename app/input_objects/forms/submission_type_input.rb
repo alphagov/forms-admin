1351889ffe022b9547855b0ecc80e19f1534812d
Laurence de Bruxelles <laurence.debruxelles@dsit.gov.uk>
2025-11-06 12:20:40 +0200

    Update receive CSV page to set form submission format as well as type

--- app/input_objects/forms/submission_type_input.rb
+++ app/input_objects/forms/submission_type_input.rb
@@ -7,11 +7,29 @@ class Forms::SubmissionTypeInput < BaseInput
     return false if invalid?
 
     form.submission_type = submission_type
+    form.submission_format = submission_format
     form.save_draft!
   end
 
   def assign_form_values
     self.submission_type = form.try(:submission_type)
     self
+  end
+
+  def submission_format
+    return nil if submission_type.blank?
+    return [] if submission_type == "email"
+
+    formats = []
+    formats << "csv" if submission_type.include? "csv"
+    formats
+  end
+
+  def submission_format=(formats)
+    self.submission_type = if formats.nil?
+                             nil
+                           else
+                             formats.include?("csv") ? "email_with_csv" : "email"
+                           end
   end
 end
