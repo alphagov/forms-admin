23c61925da504a925efc907009c1da588d7a4661
Thomas Iles <tomiles@gmail.com>
2025-11-13 11:48:51 +0000

    Sync Welsh form documents
    This commit syncs welsh FormDocuments with the English ones.
    
    There are two heavy handed simplifications here:
    - Welsh forms are synced in lock-step with English forms. When creating
      a live version of a form, the Welsh version is made live too. This
      doesn't match the design, where the two are independent.
    - Any missing text in the Welsh form is filled in from the English
      version. This makes sense for testing and draft forms but it's not how
      we would handle live forms unless we knew we had validation in place.
    
    There is quite a bit of duplication here. There is probably a better way
    to do this.

--- app/services/form_document_sync_service.rb
+++ app/services/form_document_sync_service.rb
@@ -10,25 +10,42 @@ class FormDocumentSyncService
     end
 
     def update_draft_form_document(form)
-      update_or_create_form_document(form, "draft", form.as_form_document)
+      form.available_languages.each do |language|
+        Mobility.with_locale(language) do
+          content = form.as_form_document
+          update_or_create_form_document(form, "draft", content, language:)
+        end
+      end
     end
 
   private
 
     def sync_live_form(form)
-      content = form.as_form_document(live_at: form.updated_at)
-      update_or_create_form_document(form, "live", content)
-      delete_form_documents(form, "archived")
+      FormDocument.transaction do
+        form.available_languages.each do |language|
+          Mobility.with_locale(language) do
+            content = form.as_form_document(live_at: form.updated_at)
+            update_or_create_form_document(form, "live", content, language:)
+            delete_form_documents(form, "archived")
+          end
+        end
+      end
     end
 
     def sync_archived_form(form)
-      live_form_document = FormDocument.find_by!(form:, tag: "live")
-      update_or_create_form_document(form, "archived", live_form_document.content)
-      delete_form_documents(form, "live")
+      FormDocument.transaction do
+        form.available_languages.each do |language|
+          Mobility.with_locale(language) do
+            live_form_document = FormDocument.find_by!(form:, tag: "live")
+            update_or_create_form_document(form, "archived", live_form_document.content, language:)
+            delete_form_documents(form, "live")
+          end
+        end
+      end
     end
 
-    def update_or_create_form_document(form, tag, content)
-      form_document = FormDocument.find_or_initialize_by(form_id: form.id, tag:)
+    def update_or_create_form_document(form, tag, content, language: "en")
+      form_document = FormDocument.find_or_initialize_by(form_id: form.id, tag:, language:)
       form_document.content = content
       form_document.save!
     end
