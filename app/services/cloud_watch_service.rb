31effe287a30e72f3a07f46a2f8b60a81475f276
Samuel Culley <samuel.culley@digital.cabinet-office.gov.uk>
2025-11-26 09:25:18 +0000

    Add full_metrics_data to CloudWatchService
    Adds a method to CloudWatchService which retrieves data for the past 15
    months, separated into daily data points. Metrics retrieved are for form
    starts and form submissions.
    
    If no data is available for a given day, then there is no resulting
    entry in the returned array.

--- app/services/cloud_watch_service.rb
+++ app/services/cloud_watch_service.rb
@@ -1,6 +1,7 @@
 class CloudWatchService
   REGION = "eu-west-2".freeze
   A_WEEK = 604_800
+  A_DAY = 86_400
   METRICS_NAMESPACE = "Forms".freeze
 
   attr_reader :form_id, :made_live_date
@@ -24,6 +25,20 @@ class CloudWatchService
     {
       weekly_submissions:,
       weekly_starts:,
+    }
+  rescue Aws::CloudWatch::Errors::ServiceError,
+         Aws::Errors::MissingCredentialsError => e
+
+    Sentry.capture_exception(e)
+    nil
+  end
+
+  def daily_metrics_data
+    return nil if made_live_date.nil?
+
+    {
+      submissions: daily_submissions(form_id:),
+      starts: daily_starts(form_id:),
     }
   rescue Aws::CloudWatch::Errors::ServiceError,
          Aws::Errors::MissingCredentialsError => e
@@ -72,6 +87,46 @@ private
     })
 
     response.datapoints[0]&.sum.to_i || 0
+  end
+
+  def daily_submissions(form_id:)
+    cloudwatch_client = Aws::CloudWatch::Client.new(region: REGION)
+
+    response = cloudwatch_client.get_metric_statistics({
+      metric_name: "Submitted",
+      namespace: METRICS_NAMESPACE,
+      dimensions: [
+        environment_dimension,
+        form_id_dimension(form_id),
+      ],
+      start_time: start_of_today - 15.months,
+      end_time: start_of_today,
+      period: A_DAY,
+      statistics: %w[Sum],
+      unit: "Count",
+    })
+
+    response.datapoints.sort_by(&:timestamp).map { { timestamp: it.timestamp, sum: it.sum } }
+  end
+
+  def daily_starts(form_id:)
+    cloudwatch_client = Aws::CloudWatch::Client.new(region: REGION)
+
+    response = cloudwatch_client.get_metric_statistics({
+      metric_name: "Started",
+      namespace: METRICS_NAMESPACE,
+      dimensions: [
+        environment_dimension,
+        form_id_dimension(form_id),
+      ],
+      start_time: start_of_today - 15.months,
+      end_time: start_of_today,
+      period: A_DAY,
+      statistics: %w[Sum],
+      unit: "Count",
+    })
+
+    response.datapoints.sort_by(&:timestamp).map { { timestamp: it.timestamp, sum: it.sum } }
   end
 
   def environment_dimension
