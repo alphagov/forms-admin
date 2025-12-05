9cfeaa34a7427c87c7ec7894ab6813a97571062a
chao-xian <kelvin.gan@digital.cabinet-office.gov.uk>
2025-12-05 13:25:59 +0000

    Add controller and route for copying

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,8 @@ Rails.application.routes.draw do
     get "/change-name" => "forms/change_name#edit", as: :change_form_name
     post "/change-name" => "forms/change_name#update"
 
+    get "/copy" => "forms/copy#copy", as: :copy_form
+
     scope "/live" do
       get "/" => "forms/live#show_form", as: :live_form
       get "/pages" => "forms/live#show_pages", as: :live_form_pages
