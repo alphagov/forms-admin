7a61dba80b606dc10ada05e2811bb87b78780e3f
chao-xian <kelvin.gan@digital.cabinet-office.gov.uk>
2025-12-05 17:00:28 +0000

    WIP

---
+++ app/input_objects/forms/copy_input.rb
@@ -0,0 +1,20 @@
+class Forms::CopyInput < Forms::NameInput
+  include TextInputHelper
+
+  attr_accessor :copied_name
+
+  validates :copied_name, presence: true, length: { maximum: 2000 }
+
+
+  def submit
+    return false if invalid?
+
+    form.name = copied_name
+    form.save_draft!
+  end
+
+  def assign_form_values
+    self.name = copied_name
+    self
+  end
+end
