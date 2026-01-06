dca2597f305250ef84756b6e2be212a0c95edaf4
Laurence de Bruxelles <laurence.debruxelles@dsit.gov.uk>
2026-01-06 12:10:17 +0200

    Refactor Sentry integration specs

--- spec/integration/sentry_spec.rb
+++ spec/integration/sentry_spec.rb
@@ -21,19 +21,33 @@ RSpec.describe "config/initializers/sentry" do
     before do
       raise "Something went wrong: #{form.inspect}"
     rescue RuntimeError => e
+      Sentry.set_context(:user, { email: "user@example.org", id: "some-user-id" })
       Sentry.capture_exception(e)
     end
 
-    it "scrubs email addresses from everywhere in the event" do
-      expect(last_sentry_event.to_hash.to_s).not_to include "submission-email@test.example"
+    it "captures the exception" do
+      expect(last_sentry_event).to be_present
     end
 
-    it "replaces the email address in the exception with a comment" do
-      expect(last_sentry_event.to_hash[:exception][:values].first[:value]).to include "[Filtered (client-side)]"
+    it "scrubs email addresses from everywhere in the event" do
+      expect(last_sentry_event.to_h.to_s).not_to include "submission-email@test.example"
+      expect(last_sentry_event.to_h.to_s).not_to include "user@example.org"
+    end
+
+    it "replaces the email address in the exception with a mask" do
+      expect(last_sentry_event.to_h[:exception][:values].first[:value]).to include "[Filtered (client-side)]"
     end
 
     it "keeps the rest of the exception message" do
-      expect(last_sentry_event.to_hash[:exception][:values].first[:value]).to include "Something went wrong"
+      expect(last_sentry_event.to_h[:exception][:values].first[:value]).to include "Something went wrong"
+    end
+
+    it "replaces the email address in the context with a mask" do
+      expect(last_sentry_event[:contexts][:user][:email]).to eq "[Filtered (client-side)]"
+    end
+
+    it "keeps the rest of the context" do
+      expect(last_sentry_event[:contexts][:user][:id]).to eq "some-user-id"
     end
   end
 
@@ -58,11 +72,11 @@ RSpec.describe "config/initializers/sentry" do
     end
 
     it "scrubs email addresses from everywhere in the event" do
-      expect(last_sentry_event.to_hash.to_s).not_to include "new-submission-email@test.example"
+      expect(last_sentry_event.to_h.to_s).not_to include "new-submission-email@test.example"
     end
 
     it "replaces the email address in the breadcrumbs with a comment" do
-      expect(last_sentry_event.to_hash[:breadcrumbs][:values].last[:data]["params"]["forms_submission_form"]["temporary_submission"]).to eq "[Filtered (client-side)]"
+      expect(last_sentry_event.to_h[:breadcrumbs][:values].last[:data]["params"]["forms_submission_form"]["temporary_submission"]).to eq "[Filtered (client-side)]"
     end
   end
 end
