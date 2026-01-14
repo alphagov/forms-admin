e557348900161f513ea7f5f89c1e837145693cd1
Thomas Iles <tomiles@gmail.com>
2026-01-14 16:07:33 +0000

    Add #form to DraftQuestion
    DraftQuestion stores the form it's linked to in the form_id attribute.
    This is used to access the form name, by looking up the form using
    Form.find(form_id).
    
    For the Welsh translation work, we need to check if the form has Welsh
    translations when creating new pages using DraftQuestion.
    
    DraftQuestion#form_name is now delegated to DraftQuestion#form, which
    returns the form object. A new method #form is added to DraftQuestion to
    access other attributes of the form.
    
    The link between DraftQuestion and Form should be an ActiveRecord
    association. I'll raise a ticket to make this change. I tried to do it
    as part of this commit but it means changing a tests and factories,
    which makes it a larger task.

--- app/models/draft_question.rb
+++ app/models/draft_question.rb
@@ -14,7 +14,9 @@ class DraftQuestion < ApplicationRecord
     raw_settings.deep_symbolize_keys
   end
 
-  def form_name
-    Form.find(form_id).name
+  delegate :name, to: :form, prefix: true
+
+  def form
+    Form.find(form_id)
   end
 end
