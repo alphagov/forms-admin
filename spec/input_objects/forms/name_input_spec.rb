3de7be8faca87efcefac389e13e10d465e498b7c
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2026-01-23 09:59:31 +0000

    Lower form name limit to 500 characters
    Our longest existing form name is 249 characters, ad the box for adding it is a single-line text box, so 2,000 feels too permissive.

--- spec/input_objects/forms/name_input_spec.rb
+++ spec/input_objects/forms/name_input_spec.rb
@@ -13,9 +13,9 @@ RSpec.describe Forms::NameInput, type: :model do
       )
     end
 
-    it "is invalid if longer than 2000 characters" do
-      name_input = described_class.new(name: "Apply for a juggling licence".ljust(2001, "a"))
-      error_message = I18n.t("activemodel.errors.models.forms/name_input.attributes.name.too_long")
+    it "is invalid if longer than 500 characters" do
+      name_input = described_class.new(name: "Apply for a juggling licence".ljust(501, "a"))
+      error_message = I18n.t("activemodel.errors.models.forms/name_input.attributes.name.too_long", count: 500)
 
       expect(name_input).to be_invalid
 
@@ -24,8 +24,8 @@ RSpec.describe Forms::NameInput, type: :model do
       )
     end
 
-    it "is valid if 2000 characters or fewer" do
-      name_input = described_class.new(name: "Apply for a juggling licence".ljust(2000, "a"))
+    it "is valid if 500 characters or fewer" do
+      name_input = described_class.new(name: "Apply for a juggling licence".ljust(500, "a"))
 
       expect(name_input).to be_valid
     end
--- spec/input_objects/forms/welsh_translation_input_spec.rb
+++ spec/input_objects/forms/welsh_translation_input_spec.rb
@@ -69,20 +69,20 @@ RSpec.describe Forms::WelshTranslationInput, type: :model do
       let(:mark_complete) { "true" }
 
       context "when the Welsh form name is present" do
-        context "when the Welsh form name is 2000 characters or fewer" do
-          let(:new_input_data) { super().merge(name_cy: "A form".ljust(2000, "x")) }
+        context "when the Welsh form name is 500 characters or fewer" do
+          let(:new_input_data) { super().merge(name_cy: "A form".ljust(500, "x")) }
 
           it "is valid" do
             expect(welsh_translation_input).to be_valid
           end
         end
 
-        context "when the Welsh form name is 2001 characters or more" do
-          let(:new_input_data) { super().merge(name_cy: "A form".ljust(2001, "x")) }
+        context "when the Welsh form name is 501 characters or more" do
+          let(:new_input_data) { super().merge(name_cy: "A form".ljust(501, "x")) }
 
           it "is not valid" do
             expect(welsh_translation_input).not_to be_valid
-            expect(welsh_translation_input.errors.full_messages_for(:name_cy)).to include "Name cy #{I18n.t('activemodel.errors.models.forms/welsh_translation_input.attributes.name_cy.too_long')}"
+            expect(welsh_translation_input.errors.full_messages_for(:name_cy)).to include "Name cy #{I18n.t('activemodel.errors.models.forms/welsh_translation_input.attributes.name_cy.too_long', count: 500)}"
           end
         end
       end
