aec86777010369551b4354e436159e9d7baa60c9
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2026-02-05 16:16:24 +0000

    Pass Welsh form document to view

--- app/controllers/forms/live_controller.rb
+++ app/controllers/forms/live_controller.rb
@@ -8,7 +8,7 @@ module Forms
       return redirect_to archived_form_path if current_form.is_archived?
       raise NotFoundError unless current_form.is_live?
 
-      render :show_form, locals: { form_metadata: current_form, form_document: current_live_form }
+      render :show_form, locals: { form_metadata: current_form, form_document: current_live_form, welsh_form_document: }
     end
 
     def show_pages
@@ -18,6 +18,14 @@ module Forms
       raise NotFoundError unless current_form.is_live?
 
       render :show_pages, locals: { form_document: current_live_form }
+    end
+
+  private
+
+    def welsh_form_document
+      return nil unless current_live_form.has_welsh_translation?
+
+      current_live_welsh_form
     end
   end
 end
