cd60f2a8fefdea53e4ac0623e238f23da5decdf4
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2026-02-13 09:18:04 +0000

    Provide welsh form document to to pages view

--- spec/views/forms/archived/show_pages.html.erb_spec.rb
+++ spec/views/forms/archived/show_pages.html.erb_spec.rb
@@ -3,9 +3,10 @@ require "rails_helper"
 describe "forms/archived/show_pages.html.erb" do
   let(:form_metadata) { create :form, :archived }
   let(:form_document) { FormDocument::Content.from_form_document(form_metadata.archived_form_document) }
+  let(:welsh_form_document) { nil }
 
   before do
-    render(template: "forms/archived/show_pages", locals: { form_document: })
+    render(template: "forms/archived/show_pages", locals: { form_document:, welsh_form_document: })
   end
 
   it "form name is in the page title" do
--- spec/views/forms/live/show_pages.html.erb_spec.rb
+++ spec/views/forms/live/show_pages.html.erb_spec.rb
@@ -3,9 +3,10 @@ require "rails_helper"
 describe "forms/live/show_pages.html.erb" do
   let(:form) { create :form, :live }
   let(:form_document) { FormDocument::Content.from_form_document(form.live_form_document) }
+  let(:welsh_form_document) { nil }
 
   before do
-    render(template: "forms/live/show_pages", locals: { form_document: form_document })
+    render(template: "forms/live/show_pages", locals: { form_document:, welsh_form_document: })
   end
 
   it "form name is in the page title" do
