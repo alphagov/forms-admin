dfd333d73ab0eb00e05ea0df2908c43427ba08fe
Thomas Iles <tomiles@gmail.com>
2026-02-17 10:24:00 +0000

    Add archive_welsh_only to archive form service
    The archive service is already responsible for archiving the live form document.
    
    We add a new method to archive the welsh form document.
    
    To archive an English form we use the state machine and callbacks to
    ensure the state of the form is consistent. But to archive only the
    Welsh part of a form we call the FormDocumentSyncService directly.
    
    This is because archiving a Welsh form doesn't change the state of a
    form. We can't use the state machine because we aren't changing the
    state of a form.

--- app/services/archive_form_service.rb
+++ app/services/archive_form_service.rb
@@ -1,14 +1,32 @@
 class ArchiveFormService
+  attr_reader :form, :current_user
+
   def initialize(form:, current_user:)
     @form = form
     @current_user = current_user
   end
 
   def archive
-    @form.archive_live_form!
-    SubmissionEmailMailer.alert_processor_form_archive(processor_email: @form.submission_email,
-                                                       form_name: @form.name,
-                                                       archived_by_name: @current_user.name,
-                                                       archived_by_email: @current_user.email).deliver_now
+    form.archive_live_form!
+    SubmissionEmailMailer.alert_processor_form_archive(processor_email: form.submission_email,
+                                                       form_name: form.name,
+                                                       archived_by_name: current_user.name,
+                                                       archived_by_email: current_user.email).deliver_now
+  end
+
+  def archive_welsh_only
+    return unless has_live_welsh_form?
+
+    archive_welsh_form_document
+  end
+
+private
+
+  def has_live_welsh_form?
+    form.live_welsh_form_document.present?
+  end
+
+  def archive_welsh_form_document
+    FormDocumentSyncService.new(form).synchronize_archived_welsh_form
   end
 end
