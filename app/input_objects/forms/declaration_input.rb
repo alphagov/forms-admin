177645a2bdbb15ba02863c97875413a36ffdc6f6
Thomas Iles <tomiles@gmail.com>
2026-02-24 14:15:33 +0000

    Use Markdown editor for declaration
    Change the declaration page to use the Markdown editor and the
    declaration_markdown field instead of a text area and declaration_text.

--- app/input_objects/forms/declaration_input.rb
+++ app/input_objects/forms/declaration_input.rb
@@ -1,23 +1,22 @@
 class Forms::DeclarationInput < Forms::MarkCompleteInput
   include TextInputHelper
 
-  attr_accessor :declaration_text
+  attr_accessor :declaration_markdown
 
-  validates :declaration_text, length: { maximum: 2000 }
+  validates :declaration_markdown, length: { maximum: 2000 }
 
   before_validation :strip_carriage_returns_from_input
 
   def submit
     return false if invalid?
 
-    form.declaration_text = declaration_text
-    form.declaration_markdown = MarkdownConversionService.new(declaration_text).to_markdown
+    form.declaration_markdown = declaration_markdown
     form.declaration_section_completed = mark_complete
     form.save_draft!
   end
 
   def assign_form_values
-    self.declaration_text = form.declaration_text
+    self.declaration_markdown = form.declaration_markdown
     self.mark_complete = form.try(:declaration_section_completed)
     self
   end
@@ -25,6 +24,6 @@ class Forms::DeclarationInput < Forms::MarkCompleteInput
 private
 
   def strip_carriage_returns_from_input
-    strip_carriage_returns!(declaration_text)
+    strip_carriage_returns!(declaration_markdown)
   end
 end
