042f05fe2b8659cad0a248083037dc2aa1ba0bb7
Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>
2026-02-25 17:38:29 +0000

    Add view and input object for page to enable sending daily CSV
    Add a view and an input object for configuring whether the
    send_daily_submission_batch Form attribute is enabled. This page has
    a single checkbox to enable or disable sending the daily CSV of
    submissions, and no validation.

---
+++ spec/input_objects/forms/daily_submission_batch_input_spec.rb
@@ -0,0 +1,38 @@
+require "rails_helper"
+
+RSpec.describe Forms::DailySubmissionBatchInput, type: :model do
+  describe "#submit" do
+    subject(:input) { described_class.new(form:, send_daily_submission_batch: value) }
+
+    let(:form) { create(:form, send_daily_submission_batch: false) }
+
+    context "when enabling" do
+      let(:value) { "1" }
+
+      it "updates the form send_daily_submission_batch flag to true" do
+        expect { input.submit }.to change { form.reload.send_daily_submission_batch }.to(true)
+      end
+    end
+
+    context "when disabling" do
+      let(:form) { create(:form, send_daily_submission_batch: true) }
+      let(:value) { "0" }
+
+      it "updates the form send_daily_submission_batch flag to false" do
+        expect { input.submit }.to change { form.reload.send_daily_submission_batch }.to(false)
+      end
+    end
+  end
+
+  describe "#assign_form_values" do
+    subject(:input) { described_class.new(form:) }
+
+    let(:form) { create(:form, send_daily_submission_batch: true) }
+
+    it "sets the send_daily_submission_batch value from the form" do
+      input.assign_form_values
+
+      expect(input.send_daily_submission_batch).to be(true)
+    end
+  end
+end
---
+++ spec/views/forms/daily_submission_batch/new.html.erb_spec.rb
@@ -0,0 +1,44 @@
+require "rails_helper"
+
+describe "forms/daily_submission_batch/new.html.erb" do
+  let(:send_daily_submission_batch) { true }
+  let(:form) { build(:form, id: 1, send_daily_submission_batch:) }
+  let(:daily_submission_batch_input) { Forms::DailySubmissionBatchInput.new(form:).assign_form_values }
+
+  before do
+    assign(:daily_submission_batch_input, daily_submission_batch_input)
+    render
+  end
+
+  it "sets the page title" do
+    expect(view.content_for(:title)).to eq(t("page_titles.daily_submission_batch"))
+  end
+
+  it "has the correct heading" do
+    expect(rendered).to have_css("h1", text: t("page_titles.daily_submission_batch"))
+  end
+
+  it "includes the expected fieldset legend" do
+    expect(rendered).to have_css("legend", text: "Do you want to get a daily CSV of the previous day’s completed forms?")
+  end
+
+  it "includes the expected checkbox label" do
+    expect(rendered).to have_css(".govuk-label", text: "Get a daily CSV of completed forms")
+  end
+
+  context "when the form has send_daily_submission_batch set to true" do
+    let(:send_daily_submission_batch) { true }
+
+    it "renders the checkbox as checked" do
+      expect(rendered).to have_css("input.govuk-checkboxes__input[value='1'][checked]")
+    end
+  end
+
+  context "when the form has send_daily_submission_batch set to false" do
+    let(:send_daily_submission_batch) { false }
+
+    it "renders the checkbox as unchecked" do
+      expect(rendered).not_to have_css("input.govuk-checkboxes__input[value='1'][checked]")
+    end
+  end
+end
