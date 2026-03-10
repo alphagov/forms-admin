84b5eeb4bb275a8e148fe4bcc57d183ca2511233
Stephen Daly <stephen.daly@digital.cabinet-office.gov.uk>
2026-03-10 15:09:18 +0000

    Send alerts to org admins when a draft is created for an existing form
    Send an email to organisation admins when a draft is created for an
    existing live or archived form. This is added as an after_action in
    the FormsController base controller as any controller action that
    updates a form will create a draft if the form is currently in a live
    or archived state.
    
    We need to store the initial state of the form in the controller to
    compare to in the after_action to determine if a draft was created.
    This is because the form could have been updated in a separate
    instance of the Form model, which is the case when adding a new
    question to the form - so we cannot use the `state_previously_was`
    helper method provided by ActiveModel::Dirty.
    
    As all controllers that inherit from FormsController use the
    `current_form`, we can set the instance variable in a before_action
    along with the @initial_form_state. This makes the controller tests
    for FormsController redundant as they were just checking the that
    form was only retrieved once.

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -1,9 +1,9 @@
 class FormsController < WebController
+  before_action :load_form
   after_action :verify_authorized
+  after_action :alert_org_admins_if_draft_created
 
-  def current_form
-    @current_form ||= Form.find(params[:form_id])
-  end
+  attr_reader :current_form
 
   def current_live_form
     @current_live_form ||= FormDocument::Content.from_form_document(current_form.live_form_document)
@@ -19,5 +19,19 @@ class FormsController < WebController
 
   def current_archived_welsh_form
     @current_archived_welsh_form ||= FormDocument::Content.from_form_document(current_form.archived_welsh_form_document)
+  end
+
+private
+
+  def load_form
+    @current_form = Form.find(params[:form_id])
+    @initial_form_state = @current_form.state
+  end
+
+  def alert_org_admins_if_draft_created
+    return if current_form.destroyed?
+    return unless @current_form.reload.draft_created?(@initial_form_state)
+
+    OrgAdminAlertsService.new(form: current_form, current_user:).draft_of_existing_form_created
   end
 end
