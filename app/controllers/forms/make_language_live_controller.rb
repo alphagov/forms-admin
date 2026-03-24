1e01914deaae3a4c2c19989d07ae08eab46a64c6
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2026-03-24 16:59:04 +0000

    Add view for making language live

--- app/controllers/forms/make_language_live_controller.rb
+++ app/controllers/forms/make_language_live_controller.rb
@@ -3,8 +3,7 @@ module Forms
     def new
       authorize current_form, :can_make_language_live?
       @make_language_live_input = MakeLiveInput.new(form: current_form)
-
-      render "new", status:, locals: { current_form: }
+      render_new
     end
 
     def create
@@ -15,12 +14,10 @@ module Forms
       return render_new(status: :unprocessable_content) unless @make_language_live_input.valid?
       return redirect_to form_path(@make_language_live_input.form.id) unless @make_language_live_input.confirmed?
 
-      @make_form_live_service = MakeFormLiveService.call(current_form:, current_user:, language: params[:language])
+      @make_form_live_service = MakeFormLiveService.call(current_form:, current_user:)
       # TODO: actually make the language live
 
-      @go_to_make_welsh_live_input = GoToMakeWelshLiveInput.new
-
-      render "confirmation", locals: {
+      render "forms/make_live/confirmation", locals: {
         current_form:,
         confirmation_page_title: @make_form_live_service.page_title,
         confirmation_page_body: @make_form_live_service.confirmation_page_body,
@@ -32,6 +29,10 @@ module Forms
 
     def make_language_live_input_params
       params.require(:forms_make_live_input).permit(:confirm).merge(form: current_form)
+    end
+
+    def render_new(status: :ok)
+      render "new", status:, locals: { current_form:, language: params[:language] }
     end
   end
 end
---
+++ spec/requests/forms/make_language_live_controller_spec.rb
@@ -0,0 +1,162 @@
+require "rails_helper"
+
+RSpec.describe Forms::MakeLanguageLiveController, type: :request do
+  let(:user) { build :user, organisation: }
+  let(:form) { create(:form, :ready_for_live) }
+  let(:id) { form.id }
+  let(:language) { "en" }
+
+  let(:form_params) { nil }
+
+  let(:organisation) { test_org }
+  let(:group_role) { :group_admin }
+  let(:group) { create(:group, organisation:, status: :active) }
+
+  describe "#new" do
+    before do
+      Membership.create!(group_id: group.id, user:, added_by: user, role: group_role)
+      GroupForm.create!(form_id: form.id, group_id: group.id)
+
+      login_as user
+    end
+
+    it "renders make your form live" do
+      get make_language_live_path(form_id: form.id, language:)
+      expect(response).to render_template("make_language_live/new")
+      expect(response).to have_http_status(:ok)
+    end
+
+    context "when editing a draft of an existing live form" do
+      let(:form) { create(:form, :live) }
+
+      it "redirects to the make_changes_live view" do
+        pending "not yet implemented"
+        raise
+      end
+    end
+
+    context "when editing a draft of an archived form" do
+      let(:form) { create(:form, :archived_with_draft) }
+
+      it "redirects to the make_changes_live view" do
+        pending "not yet implemented"
+        raise
+      end
+    end
+
+    context "when current user is not a group admin" do
+      let(:group_role) { :editor }
+
+      it "is forbidden" do
+        get make_language_live_path(form_id: form.id, language:)
+        expect(response).to have_http_status(:forbidden)
+      end
+    end
+  end
+
+  describe "#create" do
+    before do
+      Membership.create!(group_id: group.id, user:, added_by: user, role: group_role)
+      GroupForm.create!(form_id: form.id, group_id: group.id)
+      create(:organisation_admin_user, organisation:)
+
+      login_as user
+    end
+
+    context "when making a form live" do
+      let(:form_params) { { forms_make_live_input: { confirm: :yes, form: } } }
+
+      it "makes the form live" do
+        pending "not yet implemented"
+        raise
+      end
+
+      it "renders the confirmation page" do
+        post(make_language_live_path(form_id: form.id, language:), params: form_params)
+        expect(response).to render_template(:confirmation)
+      end
+
+      it "sends an email to the organisation admins" do
+        pending "not yet implemented"
+        raise
+      end
+
+      context "and that form has not been made live before" do
+        it "has the page title 'Your form is live'" do
+          post(make_language_live_path(form_id: form.id, language:), params: form_params)
+          expect(response.body).to include "Your form is live"
+        end
+
+        it "creates a FormDocument" do
+          pending "not yet implemented"
+          raise
+        end
+
+        it "sets the FormDocument's live_at time to be equal to the form's updated_at time" do
+          pending "not yet implemented"
+          raise
+        end
+      end
+
+      context "and that form has already been made live before" do
+        it "redirects to the make_changes_live view" do
+          pending "not yet implemented"
+          raise
+        end
+      end
+    end
+
+    context "when deciding not to make a form live" do
+      let(:form_params) { { forms_make_live_input: { confirm: :no } } }
+
+      before do
+        post(make_language_live_path(form_id: form.id, language:), params: form_params)
+      end
+
+      it "does not make the form live" do
+        expect(form.reload.draft?).to be true
+      end
+
+      it "redirects you to the form page" do
+        expect(response).to redirect_to(form_path(form.id))
+      end
+
+      it "does not send an email to the organisation admins" do
+        post(make_language_live_path(form_id: form.id, language:), params: form_params)
+        expect(ActionMailer::Base.deliveries.count).to eq(0)
+      end
+    end
+
+    context "when all tasks are not complete" do
+      let(:form) { create(:form, :missing_pages) }
+      let(:form_params) { { forms_make_live_input: { confirm: "yes", form: } } }
+
+      before do
+        post(make_language_live_path(form_id: form.id, language:), params: form_params)
+      end
+
+      it "returns 422" do
+        expect(response).to have_http_status(:unprocessable_content)
+      end
+
+      it "does not make the form live" do
+        expect(form.reload.draft?).to be true
+      end
+
+      it "re-renders the page with an error" do
+        expect(response).to render_template("new")
+        expect(response.body).to include("You cannot make your form live because you have not finished adding questions.")
+      end
+    end
+
+    context "when current user is not a group admin" do
+      let(:group_role) { :editor }
+
+      it "is forbidden" do
+        post(make_language_live_path(form_id: form.id, language:), params: form_params)
+
+        expect(response).to have_http_status(:forbidden)
+      end
+    end
+  end
+end
