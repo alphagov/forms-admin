e465b5c203c9bf7a91b39fef2b597c5163e73abe
David Biddle <david.biddle@digital.cabinet-office.gov.uk>
2026-03-25 14:34:23 +0000

    Add separate confirmation for making individual languages live

--- app/controllers/forms/make_language_live_controller.rb
+++ app/controllers/forms/make_language_live_controller.rb
@@ -20,6 +20,16 @@ module Forms
 
       @go_to_make_welsh_live_input = GoToMakeWelshLiveInput.new
 
+      redirect_to make_language_live_show_confirmation_path
+    end
+
+    def show_confirmation
+      authorize current_form, :can_make_language_live?
+
+      @make_form_live_service = MakeFormLiveService.call(current_form:, current_user:, language: params[:language])
+
+      @go_to_make_welsh_live_input = GoToMakeWelshLiveInput.new
+
       render "confirmation", locals: {
         current_form:,
         confirmation_page_title: @make_form_live_service.page_title,
@@ -28,10 +38,26 @@ module Forms
       }
     end
 
+    def submit_confirmation
+      authorize current_form, :can_make_language_live?
+
+      @go_to_make_welsh_live_input = GoToMakeWelshLiveInput.new(**go_to_make_welsh_live_input_params)
+
+      if @go_to_make_welsh_live_input.confirmed?
+        redirect_to make_language_live_path(language: "cy")
+      else
+        redirect_to form_path
+      end
+    end
+
   private
 
     def make_language_live_input_params
       params.require(:forms_make_language_live_input).permit(:confirm, :language).merge(form: current_form)
+    end
+
+    def go_to_make_welsh_live_input_params
+      params.require(:forms_go_to_make_welsh_live_input).permit(:confirm)
     end
   end
 end
